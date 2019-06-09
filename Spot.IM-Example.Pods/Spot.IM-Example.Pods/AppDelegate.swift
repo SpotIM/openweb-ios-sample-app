fd4e076e717574bd4acd2335fec81818100e64d7
Andriy Fedin <andriy.fedin@yalantis.com>
2019-06-09 22:32:48 +0300

    Pods testing project added

---
+++ Spot.IM-Example.Pods/Spot.IM-Example.Pods/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Spot.IM-Example.Pods
+//
+//  Created by Andriy Fedin on 09/06/19.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import Spot_IM_Core
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let tester = IntegrationTester.init()
+        tester.test()
+
+        return true
+    }
+}
+
---
+++ Spot.IM-Example.Pods/Spot.IM-Example.Pods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot.IM-Example.Pods
+//
+//  Created by Andriy Fedin on 09/06/19.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
