7191dd82c162917aa649f39c6a7ccad51ec9e250
Andriy Fedin <andriy.fedin@yalantis.com>
2019-06-19 17:32:36 +0300

    Spot.IM SDK and example usage added

---
+++ Spot.IM-Example.DND/Frameworks/Spot_IM_Core.framework/Headers/Spot_IM_Core.h
@@ -0,0 +1,19 @@
+//
+//  Spot_IM_Core.h
+//  Spot.IM-Core
+//
+//  Created by Andriy Fedin on 09/06/19.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spot_IM_Core.
+FOUNDATION_EXPORT double Spot_IM_CoreVersionNumber;
+
+//! Project version string for Spot_IM_Core.
+FOUNDATION_EXPORT const unsigned char Spot_IM_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spot_IM_Core/PublicHeader.h>
+
+
