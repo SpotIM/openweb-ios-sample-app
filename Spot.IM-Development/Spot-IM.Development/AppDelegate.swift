bedb9deb48246970916ccdc555802ce9d2c16cff
Andriy Fedin <andriy.fedin@yalantis.com>
2019-07-26 18:55:52 +0300

    Make use of SSO; Minor UI update.

--- Spot.IM-Development/Spot-IM.Development/AppDelegate.swift
+++ Spot.IM-Development/Spot-IM.Development/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        SPClientSettings.spotKey = "sp_ly3RvXf6"
+        SPClientSettings.setup(spotKey: "sp_ly3RvXf6")
 
         return true
     }
