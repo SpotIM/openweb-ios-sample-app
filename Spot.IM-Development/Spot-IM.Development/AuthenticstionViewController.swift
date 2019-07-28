cdde173d8ca2c41d45b9f7cb2956db2b78d3b364
Itay Dressler <itay.d@spot.im>
2019-07-29 01:40:44 +0300

    - Splited authentication code - Updated main vc

---
+++ Spot.IM-Development/Spot-IM.Development/AuthenticstionViewController.swift
@@ -0,0 +1,153 @@
+//
+//  AuthenticstionViewController.swift
+//  Spot-IM.Development
+//
+//  Created by Itay Dressler on 29/07/2019.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import Spot_IM_Core
+
+class AuthenticstionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var genericTokenIndicator: UILabel!
+    @IBOutlet weak var genericAuthenticationIndicator: UILabel!
+    @IBOutlet weak var getGenericTokenButton: UIButton!
+    @IBOutlet weak var getCodeAButton: UIButton!
+    @IBOutlet weak var foxTokenIndicator: UILabel!
+    
+    let authProvider = SPDefaultAuthProvider()
+    
+    let accessTokenNetwork = "03190715DchJcY"
+    let username = "test"
+    let password = "1234"
+    
+    var genericToken: String? {
+        didSet { updateUI() }
+    }
+    var codeA: String?
+    var codeB: String?
+    var  genericAuthDone = false {
+        didSet { updateUI() }
+    }
+    
+    var foxAuthDone: Bool = false {
+        didSet { updateUI() }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Authentication"
+    }
+    
+    
+    private func updateUI() {
+        if genericToken == nil {
+            genericTokenIndicator.text = "❌"
+            getCodeAButton.isEnabled = false
+        } else {
+            genericTokenIndicator.text = "✅"
+            getCodeAButton.isEnabled = true
+        }
+        
+        genericAuthenticationIndicator.text = genericAuthDone ? "✅" : "❌"
+        foxTokenIndicator.text = foxAuthDone ? "✅" : "❌"
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        genericTokenIndicator.text = "⏳"
+        DemoAuthenticationProvider.logIn(with: username, password: password) { (token, error) in
+            self.genericToken = token
+        }
+    }
+    
+    @IBAction func startGenericSSO(_ sender: Any) {
+        // TODO: (Fedin) remove SPClientSettings.setup from here
+        // when everything working with single key in AppDelegate
+        SPClientSettings.setup(spotKey: .demoGenericSpotKeyForSSO)
+        
+        foxAuthDone = false
+        genericAuthenticationIndicator.text = "⏳"
+        
+        authProvider.startSSO(with: nil, completion: { (response, error) in
+            if let error = error {
+                print(error)
+                self.genericAuthDone = false
+            } else {
+                self.codeA = response?.codeA
+                self.getCodeB()
+            }
+        })
+    }
+    
+    @IBAction func startFoxSSO(_ sender: Any) {
+        // TODO: (Fedin) remove SPClientSettings.setup from here
+        // when everything working with single key in AppDelegate
+        SPClientSettings.setup(spotKey: .demoFoxSpotKeyForSSO)
+        
+        genericAuthDone = false
+        foxTokenIndicator.text = "⏳"
+        
+        let params = SSOStartParameters(token: nil, secret: .demoFoxSecretForSSO)
+        authProvider.startSSO(with: params, completion: { (response, error) in
+            if let error = error {
+                print(error)
+                self.foxAuthDone = false
+            } else {
+                if let autoComplete = response?.autoComplete, autoComplete {
+                    self.foxAuthDone = response?.success ?? false
+                } else {
+                    self.codeA = response?.codeA
+                    self.getCodeB()
+                }
+            }
+        })
+    }
+    
+    private func getCodeB() {
+        DemoAuthenticationProvider.getCodeB(
+            with: codeA,
+            accessToken: genericToken,
+            username: username,
+            accessTokenNetwork: accessTokenNetwork) { (codeB, error) in
+                if let error = error {
+                    print(error)
+                    self.genericAuthDone = false
+                } else {
+                    self.codeB = codeB
+                    self.completeSSO()
+                }
+        }
+    }
+    
+    private func completeSSO() {
+        authProvider.completeSSO(with: codeB) { (jwtToken, error) in
+            if let error = error {
+                print(error)
+                self.genericAuthDone = false
+            } else if jwtToken != nil {
+                self.genericAuthDone = true
+            } else {
+                self.genericAuthDone = false
+            }
+        }
+    }
+    
+    @IBAction func resetAllAuthentication(_ sender: Any) {
+        codeA = nil
+        codeB = nil
+        genericToken = nil
+        foxAuthDone = false
+        genericAuthDone = false
+        SPPublicSessionInterface.resetUser()
+    }
+}
+/// demo constants
+private extension String {
+    static var demoGenericSpotKeyForSSO:    String { return "sp_eCIlROSD" }
+    static var demoFoxSpotKeyForSSO:        String { return "sp_ANQXRpqH" }
+    static var demoFoxSecretForSSO:         String { return  "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5REEwNkVEMjAxOCIsInR5cCI6IkpXVCJ9.eyJwaWQiOiJ1cy1lYXN0LTE6YjJkZDYyMzEtZGZkNS00MDU4LWI1ZDAtNDE5YjcxM2U3MmQ1IiwidWlkIjoiWWpKa1pEWXlNekV0Wkdaa05TMDBNRFU0TFdJMVpEQXROREU1WWpjeE0yVTNNbVExIiwic2lkIjoiNDBhNDhkZTAtODAyMy00OWQzLWJhYjgtNTU4MjBiYzBhMWI1Iiwic2RjIjoidXMtZWFzdC0xIiwiYXR5cGUiOiJpZGVudGl0eSIsImR0eXBlIjoid2ViIiwidXR5cGUiOiJlbWFpbCIsImRpZCI6IiIsIm12cGRpZCI6IiIsInZlciI6MiwiZXhwIjoxNTkzNjgyNzU3LCJqdGkiOiIyOWRjNGM3OS1kZWQ0LTQzNGUtYjc2Ni1iMjkzODM4YzQwNGMiLCJpYXQiOjE1NjIxNDY3NTd9.jACKyPFVpZEIa5lM9hgNbYUZzim4dTbb8nxr9C6hxNtnPNORTihpkfMK9gkFAnTP0hnPClqZUL_n-IM1HzHVzISztKEK9MRsC3JlCCL122syhtunQQnWp5xXX-Rn8hl-wM8ars4PK2izoFfyDInd-dw55kkTo6NryW-lLWcwbZFxXneb7MMvjpcuGB9N_g27VsK1nneUEFMZI1HchZNQmBUGyFRaH6ZxQ9ehFqpGEMIobaw6oN-tKDTjXqfpuyEm0QjWYGWVFF8pwLp9hHGuey2GuyScyd7NBtP7DZ_3_MKSbJeGBqpxc0yiRzKtgumY76lZgiW1LL38EMtUWIbIMw"
+    }
+}
