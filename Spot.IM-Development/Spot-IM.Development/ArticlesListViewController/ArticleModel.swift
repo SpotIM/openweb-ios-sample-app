7a8e826fdfb3ec892dbf257bc17bb0a6d4a13817
Itay Dressler <itay.d@spot.im>
2019-08-15 01:26:41 +0300

    Added basic articles screen

---
+++ Spot.IM-Development/Spot-IM.Development/ArticlesListViewController/ArticleModel.swift
@@ -0,0 +1,33 @@
+//
+//  ArticleModel.swift
+//  Spot-IM.Development
+//
+//  Created by Itay Dressler on 14/08/2019.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+public struct Post: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case spotId, conversationId, publishedAt, extractData
+    }
+    
+    let spotId : String
+    let conversationId: String
+    let publishedAt: String
+    let extractData:Article
+}
+
+public struct Article: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case url, title, width, height, description, thumbnailUrl
+    }
+    
+    let url : String
+    let title: String
+    let width: Int
+    let height: Int
+    let description: String
+    let thumbnailUrl: String
+}
