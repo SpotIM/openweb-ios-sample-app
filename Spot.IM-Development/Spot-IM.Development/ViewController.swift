7aad170ac007a096b8f2948595b7fd766ee05540
Itay Dressler <itay.d@spot.im>
2019-09-15 00:53:18 +0300

    Added fox authentication implementation.

--- Spot.IM-Development/Spot-IM.Development/ViewController.swift
+++ Spot.IM-Development/Spot-IM.Development/ViewController.swift
@@ -41,66 +41,56 @@ class ViewController: UIViewController {
         logo.clipsToBounds = true
         logo.layer.cornerRadius = 8
     }
-
-    @IBAction private func showMainConversation(_ sender: UIButton) {
-        SPPublicSessionInterface.resetUser()
-        setup(with: .demoGenericSpotKeyForSSO, from: sender)
-        showArticles(with: .demoGenericSpotKeyForSSO)
-    }
     
-    @IBAction private func showFoxMainConversation(_ sender: UIButton) {
-        SPPublicSessionInterface.resetUser()
-        setup(with: .demoFoxSpotKeyForSSO, from: sender)
-
-        sender.setTitle("Authenticating to Fox ⌛️", for: .normal)
-
-        authProvider.startSSO(with: .demoFoxSecretForSSO, completion: { (response, error) in
-            sender.setTitle(self.loadingButtonTitleBackup, for: .normal)
-            if let error = error {
-                print(error)
-            } else {
-                self.showArticles(with: .demoFoxSpotKeyForSSO)
-            }
-        })
-    }
-
-    @IBAction private func showDemoSpotConversation(_ sender: UIButton) {
-        SPPublicSessionInterface.resetUser()
-        setup(with: .demoMainSpotKey, from: sender)
-        showArticles(with: .demoMainSpotKey)
-    }
-
     @IBAction private func showPreConversation(_ sender: UIButton) {
         SPPublicSessionInterface.resetUser()
         setup(with: .demoMainSpotKey, from: sender)
         performSegue(withIdentifier: "showPreConversationSegue", sender: self)
+
+        setSpotId(spotId: .demoGenericSpotKeyForSSO)
+        self.showArticles(with: .demoGenericSpotKeyForSSO, authenticationControllerId: .defaultAuthenticationControllerId)
+    }
+    
+    @IBAction private func showFoxMainConversation(_ sender: UIButton) {
+        setSpotId(spotId: .demoFoxSpotKeyForSSO)
+        self.showArticles(with: .demoFoxSpotKeyForSSO, authenticationControllerId: .foxAuthenticationControllerId)
     }
 
     @IBAction private func crashButtonTapped(_ sender: AnyObject) {
         Crashlytics.sharedInstance().crash()
     }
 
-    private func showArticles(with spotId: String) {
-        let controller = ArticlesListViewController(spotId: spotId)
+    private func showArticles(with spotId: String, authenticationControllerId: String) {
+        let controller = ArticlesListViewController(spotId: spotId, authenticationControllerId: authenticationControllerId)
         navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    private func setSpotId(spotId:String) {
+        let key = "spotIdKey"
+        if let lastSpot = UserDefaults.standard.value(forKey: key) as? String, lastSpot != spotId {
+            SPPublicSessionInterface.resetUser()
+        }
+        
+        UserDefaults.standard.setValue(spotId, forKey: key)
+        SPClientSettings.setup(spotKey: spotId)
     }
 
     private func setup(with spotId: String, from sender: UIButton) {
         loadingButtonTitleBackup = sender.titleLabel?.text
-        sender.setTitle("Loading configuration ⏳", for: .normal)
-
-        SPClientSettings.setup(spotKey: spotId)
 
         sender.setTitle(self.loadingButtonTitleBackup, for: .normal)
+        setSpotId(spotId: spotId)
     }
 }
 
 /// demo constants
 private extension String {
-    static var demoGenericSpotKeyForSSO:    String { return "sp_eCIlROSD" }
-    static var demoFoxSpotKeyForSSO:        String { return "sp_ANQXRpqH" }
-    static var demoMainSpotKey:             String { return "sp_ly3RvXf6" }
-    static var demoFoxSecretForSSO:         String { return  "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5REEwNkVEMjAxOCIsInR5cCI6IkpXVCJ9.eyJwaWQiOiJ1cy1lYXN0LTE6YjJkZDYyMzEtZGZkNS00MDU4LWI1ZDAtNDE5YjcxM2U3MmQ1IiwidWlkIjoiWWpKa1pEWXlNekV0Wkdaa05TMDBNRFU0TFdJMVpEQXROREU1WWpjeE0yVTNNbVExIiwic2lkIjoiNDBhNDhkZTAtODAyMy00OWQzLWJhYjgtNTU4MjBiYzBhMWI1Iiwic2RjIjoidXMtZWFzdC0xIiwiYXR5cGUiOiJpZGVudGl0eSIsImR0eXBlIjoid2ViIiwidXR5cGUiOiJlbWFpbCIsImRpZCI6IiIsIm12cGRpZCI6IiIsInZlciI6MiwiZXhwIjoxNTkzNjgyNzU3LCJqdGkiOiIyOWRjNGM3OS1kZWQ0LTQzNGUtYjc2Ni1iMjkzODM4YzQwNGMiLCJpYXQiOjE1NjIxNDY3NTd9.jACKyPFVpZEIa5lM9hgNbYUZzim4dTbb8nxr9C6hxNtnPNORTihpkfMK9gkFAnTP0hnPClqZUL_n-IM1HzHVzISztKEK9MRsC3JlCCL122syhtunQQnWp5xXX-Rn8hl-wM8ars4PK2izoFfyDInd-dw55kkTo6NryW-lLWcwbZFxXneb7MMvjpcuGB9N_g27VsK1nneUEFMZI1HchZNQmBUGyFRaH6ZxQ9ehFqpGEMIobaw6oN-tKDTjXqfpuyEm0QjWYGWVFF8pwLp9hHGuey2GuyScyd7NBtP7DZ_3_MKSbJeGBqpxc0yiRzKtgumY76lZgiW1LL38EMtUWIbIMw"
+    static var defaultAuthenticationControllerId:   String { return "AuthenticstionViewController" }
+    static var foxAuthenticationControllerId:       String { return "FoxAuthenticationViewController" }
+    static var demoGenericSpotKeyForSSO:            String { return "sp_eCIlROSD" }
+    static var demoFoxSpotKeyForSSO:                String { return "sp_ANQXRpqH" }
+    static var demoMainSpotKey:                     String { return "sp_ly3RvXf6" }
+    static var demoFoxSecretForSSO:                 String { return  "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5REEwNkVEMjAxOCIsInR5cCI6IkpXVCJ9.eyJwaWQiOiJ1cy1lYXN0LTE6YjJkZDYyMzEtZGZkNS00MDU4LWI1ZDAtNDE5YjcxM2U3MmQ1IiwidWlkIjoiWWpKa1pEWXlNekV0Wkdaa05TMDBNRFU0TFdJMVpEQXROREU1WWpjeE0yVTNNbVExIiwic2lkIjoiNDBhNDhkZTAtODAyMy00OWQzLWJhYjgtNTU4MjBiYzBhMWI1Iiwic2RjIjoidXMtZWFzdC0xIiwiYXR5cGUiOiJpZGVudGl0eSIsImR0eXBlIjoid2ViIiwidXR5cGUiOiJlbWFpbCIsImRpZCI6IiIsIm12cGRpZCI6IiIsInZlciI6MiwiZXhwIjoxNTkzNjgyNzU3LCJqdGkiOiIyOWRjNGM3OS1kZWQ0LTQzNGUtYjc2Ni1iMjkzODM4YzQwNGMiLCJpYXQiOjE1NjIxNDY3NTd9.jACKyPFVpZEIa5lM9hgNbYUZzim4dTbb8nxr9C6hxNtnPNORTihpkfMK9gkFAnTP0hnPClqZUL_n-IM1HzHVzISztKEK9MRsC3JlCCL122syhtunQQnWp5xXX-Rn8hl-wM8ars4PK2izoFfyDInd-dw55kkTo6NryW-lLWcwbZFxXneb7MMvjpcuGB9N_g27VsK1nneUEFMZI1HchZNQmBUGyFRaH6ZxQ9ehFqpGEMIobaw6oN-tKDTjXqfpuyEm0QjWYGWVFF8pwLp9hHGuey2GuyScyd7NBtP7DZ_3_MKSbJeGBqpxc0yiRzKtgumY76lZgiW1LL38EMtUWIbIMw"
     }
 }
 
---
+++ Spot.IM-Development/Spot-IM.Development/ViewControllers/FoxAuthenticationViewController.swift
@@ -0,0 +1,120 @@
+//
+//  FoxAuthenticationViewController.swift
+//  Spot-IM.Development
+//
+//  Created by Itay Dressler on 14/09/2019.
+//  Copyright © 2019 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import Spot_IM_Core
+
+enum FoxError: Error {
+    case runtimeError(String)
+}
+
+class FoxAuthenticationViewController: UIViewController, SSOAuthenticatable {
+    
+    var ssoAuthProvider: SPAuthenticationProvider = SPDefaultAuthProvider()
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Authentication"
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        guard let email = self.emailTextfield.text, let password = passwordTextField.text else {
+            return
+        }
+        
+        self.authenticate(email: email, password: password) {[weak self] (token, error) in
+            DispatchQueue.main.sync {
+                guard let token = token else {
+                    self?.alert(text: error ?? "No description")
+                    return
+                }
+                
+                self?.authenticateWithSpotIm(token: token)
+            }
+        }
+    }
+    
+    private func alert(text:String) {
+        let alert = UIAlertController(title: "Failed authenticating with Fox", message: text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+           }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func authenticateWithSpotIm(token:String) {
+        ssoAuthProvider.startSSO(with: token, completion: { [weak self] response, error in
+            if let success = response?.success, success {
+                print("Authentication successful!")
+                self?.ssoAuthProvider.ssoAuthDelegate?.ssoFlowDidSucceed()
+            } else {
+                print("Authentication error:\n\(String(describing: error))")
+                self?.ssoAuthProvider.ssoAuthDelegate?.ssoFlowDidFail(with: error)
+            }
+        })
+    }
+    
+    @IBAction func createAccountTapped(_ sender: Any) {
+        if let url = URL(string: "https://my.foxnews.com/?p=create-account") {
+            UIApplication.shared.open(url)
+        }
+    }
+    private func authenticate(email:String, password:String,  completion: @escaping (_ token: String?, _ error: String?) -> ()) {
+        
+        let parameters = ["email": email, "password": password]
+        
+        let url = URL(string: "https://api2.fox.com/v2.0/login")!
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.setValue("vHeTnXOe984VBvC0ud8lPpSbsxJ0c7kQ", forHTTPHeaderField: "x-api-key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+            
+            guard error == nil else {
+                completion(nil, nil)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, nil)
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    if let token = json["accessToken"] as? String {
+                        completion(token, nil)
+                        return
+                    }
+                    
+                    if let type = json["@type"] as? String, type == "Error", let detail = json["detail"] as? String{
+                        completion(nil, detail)
+                        return
+                    }
+                  
+                }
+                completion(nil, nil)
+            } catch let error {
+                completion(nil, error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
+}
