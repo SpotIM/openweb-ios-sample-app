fc1df82ebdcdf51e7ef37c61eb8bcced9d73b593
Itay Dressler <itay.d@spot.im>
2019-09-15 19:27:49 +0300

    Fixed crash with deleted IBOutlets.

--- Spot.IM-Development/Spot-IM.Development/ViewController.swift
+++ Spot.IM-Development/Spot-IM.Development/ViewController.swift
@@ -42,6 +42,17 @@ class ViewController: UIViewController {
         logo.layer.cornerRadius = 8
     }
     
+    @IBAction private func showMainConversation(_ sender: UIButton) {
+        setup(with: .demoGenericSpotKeyForSSO, from: sender)
+        showArticles(with: .demoGenericSpotKeyForSSO, authenticationControllerId: .defaultAuthenticationControllerId)
+    }
+    
+    @IBAction private func showDemoSpotConversation(_ sender: UIButton) {
+        setup(with: .demoMainSpotKey, from: sender)
+        showArticles(with: .demoMainSpotKey, authenticationControllerId: .defaultAuthenticationControllerId)
+    }
+
+    
     @IBAction private func showPreConversation(_ sender: UIButton) {
         SPPublicSessionInterface.resetUser()
         setup(with: .demoMainSpotKey, from: sender)
@@ -77,9 +88,8 @@ class ViewController: UIViewController {
 
     private func setup(with spotId: String, from sender: UIButton) {
         loadingButtonTitleBackup = sender.titleLabel?.text
-
-        sender.setTitle(self.loadingButtonTitleBackup, for: .normal)
         setSpotId(spotId: spotId)
+        sender.setTitle(self.loadingButtonTitleBackup, for: .normal)
     }
 }
 
@@ -90,7 +100,5 @@ private extension String {
     static var demoGenericSpotKeyForSSO:            String { return "sp_eCIlROSD" }
     static var demoFoxSpotKeyForSSO:                String { return "sp_ANQXRpqH" }
     static var demoMainSpotKey:                     String { return "sp_ly3RvXf6" }
-    static var demoFoxSecretForSSO:                 String { return  "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5REEwNkVEMjAxOCIsInR5cCI6IkpXVCJ9.eyJwaWQiOiJ1cy1lYXN0LTE6YjJkZDYyMzEtZGZkNS00MDU4LWI1ZDAtNDE5YjcxM2U3MmQ1IiwidWlkIjoiWWpKa1pEWXlNekV0Wkdaa05TMDBNRFU0TFdJMVpEQXROREU1WWpjeE0yVTNNbVExIiwic2lkIjoiNDBhNDhkZTAtODAyMy00OWQzLWJhYjgtNTU4MjBiYzBhMWI1Iiwic2RjIjoidXMtZWFzdC0xIiwiYXR5cGUiOiJpZGVudGl0eSIsImR0eXBlIjoid2ViIiwidXR5cGUiOiJlbWFpbCIsImRpZCI6IiIsIm12cGRpZCI6IiIsInZlciI6MiwiZXhwIjoxNTkzNjgyNzU3LCJqdGkiOiIyOWRjNGM3OS1kZWQ0LTQzNGUtYjc2Ni1iMjkzODM4YzQwNGMiLCJpYXQiOjE1NjIxNDY3NTd9.jACKyPFVpZEIa5lM9hgNbYUZzim4dTbb8nxr9C6hxNtnPNORTihpkfMK9gkFAnTP0hnPClqZUL_n-IM1HzHVzISztKEK9MRsC3JlCCL122syhtunQQnWp5xXX-Rn8hl-wM8ars4PK2izoFfyDInd-dw55kkTo6NryW-lLWcwbZFxXneb7MMvjpcuGB9N_g27VsK1nneUEFMZI1HchZNQmBUGyFRaH6ZxQ9ehFqpGEMIobaw6oN-tKDTjXqfpuyEm0QjWYGWVFF8pwLp9hHGuey2GuyScyd7NBtP7DZ_3_MKSbJeGBqpxc0yiRzKtgumY76lZgiW1LL38EMtUWIbIMw"
-    }
 }
 
