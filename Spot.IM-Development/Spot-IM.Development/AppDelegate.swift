f577f720ab2c0c1b4b29317b692dc6f7716d417f
Eugene Pravda <>
2019-09-27 13:06:03 +0300

    add all apropriate files and identificators

--- Spot.IM-Development/Spot-IM.Development/AppDelegate.swift
+++ Spot.IM-Development/Spot-IM.Development/AppDelegate.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 import Spot_IM_Core
 import FirebaseCore
 
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        Fabric.with([Crashlytics.self])
         
         return true
     }
