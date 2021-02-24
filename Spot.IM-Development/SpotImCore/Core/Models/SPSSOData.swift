81b4b31680a9faaefe9be0839f94a848a1416931
Alon Shprung <alon@cherrypick-consulting.com>
2021-02-24 15:48:56 +0200

    add sso data to user model

---
+++ Spot.IM-Development/SpotImCore/Core/Models/SPSSOData.swift
@@ -0,0 +1,13 @@
+//
+//  SPSSOData.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 24/02/2021.
+//  Copyright © 2021 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+internal struct SPSSOData: Codable {
+    var isSubscriber: Bool
+}
