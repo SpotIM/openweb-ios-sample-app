24930aa9affd780fcfba2e43644a17f12d20a7af
Alon Shprung <alon@cherrypick-consulting.com>
2021-03-17 11:27:49 +0200

    open profile web module

---
+++ Spot.IM-Development/SpotImCore/Core/Common/Extensions/URL+QueryParams.swift
@@ -0,0 +1,32 @@
+//
+//  URL+QueryParams.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 16/03/2021.
+//  Copyright © 2021 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+
+    mutating func appendQueryParam(name: String, value: String?) {
+
+        guard var urlComponents = URLComponents(string: absoluteString) else { return }
+
+        // Create array of existing query items
+        var queryItems: [URLQueryItem] = urlComponents.queryItems ??  []
+
+        // Create query item
+        let queryItem = URLQueryItem(name: name, value: value)
+
+        // Append the new query item in the existing query items array
+        queryItems.append(queryItem)
+
+        // Append updated query items array in the url component object
+        urlComponents.queryItems = queryItems
+
+        // Returns the url from new url components
+        self = urlComponents.url!
+    }
+}
