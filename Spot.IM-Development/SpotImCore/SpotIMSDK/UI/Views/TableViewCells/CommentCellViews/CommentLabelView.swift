ae57a773b82fa1fa237a4ebf82fb9dc36fc42d8c
Nogah <nogah@cherrypick-consulting.com>
2021-04-11 17:55:18 +0300

    convert rgb to hex

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/TableViewCells/CommentCellViews/CommentLabelView.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/TableViewCells/CommentCellViews/CommentLabelView.swift
@@ -14,33 +14,32 @@ internal final class CommentLabelView: BaseView {
     private let iconImageView: BaseUIImageView = .init()
     private let label: BaseLabel = .init()
     
-    private var iconUrl: String? = nil
-    private var labelText: String = "label text"
     private var commentLabelColor: UIColor = .clear
     private var state: LabelState = .hidden
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let colorString = "00ab5e"
-        commentLabelColor = UIColor.color(with: colorString) ?? .clear
+//        let colorString = "red"
+//        commentLabelColor = UIColor.color(with: colorString) ?? .clear
         setupUI()
-//        iconUrl = "https://images.spot.im/image/upload/f_png/e_colorize,co_rgb:\(colorString)/font-awesome/\(iconType)-\(iconName).png"
-//        let url: URL = URL(string: iconUrl!)!
-//        setImage(with: url)
     }
     
-    func setLabel(iconName: String?, iconType: String?, hexColor: String?, labelText: String?) {
-        if let iconName = iconName, let iconType = iconType, let hexColor = hexColor, let labelText = labelText {
-            let hexColorTemp = "00ab5e"
-            self.commentLabelColor = UIColor.color(with: hexColorTemp) ?? .clear
-            self.labelText = labelText
+    func setLabel(iconName: String?, iconType: String?, rgbColor: String?, labelText: String?) {
+        if let iconName = iconName, let iconType = iconType, let rgbColor = rgbColor, let labelText = labelText {
+            let hexColor = UIColor.rgbToHex(with: rgbColor) ?? "blue"
+            self.commentLabelColor = UIColor.color(with: hexColor) ?? .clear
             // TODO: color to hex from rgb
-            iconUrl = "https://images.spot.im/image/upload/f_png/e_colorize,co_rgb:\(hexColorTemp)/font-awesome/\(iconType)-\(iconName).png"
-            let url: URL = URL(string: iconUrl!)!
+            let iconUrl = "https://images.spot.im/image/upload/f_png/e_colorize,co_rgb:\(hexColor)/font-awesome/\(iconType)-\(iconName).png"
+            let url: URL = URL(string: iconUrl)!
+            // update UI
             setImage(with: url)
+            labelContainer.backgroundColor = commentLabelColor.withAlphaComponent(0.1)
+            label.textColor = commentLabelColor
+            label.text = labelText
         }
     }
     
+    // TODO
     func setState(state: LabelState) {
         switch state {
             case .hidden:
@@ -60,13 +59,12 @@ internal final class CommentLabelView: BaseView {
     
     private func configureLabelContainer() {
         labelContainer.addSubviews(iconImageView, label)
-        labelContainer.backgroundColor = commentLabelColor.withAlphaComponent(0.1)
+//        labelContainer.backgroundColor = commentLabelColor.withAlphaComponent(0.1)
         labelContainer.layer.cornerRadius = 3
         labelContainer.pinEdges(to: self)
     }
     
     private func configureIconImageView() {
-        iconImageView.backgroundColor = .spAvatarBG
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.backgroundColor = .clear
         iconImageView.clipsToBounds = true
@@ -81,8 +79,8 @@ internal final class CommentLabelView: BaseView {
     
     private func configureLabel() {
         label.font = .preferred(style: .medium, of: Theme.fontSize)
-        label.textColor = commentLabelColor
-        label.text = labelText
+//        label.textColor = .red //commentLabelColor
+//        label.text = labelText
         label.layout {
             $0.top.equal(to: labelContainer.topAnchor, offsetBy: 5)
             $0.bottom.equal(to: labelContainer.bottomAnchor, offsetBy: -5)
