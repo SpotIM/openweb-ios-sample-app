eb438ad6e206dc97eced88a309dfc954132a8a41
Oded Regev <oded@cherrypick-consulting.com>
2021-04-12 14:00:07 +0300

    add the same app state logic to full-conv

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/ViewControllers/MainConversation/SPMainConversationViewController.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/ViewControllers/MainConversation/SPMainConversationViewController.swift
@@ -122,15 +122,20 @@ final class SPMainConversationViewController: SPBaseConversationViewController, 
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
+        let state = UIApplication.shared.applicationState
         if #available(iOS 12.0, *) {
             if previousTraitCollection?.userInterfaceStyle != self.traitCollection.userInterfaceStyle {
-                print("oded - reloadData \(self.tableView)")
+                // traitCollectionDidChange() is called multiple times, see: https://stackoverflow.com/a/63380259/583425
+                if state != .background {
+                    self.tableView.reloadData()
+                    self.updateColorsAccordingToStyle()
+                }
+            }
+        } else {
+            if state != .background {
                 self.tableView.reloadData()
                 self.updateColorsAccordingToStyle()
             }
-        } else {
-            self.tableView.reloadData()
-            self.updateColorsAccordingToStyle()
         }
     }
 
