b22028f40a8b267916e55a347119de932618363f
Nogah <nogah@cherrypick-consulting.com>
2021-04-21 11:38:03 +0300

    set default labels from confix

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/SPCommentLabelsContainerView.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/SPCommentLabelsContainerView.swift
@@ -13,63 +13,63 @@ internal final class SPCommentLabelsContainerView: BaseView, UIGestureRecognizer
     
     var container: BaseStackView = .init()
     var labelsViews: [CommentLabelView] = .init()
-    var guidelineText: BaseLabel = .init()
-    var maxLabels: Int
+    var guidelineTextLabel: BaseLabel = .init()
+    var maxLabels: Int = 0
     var selectedLabelsIds: [String] = .init()
 
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setupUI()
-//    }
-    
-    init(labels: [CommentLabel], guidelineText: String, maxLabels: Int) {
-        self.maxLabels = maxLabels
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI(labels: labels, guidelineText: guidelineText)
+        setupUI()
     }
     
-    private func setupUI(labels: [CommentLabel], guidelineText: String) {
-        addSubviews(self.guidelineText, container)
-        configureGuidelineText(text: guidelineText)
-        configureLabels(labels: labels)
-        configureLabelsContainer()
-    }
-    
-    private func configureGuidelineText(text: String) {
-        guidelineText.text = text
-        guidelineText.textColor = .steelGrey
-        guidelineText.font = UIFont.preferred(style: .medium, of: 13.0)
-        guidelineText.layout {
-            $0.top.equal(to: topAnchor)
-            $0.leading.equal(to: leadingAnchor)
-            $0.trailing.equal(to: trailingAnchor)
-        }
-    }
-    
-    private func configureLabels(labels: [CommentLabel]) {
-        labels.forEach { label in
-            let labelView = CommentLabelView()
-            labelView.setLabel(commentLabelIconUrl: label.iconUrl, labelColor: label.color, labelText: label.text, labelId: label.id, state: .notSelected)
-        }
-    }
-    
-    private func configureLabelsContainer() {
-        container.axis = .horizontal
-        container.alignment = .leading
-        container.distribution = .fillEqually
+    func setLabelsContainer(labels: [CommentLabel], guidelineText: String, maxLabels: Int) {
+        self.maxLabels = maxLabels
+        guidelineTextLabel.text = guidelineText
         
-        container.spacing = 10
+        setLabels(labels: labels)
+        
         labelsViews.forEach { label in
             container.addArrangedSubview(label)
             let recognizer = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
             recognizer.delegate = self
             label.addGestureRecognizer(recognizer)
         }
-
-        container.layout {
-            $0.top.equal(to: guidelineText.bottomAnchor, offsetBy: 10.0)
+    }
+    
+    private func setupUI() {
+        addSubviews(guidelineTextLabel, container)
+        configureGuidelineText()
+        configureLabelsContainer()
+    }
+    
+    private func configureGuidelineText() {
+        guidelineTextLabel.textColor = .steelGrey
+        guidelineTextLabel.font = UIFont.preferred(style: .medium, of: 13.0)
+        guidelineTextLabel.layout {
+            $0.top.equal(to: topAnchor)
             $0.leading.equal(to: leadingAnchor)
             $0.trailing.equal(to: trailingAnchor)
+        }
+    }
+    
+    private func setLabels(labels: [CommentLabel]) {
+        labels.forEach { label in
+            let labelView = CommentLabelView()
+            labelView.setLabel(commentLabelIconUrl: label.iconUrl, labelColor: label.color, labelText: label.text, labelId: label.id, state: .notSelected)
+            labelsViews.append(labelView)
+        }
+    }
+    
+    private func configureLabelsContainer() {
+        container.axis = .horizontal
+        container.alignment = .leading
+        container.distribution = .equalSpacing
+        container.spacing = 10
+
+        container.layout {
+            $0.top.equal(to: guidelineTextLabel.bottomAnchor, offsetBy: 10.0)
+            $0.leading.equal(to: leadingAnchor)
+            $0.trailing.lessThanOrEqual(to: trailingAnchor)
             $0.height.equal(to: 28.0)
         }
     }
