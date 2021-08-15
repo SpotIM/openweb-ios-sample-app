1290b85d6a084cdb3e6b3f2b51355cd9c17b0723
Oded Regev <oded@cherrypick-consulting.com>
2021-08-15 14:34:16 +0300

    d

--- Spot.IM-Development/commit_and_push_changes.sh
+++ Spot.IM-Development/commit_and_push_changes.sh
@@ -13,8 +13,5 @@ git config --global user.email "odedre@gmail.com"
 git config --global user.name "Oded Regev via CircleCI"
 git commit -m "CircleCI update to version $RELEASE_VERSION [skip ci]"
 git tag $RELEASE_VERSION
-git remote -v
-git remote set-url origin git@odedre.bitbucket.org:zencitytech/zencity-ios-app.git
-git remote -v
 git push origin $CIRCLE_BRANCH
 git push origin --tags
