20ef534bcfc13e099569cd14d9af2911ea5b69d1
Alon Shprung <alon@cherrypick-consulting.com>
2021-10-14 11:11:51 +0300

    upload image to cloudinary + add image to comment + content validation

--- Spot.IM-Development/SpotImCore/Core/Network/Requests/SPCloudinaryRequests.swift
+++ Spot.IM-Development/SpotImCore/Core/Network/Requests/SPCloudinaryRequests.swift
@@ -11,22 +11,30 @@ import Alamofire
 
 internal enum SPCloudinaryRequests: SPRequest {
     case fetchImage(url: URL)
+    case login
+    case upload
 
     internal var method: HTTPMethod {
         switch self {
         case .fetchImage: return .get
+        case .login: return .post
+        case .upload: return .post
         }
     }
 
     internal var pathString: String {
         switch self {
         case .fetchImage: return ""
+        case .login: return "/extract/cloudinary/sign"
+        case .upload: return "/image/upload"
         }
     }
 
     internal var url: URL! {
         switch self {
         case .fetchImage(let url): return url
+        case .login: return URL(string: APIConstants.spotimGatewayApiUrlString.appending(pathString))
+        case .upload: return URL(string: APIConstants.cloudinaryUploadBaseURL.appending(pathString))
         }
     }
 }
