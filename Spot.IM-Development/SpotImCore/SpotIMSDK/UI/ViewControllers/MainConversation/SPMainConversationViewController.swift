978a19bc7fe7cfa727668aa0a7a733ee1b8dbb4d
Tomer Ben-Rachel <tomer.br@openweb.com>
2021-11-25 14:46:39 +0200

    feature/MOBILE-1425-Edit: Logic to handle edit option pressed. Added editComment to SPCommentsCreationDelegate

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/ViewControllers/MainConversation/SPMainConversationViewController.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/ViewControllers/MainConversation/SPMainConversationViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 internal protocol SPCommentsCreationDelegate: AnyObject {
     func createComment(with dataModel: SPMainConversationModel)
     func createReply(with dataModel: SPMainConversationModel, to id: String)
+    func editComment(with dataModel: SPMainConversationModel, to id: String)
 }
 
 final class SPMainConversationViewController: SPBaseConversationViewController, UserPresentable {
