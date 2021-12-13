11b46602307913e5a59e7f1de4434ccde87bf15f
Alon Haiut <alon.h@openweb.com>
2021-12-13 17:24:17 +0200

    Removed old files after name changing

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/OnlineUsersViewingCounter/Views/OnlineUsersViewingCounterView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OnlineUsersViewingCounterView.swift
-//  SpotImCore
-//
-//  Created by Alon Haiut on 13/12/2021.
-//  Copyright © 2021 Spot.IM. All rights reserved.
-//
-
-import Foundation
