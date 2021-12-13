98dc5411fbf510bdc025e8e9fb5da56f51019964
Alon Haiut <alon.h@openweb.com>
2021-12-13 17:06:22 +0200

    Added the view and the view model of online users viewing counter. VM almost done

---
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/OnlineUsersViewingCounter/Views/OnlineUsersViewingCounterView.swift
@@ -0,0 +1,9 @@
+//
+//  OnlineUsersViewingCounterView.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 13/12/2021.
+//  Copyright © 2021 Spot.IM. All rights reserved.
+//
+
+import Foundation
