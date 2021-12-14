df18e1f6e6320ac11dd4ee92f5d9619eb5061587
Alon Haiut <alon.h@openweb.com>
2021-12-14 13:54:28 +0200

    Added the viewing users counter view to the header

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/PreConversationHeader/SPPreConversationHeaderView.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/PreConversationHeader/SPPreConversationHeaderView.swift
@@ -16,16 +16,20 @@ internal final class SPPreConversationHeaderView: BaseView {
     
     private lazy var titleLabel: BaseLabel = {
         let lbl = BaseLabel()
-        lbl.font = UIFont.preferred(style: .bold, of: Theme.titleFontSize)
+        lbl.font = UIFont.preferred(style: .bold, of: Metrics.titleFontSize)
         lbl.textColor = .spForeground0
         return lbl
     }()
     
     private lazy var counterLabel: BaseLabel = {
         let lbl = BaseLabel()
-        lbl.font = UIFont.preferred(style: .regular, of: Theme.counterFontSize)
+        lbl.font = UIFont.preferred(style: .regular, of: Metrics.counterFontSize)
         lbl.textColor = .spForeground1
         return lbl
+    }()
+    
+    private lazy var onlineViewingUsersView: OWOnlineViewingUsersCounterView = {
+       return OWOnlineViewingUsersCounterView()
     }()
     
     internal weak var delegate: SPPreConversationHeaderViewDelegate?
@@ -60,25 +64,38 @@ internal final class SPPreConversationHeaderView: BaseView {
         }
         updateCustomUI()
     }
+    
+    // Idealy this header view will have a VM as well which will hold the online users VM
+    // I decided to wait until we will choose if to use RxSwift or Combine and then I will refactor it
+    // The delegate flow for updating the custom UI here is anti patteren which will also be refactor soon. Prevented me from creating a VM file at the current state because it will be too much boilerplate code
+    func configure(onlineViewingUsersVM: OWOnlineViewingUsersCounterViewModeling) {
+        onlineViewingUsersView.configure(with: onlineViewingUsersVM)
+    }
 
     private func setupUI() {
         self.addSubview(titleLabel)
         titleLabel.layout {
             $0.centerY.equal(to: centerYAnchor)
-            $0.leading.equal(to: leadingAnchor, offsetBy: Theme.margins.left)
+            $0.leading.equal(to: leadingAnchor, offsetBy: Metrics.margins.left)
         }
         
         self.addSubview(counterLabel)
         counterLabel.layout {
             $0.firstBaseline.equal(to: titleLabel.firstBaselineAnchor)
-            $0.leading.equal(to: titleLabel.trailingAnchor, offsetBy: Theme.counterLeading)
+            $0.leading.equal(to: titleLabel.trailingAnchor, offsetBy: Metrics.counterLeading)
             $0.trailing.lessThanOrEqual(to: trailingAnchor)
+        }
+        
+        self.addSubview(onlineViewingUsersView)
+        onlineViewingUsersView.layout {
+            $0.centerY.equal(to: titleLabel.centerYAnchor)
+            $0.trailing.equal(to: trailingAnchor, offsetBy: -Metrics.margins.right)
         }
     }
 }
 
 private extension SPPreConversationHeaderView {
-    private enum Theme {
+    private enum Metrics {
         static let counterLeading: CGFloat = 5
         static let titleFontSize: CGFloat = 25
         static let counterFontSize: CGFloat = 16
