30e080846ccccbbaffcb7fd22ad397aced8f2175
Alon Haiut <alon.h@openweb.com>
2021-12-14 16:57:42 +0200

    Added that code back for dark mode changes

--- Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/PreConversationHeader/SPPreConversationHeaderView.swift
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/PreConversationHeader/SPPreConversationHeaderView.swift
@@ -42,6 +42,9 @@ internal final class SPPreConversationHeaderView: BaseView {
     
     // Handle dark mode \ light mode change
     func updateColorsAccordingToStyle() {
+        backgroundColor = .spBackground0
+        titleLabel.textColor = .spForeground0
+        counterLabel.textColor = .spForeground1
         updateCustomUI()
     }
     
