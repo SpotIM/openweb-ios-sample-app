9bd87af6d2550299624f35679f2e05c30463b6a6
Tomer Ben-Rachel <tomer.br@openweb.com>
2022-01-27 08:31:54 +0200

    feature/MOBILE-1538-subscriber-badge: Creating OWUserSubscriberBadgeVM

---
+++ Spot.IM-Development/SpotImCore/SpotIMSDK/UI/Views/UserSubscriberBadge/ViewModels/OWUserSubscriberBadgeViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  OWUserSubscriberBadgeViewModel.swift
+//  SpotImCore
+//
+//  Created by Tomer Ben Rachel on 26/01/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol OWUserSubscriberBadgeViewModelingInputs {
+    func configureModel(_ model: OWSubscriberBadge)
+}
+
+protocol OWUserSubscriberBadgeViewModelingOutputs {
+    var image: UIImage { get }
+}
+
+protocol OWUserSubscriberBadgeViewModeling {
+    var inputs: OWUserSubscriberBadgeViewModelingInputs { get }
+    var outputs: OWUserSubscriberBadgeViewModelingOutputs { get }
+}
+
+class OWUserSubscriberBadgeViewModel: OWUserSubscriberBadgeViewModeling,
+                                      OWUserSubscriberBadgeViewModelingInputs,
+                                      OWUserSubscriberBadgeViewModelingOutputs {
+
+    var inputs: OWUserSubscriberBadgeViewModelingInputs { return self }
+    var outputs: OWUserSubscriberBadgeViewModelingOutputs { return self }
+    
+    fileprivate var model = BehaviorSubject<OWSubscriberBadge?>(value: nil)
+    
+    fileprivate let subscriberBadgeBaseUrl: String = "\(APIConstants.fetchImageBaseURL) \(SPImageRequestConstants.cloudinaryIconParamString) \(SPImageRequestConstants.iconPathComponent)"
+    
+    var iconUrl: Observable<URL?> {
+        return model
+            .unwrap()
+            .map {
+                return URL(
+                    string: "\(self.subscriberBadgeBaseUrl) \($0.type) - \($0.name) .png"
+                    )
+            }
+    }
+    
+    init (_ model: OWSubscriberBadge) {
+        configureModel(model)
+    }
+    
+    lazy var image: UIImage = {
+        return UIImage()
+    }()
+    
+    func configureModel(_ model: OWSubscriberBadge) {
+        self.model.onNext(model)
+    }
+    
+}
