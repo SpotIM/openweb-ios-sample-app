45cce971ef4c8cf8b9127aaaa9d049da60b0ae7e
Alon Haiut <alon.h@openweb.com>
2022-02-01 16:39:45 +0200

    Added enforceSemanticAttribute

---
+++ Spot.IM-Development/SpotImCore/Extensions/UIView+SemanticAttribute.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+SemanticAttribute.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 01/02/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @discardableResult func enforceSemanticAttribute() -> Self {
+        self.semanticContentAttribute = LocalizationManager.currentLanguage?.customSemanticAttribute
+        ?? self.semanticContentAttribute
+        
+        return self
+    }
+}
