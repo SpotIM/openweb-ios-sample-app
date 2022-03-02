7de5b5f91134946ea1a8db6de3dc1588976a256c
Nogah <nogah@cherrypick-consulting.com>
2022-03-02 09:36:41 +0200

    use SPUser instead of SPCommentUser

--- Spot.IM-Development/SpotImCore/UI/ViewControllers/CommentCreation/GenericViewController/SPCommentCreationModel.swift
+++ Spot.IM-Development/SpotImCore/UI/ViewControllers/CommentCreation/GenericViewController/SPCommentCreationModel.swift
@@ -151,10 +151,10 @@ class SPCommentCreationModel {
         let userId = SPUserSessionHolder.session.user?.id
         responseData.userId = userId
         
-        if let userId = responseData.userId {
-            let user = SPComment.CommentUser(id: userId)
-            responseData.users = [userId: user]
-        }
+//        if let userId = responseData.userId {
+//            let user = SPComment.CommentUser(id: userId)
+//            responseData.users = [userId: user]
+//        }
         
         if let labels = self.selectedLabels {
             let commentLabels = SPComment.CommentLabel(section: labels.section, ids: labels.ids)
