b7777afdc9856d462bdeb0e18649d28ec7abf2ce
Alon Shprung <alon@cherrypick-consulting.com>
2022-03-03 11:14:00 +0200

    rename

--- Spot.IM-Development/SpotImCore/UI/ViewControllers/CommentCreation/GenericViewController/SPCommentCreationModel.swift
+++ Spot.IM-Development/SpotImCore/UI/ViewControllers/CommentCreation/GenericViewController/SPCommentCreationModel.swift
@@ -178,7 +178,7 @@ class SPCommentCreationModel {
     
     func updateCommentText(_ text: String) {
         commentText = text
-        saveCommentInCache()
+        saveCommentTextInCache()
     }
     
     func isCommentAReply() -> Bool {
@@ -189,7 +189,7 @@ class SPCommentCreationModel {
         return dataModel.editModel != nil
     }
     
-    private func saveCommentInCache() {
+    private func saveCommentTextInCache() {
         guard !isInEditMode() else { return } // do not save edited message in cache
         let commentIdentifier: String = getCommentIdentifierForCommentType()
         cacheService.update(comment: commentText, with: commentIdentifier)
