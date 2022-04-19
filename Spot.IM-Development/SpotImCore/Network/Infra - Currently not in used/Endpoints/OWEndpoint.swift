815d533565162f542c41935d152b5fa674f050c1
Alon Haiut <alon.h@openweb.com>
2022-04-19 21:08:05 +0300

    Most of the infra for the network is pretty much done. I still need to finish the http headers injection and stuff like the logger.

---
+++ Spot.IM-Development/SpotImCore/Network/Infra - Currently not in used/Endpoints/OWEndpoint.swift
@@ -0,0 +1,27 @@
+//
+//  OWEndpoint.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 19/04/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/*
+ OWEndpoint - protocol which represent the necessary stuff related to an endpoint
+ All endpoints will conform to it
+ */
+protocol OWEndpoint {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+    var overrideBaseURL: URL? { get }
+}
+
+extension OWEndpoint {
+    var overrideBaseURL: URL? {
+        return nil
+    }
+}
---
+++ Spot.IM-Development/SpotImCore/Network/Infra - Currently not in used/Middleware/Loggers/OWRequestLogger.swift
@@ -0,0 +1,22 @@
+//
+//  OWRequestLogger.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 19/04/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+class OWRequestLogger: OWNetworkLogging, OWRequestMiddleware {
+    func process(request: URLRequest) -> URLRequest {
+        if let url = request.url{
+            log(output: "Network send request: \(url)")
+        }
+        return request
+    }
+
+    func log(output: String) {
+        // TODO: Complete the logger. Process function will probably be changed based on the log level
+    }
+}
