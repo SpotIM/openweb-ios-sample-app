e34df1da4f865a5676dc1819658d7b1d88a6a3a0
Alon Haiut <alon.h@openweb.com>
2022-04-19 21:08:05 +0300

    Most of the infra for the network is pretty much done. I still need to finish the http headers injection and stuff like the logger.

---
+++ Spot.IM-Development/SpotImCore/Network/Infra - Currently not in used/Environment/OWEnvironment.swift
@@ -0,0 +1,40 @@
+//
+//  OWEnvironment.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 19/04/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+/*
+ OWEnvironmentProtocol - configuration protocol which present the environment we are working on
+ */
+protocol OWEnvironmentProtocol {
+    var scheme: String { get }
+    var domain: String { get }
+    var baseURL: URL { get }
+}
+
+class OWEnvironment: OWEnvironmentProtocol {
+    let scheme: String
+    let domain: String
+
+    static var currentEnvironment: OWEnvironmentProtocol!
+    static var production = OWEnvironment(scheme: "https", domain: "mobile-gw.spot.im")
+
+    static func set(environment: OWEnvironmentProtocol) {
+        currentEnvironment = environment
+    }
+
+    init(scheme: String, domain: String) {
+        self.scheme = scheme
+        self.domain = domain
+    }
+
+    lazy var baseURL: URL = {
+        let urlString = "\(scheme)://\(domain)"
+        return URL(string: urlString)!
+    }()
+}
---
+++ Spot.IM-Development/SpotImCore/Network/Infra - Currently not in used/Middleware/Loggers/OWNetworkLogging.swift
@@ -0,0 +1,16 @@
+//
+//  OWNetworkLogging.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 19/04/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+/*
+ OWNetworkLogging - protocol for logging network related stuff
+ */
+protocol OWNetworkLogging {
+    func log(output: String)
+}
