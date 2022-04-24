fe47ec80010b081663bd5ff92e338d4a3a3081e7
Alon Shprung <alon@cherrypick-consulting.com>
2022-04-24 14:36:42 +0300

    add SPViewActionsCallbacks

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/SPViewActionsCallbacks.swift
@@ -0,0 +1,35 @@
+//
+//  SPViewActionsCallbacks.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 20/04/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+public typealias SPViewActionsCallbacks = (SPViewActionCallbackType, SPViewSourceType, String) -> Void
+
+public enum SPViewSourceType {
+    case preConversation
+    case conversation
+    case createComment
+    case login
+    
+    public var description: String {
+        switch self {
+        case .preConversation:
+            return "Pre-Conversation screen"
+        case .conversation:
+            return "Conversation screen"
+        case .createComment:
+            return "Create Comment screen"
+        case .login:
+            return "Login screen"
+        }
+    }
+}
+
+public enum SPViewActionCallbackType {
+    case articleHeaderPressed
+}
