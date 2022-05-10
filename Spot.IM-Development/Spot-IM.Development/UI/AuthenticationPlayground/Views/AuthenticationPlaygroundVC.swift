4885539ef87341230e8bba503863847d140aa168
Alon Haiut <alon.h@openweb.com>
2022-05-10 11:26:48 +0300

    Started to add infra for a good authentication screen

---
+++ Spot.IM-Development/Spot-IM.Development/UI/AuthenticationPlayground/Views/AuthenticationPlaygroundVC.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationPlaygroundVC.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 10/05/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
