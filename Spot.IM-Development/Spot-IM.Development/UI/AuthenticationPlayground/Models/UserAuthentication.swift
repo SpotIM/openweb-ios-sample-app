2d2c528df47c21f6630623f38cb5c2d16e39eefc
Alon Haiut <alon.h@openweb.com>
2022-05-10 11:26:48 +0300

    Started to add infra for a good authentication screen

---
+++ Spot.IM-Development/Spot-IM.Development/UI/AuthenticationPlayground/Models/UserAuthentication.swift
@@ -0,0 +1,15 @@
+//
+//  UserAuthentication.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 10/05/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct UserAuthentication {
+    let username: String
+    let password: String
+    let userToken: String
+}
