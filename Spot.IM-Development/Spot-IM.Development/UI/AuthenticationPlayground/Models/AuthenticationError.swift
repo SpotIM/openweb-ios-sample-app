f6a25497e81552ec84e3248f6c61f2f0027e9104
Alon Haiut <alon.h@openweb.com>
2022-05-15 17:41:58 +0300

    Added authentication errors in the sampleApp

---
+++ Spot.IM-Development/Spot-IM.Development/UI/AuthenticationPlayground/Models/AuthenticationError.swift
@@ -0,0 +1,15 @@
+//
+//  AuthenticationError.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 11/05/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum AuthenticationError: Error {
+    case startSSOCodeAMissing
+    case completeSSOFailed
+    case JWTSSOFailed
+}
