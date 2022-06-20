5cba94453c252cabd15f70fd8c8b45f3e93cf59f
Alon Shprung <alon@cherrypick-consulting.com>
2022-06-20 16:52:36 +0300

    create OWCommentUserView and add it to comment cell

---
+++ Spot.IM-Development/SpotImCore/UI/Views/TableViewCells/CommentCellViews/Views/OWCommentUserView.swift
@@ -0,0 +1,111 @@
+//
+//  OWCommentUserView.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 20/06/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class OWCommentUserView: OWBaseView {
+    
+    fileprivate struct Metrics {
+        static let topOffset: CGFloat = 14.0
+        static let topCollapsedOffset: CGFloat = 38.0
+        static let leadingOffset: CGFloat = 16.0
+        static let userViewCollapsedHeight: CGFloat = 44.0
+        static let userViewExpandedHeight: CGFloat = 69.0
+        static let avatarSideSize: CGFloat = 39.0
+        static let avatarImageViewTrailingOffset: CGFloat = 11.0
+    }
+    
+    fileprivate var viewModel: OWCommentUserViewModeling!
+    fileprivate var disposeBag: DisposeBag!
+        
+    private let avatarImageView: SPAvatarView = SPAvatarView()
+    private let userNameView: UserNameView = .init()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        addSubviews(avatarImageView, userNameView)
+        configureAvatarView()
+        configureUserNameView()
+    }
+    
+    // Handle dark mode \ light mode change
+    func updateColorsAccordingToStyle() {
+        userNameView.updateColorsAccordingToStyle()
+        avatarImageView.updateColorsAccordingToStyle()
+    }
+    
+    private func configureAvatarView() {
+        avatarImageView.delegate = self
+        avatarImageView.OWSnp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Metrics.leadingOffset)
+            make.trailing.equalTo(userNameView.OWSnp.leading).offset(-Metrics.avatarImageViewTrailingOffset)
+            make.top.equalTo(userNameView)
+            make.size.equalTo(Metrics.avatarSideSize)
+        }
+    }
+    
+    private func configureUserNameView() {
+        userNameView.delegate = self
+        userNameView.OWSnp.makeConstraints { make in
+            make.trailing.top.equalToSuperview()
+            make.height.equalTo(Metrics.userViewCollapsedHeight)
+        }
+    }
+    
+    func updateUserView(with dataModel: CommentViewModel) {
+        userNameView.setDeletedOrReported(isDeleted: dataModel.isDeleted, isReported: dataModel.isReported)
+        
+        userNameView.setUserName(
+            dataModel.displayName,
+            badgeTitle: dataModel.badgeTitle,
+            contentType: .reply,
+            isDeleted: dataModel.isDeletedOrReported(),
+            isOneLine: dataModel.isUsernameOneRow())
+        userNameView.setMoreButton(hidden: dataModel.isDeletedOrReported())
+        userNameView.setSubtitle(
+            dataModel.replyingToDisplayName?.isEmpty ?? true
+                ? dataModel.timestamp
+                : "\(dataModel.replyingToDisplayName!) · ".appending(dataModel.timestamp ?? "")
+        )
+        let userViewHeight = dataModel.usernameViewHeight()
+        userNameView.OWSnp.updateConstraints { make in
+            make.height.equalTo(userViewHeight)
+        }
+
+        userNameView.configureSubscriberBadgeVM(viewModel: dataModel.subscriberBadgeVM)
+    }
+    
+    func updateAvatarView(with dataModel: CommentViewModel) {
+        avatarImageView.configure(with: dataModel.avatarViewVM)
+    }
+}
+
+extension OWCommentUserView: UserNameViewDelegate {
+    
+    func moreButtonDidTapped(sender: UIButton) {
+//        delegate?.moreTapped(for: commentId, replyingToID: replyingToId, sender: sender)
+    }
+    
+    func userNameDidTapped() {
+//        delegate?.respondToAuthorTap(for: commentId, isAvatarClicked: false)
+    }
+}
+
+extension OWCommentUserView: OWAvatarViewDelegate {
+    
+    func avatarDidTapped() {
+//        delegate?.respondToAuthorTap(for: commentId, isAvatarClicked: true)
+    }
+}
