c53509d52452bc5250fd5797b308431fb507beb7
Alon Haiut <alon.h@openweb.com>
2022-06-28 14:05:49 +0300

    Added some stuff to the future manager

--- Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/OWManager.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/OWManager.swift
@@ -7,14 +7,37 @@
 //
 
 import Foundation
+import RxSwift
 
-class OWManager {
+// Will be a public protocol which expose the different layers of the manager
+protocol OWManagerProtocol {
+    
+}
+
+// Internal protocol
+protocol OWManagerInternalProtocol: AnyObject  {
+    var spotConfig: Observable<SPSpotConfiguration> { get }
+}
+
+class OWManager: OWManagerInternalProtocol {
     
     // Singleton, will be public access once a new API will be ready.
     // Publishers and SDK consumers will basically interact with the manager / public encapsulation of it.
     static let shared = OWManager()
     
-    private init() {
-        
+    // Memebers variables
+    fileprivate let servicesProvider: OWSharedServicesProviding
+    fileprivate let _spotConfig = BehaviorSubject<SPSpotConfiguration?>(value: nil)
+    
+    private init(servicesProvider: OWSharedServicesProviding = OWSharedServicesProvider.shared) {
+        self.servicesProvider = servicesProvider
+    }
+}
+
+extension OWManager {
+    var spotConfig: Observable<SPSpotConfiguration> {
+        return _spotConfig
+            .unwrap()
+            .asObservable()
     }
 }
