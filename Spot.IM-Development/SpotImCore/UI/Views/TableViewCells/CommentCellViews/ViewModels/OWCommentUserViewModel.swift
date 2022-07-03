75ce106457338108ad3ccc204fdebc921d68b6dd
Alon Shprung <alon@cherrypick-consulting.com>
2022-07-03 12:05:12 +0300

    more rx changes - remove internal delegates

--- Spot.IM-Development/SpotImCore/UI/Views/TableViewCells/CommentCellViews/ViewModels/OWCommentUserViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/Views/TableViewCells/CommentCellViews/ViewModels/OWCommentUserViewModel.swift
@@ -12,10 +12,13 @@ import UIKit
 
 protocol OWCommentUserViewModelingInputs {
     func configure(with model: CommentViewModel)
+    func setDelegate(_ delegate: SPCommentCellDelegate)
+    
+    var tapAuthor: PublishSubject<Bool> { get }
 }
 
 protocol OWCommentUserViewModelingOutputs {
-    var subscriberBadgeVM: OWUserSubscriberBadgeViewModeling { get }
+    var userNameVM: OWUserNameViewModeling { get }
     var avatarVM: OWAvatarViewModeling { get }
 }
 
@@ -31,18 +34,55 @@ class OWCommentUserViewModel: OWCommentUserViewModeling,
     var inputs: OWCommentUserViewModelingInputs { return self }
     var outputs: OWCommentUserViewModelingOutputs { return self }
     
+    fileprivate let disposeBag = DisposeBag()
+    
+    // TODO - use RX
+    fileprivate var delegate: SPCommentCellDelegate?
+    
+    fileprivate var commentId: String?
+    fileprivate var replyToCommentId: String?
+    
     let avatarVM: OWAvatarViewModeling
+    let userNameVM: OWUserNameViewModeling
     
     init(user: SPUser?, imageProvider: SPImageProvider? = nil) {
         avatarVM = OWAvatarViewModel(user: user, imageURLProvider: imageProvider)
-        if let user = user {
-            subscriberBadgeVM.inputs.configureUser(user: user)
-        }
+        userNameVM = OWUserNameViewModel(user: user)
+        
+        self.setupObservers()
     }
     
     let subscriberBadgeVM: OWUserSubscriberBadgeViewModeling = OWUserSubscriberBadgeViewModel()
     
+    var tapAuthor = PublishSubject<Bool>()
+    
     func configure(with model: CommentViewModel) {
+        commentId = model.commentId
+        replyToCommentId = model.replyingToCommentId
+    }
+    
+    func setDelegate(_ delegate: SPCommentCellDelegate) {
+        self.delegate = delegate
+    }
+    
+    func setupObservers() {
+        userNameVM.inputs.tapUserName.subscribe(onNext: { [weak self] _ in
+            self?.tapAuthor.onNext(false)
+        }).disposed(by: disposeBag)
         
+        tapAuthor.subscribe(onNext: {  [weak self] isAvatarClicked in
+            guard let self = self else { return }
+            self.delegate?.respondToAuthorTap(for: self.commentId, isAvatarClicked: isAvatarClicked)
+        }).disposed(by: disposeBag)
+        
+        userNameVM.inputs.tapMore.subscribe(onNext: { [weak self] sender in
+            guard let self = self else { return }
+            self.delegate?.moreTapped(for: self.commentId, replyingToID: self.replyToCommentId, sender: sender)
+        }).disposed(by: disposeBag)
+        
+        avatarVM.inputs.tapAvatar.subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
+            self.delegate?.respondToAuthorTap(for: self.commentId, isAvatarClicked: true)
+        }).disposed(by: disposeBag)
     }
 }
