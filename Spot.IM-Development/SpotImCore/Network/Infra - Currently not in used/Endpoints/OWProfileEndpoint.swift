5e28a34df44c8e6eb9c010210b5a84a110d579ca
Nogah <nogah@applift-consulting.com>
2022-07-20 16:07:08 +0300

    add OWProfileEndpoint

---
+++ Spot.IM-Development/SpotImCore/Network/Infra - Currently not in used/Endpoints/OWProfileEndpoint.swift
@@ -0,0 +1,57 @@
+//
+//  OWProfileEndpoint.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 20/07/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum OWProfileEndpoint: OWEndpoint {
+    case createSingleUseToken
+    
+    // MARK: - HTTPMethod
+    var method: HTTPMethod {
+        switch self {
+        case .createSingleUseToken:
+            return .post
+        }
+    }
+    
+    // MARK: - Path
+    var path: String {
+        switch self {
+        case .createSingleUseToken:
+            return "/profile/create-single-use-token"
+        }
+    }
+    
+    // MARK: - Parameters
+    var parameters: Parameters? {
+        switch self {
+        case .createSingleUseToken:
+            var requestParams: Parameters = ["access_token": SPUserSessionHolder.session.token?.replacingOccurrences(of: "Bearer ", with: "")]
+            if let openwebToken = SPUserSessionHolder.session.openwebToken {
+                requestParams["open_web_token"] = openwebToken
+            }
+            return requestParams
+        }
+    }
+}
+
+protocol OWProfileAPI {
+    func createSingleUseToken() -> OWNetworkResponse<[String: String]>
+}
+
+extension OWNetworkAPI: OWProfileAPI {
+    // Access by .realtime for readability
+    var profile: OWProfileAPI { return self }
+    
+    func createSingleUseToken() -> OWNetworkResponse<[String: String]> {
+        let endpoint = OWProfileEndpoint.createSingleUseToken
+        let requestConfigure = request(for: endpoint)
+        return performRequest(route: requestConfigure)
+    }
+}
