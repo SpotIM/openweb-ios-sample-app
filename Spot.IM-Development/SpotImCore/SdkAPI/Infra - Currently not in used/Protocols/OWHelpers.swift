c0933e9adeb19c350a8aa4faf0a0985862757568
Alon Haiut <alon.h@openweb.com>
2022-07-20 18:04:56 +0300

    Infra is pretty much ready

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/OWHelpers.swift
@@ -0,0 +1,14 @@
+//
+//  OWHelpers.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 20/07/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+// Will be a public protocol
+protocol OWHelpers {
+    var additionalConfigurations: [OWAdditionalConfiguration] { get set }
+}
