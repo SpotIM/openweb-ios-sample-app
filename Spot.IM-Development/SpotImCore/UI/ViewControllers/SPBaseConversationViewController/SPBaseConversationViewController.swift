790adb06702e2a7708f17cd1ed2630b5c70ec028
Alon Shprung <alonshp1@gmail.com>
2022-08-22 14:37:18 +0300

    create comment entry view and vm logic

--- Spot.IM-Development/SpotImCore/UI/ViewControllers/SPBaseConversationViewController/SPBaseConversationViewController.swift
+++ Spot.IM-Development/SpotImCore/UI/ViewControllers/SPBaseConversationViewController/SPBaseConversationViewController.swift
@@ -852,8 +852,8 @@ extension SPBaseConversationViewController: MainConversationModelDelegate {
     
 }
 
-extension SPBaseConversationViewController: SPMainConversationFooterViewDelegate {
-    func labelContainerDidTap(_ footerView: SPMainConversationFooterView) {
+extension SPBaseConversationViewController: OWCommentCreationEntryViewDelegate {
+    func labelContainerDidTap() {
         guard let delegate = delegate else { return }
         
         if SpotIm.reactNativeShowLoginScreenOnRootVC &&
@@ -865,7 +865,7 @@ extension SPBaseConversationViewController: SPMainConversationFooterViewDelegate
         delegate.createComment(with: model)
     }
     
-    func userAvatarDidTap(_ foorterView: SPMainConversationFooterView) {
+    func userAvatarDidTap() {
         myUserAvatarDidTap()
     }
     
