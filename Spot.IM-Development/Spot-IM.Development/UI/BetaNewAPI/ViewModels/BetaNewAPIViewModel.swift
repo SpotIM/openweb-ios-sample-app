bd001296e3dc95c422326b33ea721e0597d3df5f
Alon Haiut <alon.h@openweb.com>
2022-08-31 17:52:20 +0300

    Infra for Beta API screen inside the sample app is ready

---
+++ Spot.IM-Development/Spot-IM.Development/UI/BetaNewAPI/ViewModels/BetaNewAPIViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  BetaNewAPIViewModel.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 31/08/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import SpotImCore
+
+protocol BetaNewAPIViewModelingInputs {
+    
+}
+
+protocol BetaNewAPIViewModelingOutputs {
+    var title: String { get }
+}
+
+protocol BetaNewAPIViewModeling {
+    var inputs: BetaNewAPIViewModelingInputs { get }
+    var outputs: BetaNewAPIViewModelingOutputs { get }
+}
+
+class BetaNewAPIViewModel: BetaNewAPIViewModeling, BetaNewAPIViewModelingInputs, BetaNewAPIViewModelingOutputs {
+    var inputs: BetaNewAPIViewModelingInputs { return self }
+    var outputs: BetaNewAPIViewModelingOutputs { return self }
+    
+    fileprivate let disposeBag = DisposeBag()
+    
+    lazy var title: String = {
+        return NSLocalizedString("NewAPI", comment: "")
+    }()
+    
+    init() {
+        setupObservers()
+    }
+}
+
+fileprivate extension BetaNewAPIViewModel {
+    func setupObservers() {
+        
+    }
+}
