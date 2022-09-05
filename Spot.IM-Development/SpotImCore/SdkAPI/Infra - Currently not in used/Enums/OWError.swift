ed70e005fc1654f20bf90f1b6b098d462148f68f
Alon Haiut <alon.h@openweb.com>
2022-09-05 18:21:19 +0300

    In the middle of adding more protocols

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWError.swift
@@ -0,0 +1,19 @@
+//
+//  OWError.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 05/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+#if NEW_API
+enum OWError: Error {
+
+}
+#else
+enum OWError: Error {
+
+}
+#endif
