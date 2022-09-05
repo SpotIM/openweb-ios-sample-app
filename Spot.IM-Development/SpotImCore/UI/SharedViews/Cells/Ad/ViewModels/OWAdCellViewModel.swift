a877be2256ebbe830637947169c7b8dce34c3c2f
Alon Shprung <alonshp1@gmail.com>
2022-09-05 17:48:35 +0300

    more renaming and arranging files

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/Ad/ViewModels/OWAdCellViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  OWAdCellViewModel.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 27/07/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol OWAdCellViewModelingInputs {
+    
+}
+
+protocol OWAdCellViewModelingOutputs {
+    
+}
+
+protocol OWAdCellViewModeling: OWCellViewModel {
+    var inputs: OWAdCellViewModelingInputs { get }
+    var outputs: OWAdCellViewModelingOutputs { get }
+}
+
+class OWAdCellViewModel: OWAdCellViewModeling, OWAdCellViewModelingInputs, OWAdCellViewModelingOutputs {
+    var inputs: OWAdCellViewModelingInputs { return self }
+    var outputs: OWAdCellViewModelingOutputs { return self }
+}
+
+extension OWAdCellViewModel {
+    static func stub() -> OWAdCellViewModeling {
+        return OWAdCellViewModel()
+    }
+}
