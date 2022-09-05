03dfb272a2fb89e31226a4eb5c0696404a377f1f
Alon Shprung <alonshp1@gmail.com>
2022-09-05 16:47:26 +0300

    add comment creation files and coment creation VMs implementation

---
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/Views/OWCommentCreationVC.swift
@@ -0,0 +1,9 @@
+//
+//  OWCommentCreationVC.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 05/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
---
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/Views/OWCommentCreationView.swift
@@ -0,0 +1,9 @@
+//
+//  OWCommentCreationView.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 05/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
