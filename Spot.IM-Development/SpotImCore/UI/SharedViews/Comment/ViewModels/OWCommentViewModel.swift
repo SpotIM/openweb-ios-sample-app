87b99bbbc2cfc08b83fa10cff91263e4c170c484
Alon Shprung <alonshp1@gmail.com>
2022-09-07 11:01:47 +0300

    prepare view and vm files

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/ViewModels/OWCommentViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  OWCommentViewModel.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 05/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+protocol OWCommentViewModelingInputs {
+}
+
+protocol OWCommentViewModelingOutputs {
+}
+
+protocol OWCommentViewModeling {
+    var inputs: OWCommentViewModelingInputs { get }
+    var outputs: OWCommentViewModelingOutputs { get }
+}
+
+class OWCommentViewModel: OWCommentViewModeling,
+                          OWCommentViewModelingInputs,
+                          OWCommentViewModelingOutputs {
+    
+    var inputs: OWCommentViewModelingInputs { return self }
+    var outputs: OWCommentViewModelingOutputs { return self }
+    
+}
---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentView.swift
@@ -0,0 +1,41 @@
+//
+//  OWCommentView.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 05/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class OWCommentView: UIView {
+    fileprivate struct Metrics {
+        
+    }
+    
+    fileprivate let viewModel: OWCommentViewModeling
+    fileprivate let disposeBag = DisposeBag()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(viewModel: OWCommentViewModeling) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        setupViews()
+        setupObservers()
+    }
+}
+
+fileprivate extension OWCommentView {
+    func setupViews() {
+
+    }
+    
+    func setupObservers() {
+
+    }
+}
