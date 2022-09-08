3806453fa3128a4e070635339578f4b921604934
Alon Haiut <alon.h@openweb.com>
2022-09-08 11:37:00 +0300

    Keep working on the conversation integration with the new API. Infra for connecting the actual API with the coordinator is almost ready

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/NavigationController/OWNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  OWNavigationController.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 08/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import UIKit
+
+class OWNavigationController: UINavigationController {
+    
+}
