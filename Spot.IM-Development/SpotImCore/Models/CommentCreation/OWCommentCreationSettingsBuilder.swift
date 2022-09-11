6e7de6ae53c095acf4d731252821ee9ed8b035ef
Alon Haiut <alon.h@openweb.com>
2022-09-11 12:23:59 +0300

    Added OWCommentCreationSettingsBuilder

---
+++ Spot.IM-Development/SpotImCore/Models/CommentCreation/OWCommentCreationSettingsBuilder.swift
@@ -0,0 +1,24 @@
+//
+//  OWCommentCreationSettingsBuilder.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 11/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+#if NEW_API
+public struct OWCommentCreationSettingsBuilder: OWCommentCreationSettingsProtocol {
+    public var conversationSettings: OWConversationSettingsProtocol
+    
+    public init(conversationSettings: OWConversationSettingsProtocol) {
+        self.conversationSettings = conversationSettings
+    }
+    
+    @discardableResult public mutating func conversationSettings(settings: OWConversationSettingsProtocol) -> OWCommentCreationSettingsProtocol {
+        self.conversationSettings = settings
+        return self
+    }
+}
+#endif
