ff0912b02b6b20e423b8704a41d7abe490800729
Nogah <nogah@applift-consulting.com>
2022-09-12 10:24:13 +0300

    add flushEventsQueue using Rx

--- Spot.IM-Development/SpotImCore/Services/OWAnalyticsService.swift
+++ Spot.IM-Development/SpotImCore/Services/OWAnalyticsService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RxSwift
 
 fileprivate let MAX_EVENTS = 10 // TODO: change if needed
 
@@ -17,18 +18,68 @@ protocol OWAnalyticsServicing {
 
 class OWAnalyticsService: OWAnalyticsServicing {
     fileprivate let maxEventsForFlush: Int
-    fileprivate var events: [OWAnalyticEvent]
+    fileprivate let appLifeCycle: OWRxAppLifeCycleProtocol
+    fileprivate var analyticsEvents: [OWAnalyticEvent]
     
-    init(maxEventsForFlush: Int = MAX_EVENTS) {
+    fileprivate let flushEventsQueue = DispatchQueue(label: "OpenWebSDKAnalyticsDispatchQueue", qos: .background) // Serial queue
+    fileprivate let disposeBag = DisposeBag()
+    
+    init(maxEventsForFlush: Int = MAX_EVENTS,
+         appLifeCycle: OWRxAppLifeCycleProtocol = OWSharedServicesProvider.shared.appLifeCycle()) {
         self.maxEventsForFlush = maxEventsForFlush
-        events = []
+        self.appLifeCycle = appLifeCycle
+        analyticsEvents = []
+        
+        setupObservers()
     }
     
     func sendAnalyticEvent(event: OWAnalyticEvent) {
+        flushEventsQueue.async { [weak self] in
+            guard let self = self else { return }
+            self.analyticsEvents.append(event)
+            self.flushEventsIfNeeded()
+        }
     }
     
     func sendAnalyticEvents(events: [OWAnalyticEvent]) {
+        flushEventsQueue.async { [weak self] in
+            guard let self = self else { return }
+            self.analyticsEvents.append(contentsOf: events)
+            self.flushEventsIfNeeded()
+        }
     }
     
+}
+
+fileprivate extension OWAnalyticsService {
+    func flushEventsIfNeeded() {
+        if analyticsEvents.count >= maxEventsForFlush {
+            flushEvents()
+        }
+    }
     
+    // TODO: perhaps some retry/report is needed if `analytics.sendEvents` fails ?
+    func flushEvents() {
+        OWSharedServicesProvider.shared.netwokAPI().analytics.sendEvents(events: analyticsEvents)
+        analyticsEvents = []
+    }
+}
+
+// Rx
+fileprivate extension OWAnalyticsService {
+    func setupObservers() {
+        appLifeCycle.didEnterBackground
+            .filter { [weak self] in
+                guard let self = self else { return false }
+                return !self.analyticsEvents.isEmpty
+            }
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.flushEventsQueue.async { [weak self] in
+                    guard let self = self else { return }
+                    self.flushEvents()
+                }
+            })
+            .disposed(by: disposeBag)
+    }
 }
