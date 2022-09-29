0c7eabfe69e52ab0bc4ad15cf0a206a7000c3f40
Alon Haiut <alon.h@openweb.com>
2022-09-29 12:51:19 +0300

    Added OWScreenPushStyle - we now have an option to push which looks like present

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/NavigationController/OWScreenPushStyle.swift
@@ -0,0 +1,14 @@
+//
+//  OWScreenPushStyle.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 28/09/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum OWScreenPushStyle {
+    case regular
+    case presentStyle
+}
