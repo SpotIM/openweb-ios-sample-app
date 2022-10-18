19875472eee4f8c547ea9724209acf007cbc510b
Alon Haiut <alon.h@openweb.com>
2022-10-18 18:52:46 +0300

    Started to work on the View infra part for the Shimmering Skeleton

---
+++ Spot.IM-Development/SpotImCore/Protocols/SkeletonShimmering/OWAssociatedSkeletonShimmering.swift
@@ -0,0 +1,15 @@
+//
+//  OWAssociatedSkeletonShimmering.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 18/10/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct OWAssociatedSkeletonShimmering {
+    static var skeletonLayerIdentifier = "OWSkeletonLayerIdentifier"
+    static var shimmeringLayerIdentifier = "OWShimmeringLayerIdentifier"
+    static var shimmeringLayerAnimationIdentifier = "OWShimmeringLayerIdentifier"
+}
---
+++ Spot.IM-Development/SpotImCore/Protocols/SkeletonShimmering/OWInitializable.swift
@@ -0,0 +1,13 @@
+//
+//  OWInitializable.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 18/10/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+protocol OWInitializable {
+    init()
+}
