76104fa97bae78930acedca49f2bc7bab929cf8d
Nogah <nogah@applift-consulting.com>
2022-10-31 16:54:51 +0200

    support dark/light mode change

--- Spot.IM-Development/SpotImCore/UI/PreConversation/Views/OWPreConversationView.swift
+++ Spot.IM-Development/SpotImCore/UI/PreConversation/Views/OWPreConversationView.swift
@@ -205,3 +205,18 @@ fileprivate extension OWPreConversationView {
         }
     }
 }
+
+extension OWPreConversationHeaderView {
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        let state = UIApplication.shared.applicationState
+        if #available(iOS 12.0, *) {
+            if previousTraitCollection?.userInterfaceStyle != self.traitCollection.userInterfaceStyle {
+                // traitCollectionDidChange() is called multiple times, see: https://stackoverflow.com/a/63380259/583425
+                if state != .background {
+                    OWSharedServicesProvider.shared.themeStyleService().setStyle(style: self.traitCollection.userInterfaceStyle == .dark ? OWThemeStyle.dark : OWThemeStyle.light)
+                }
+            }
+        }
+    }
+}
