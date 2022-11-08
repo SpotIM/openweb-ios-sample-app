24da6bb75ce2763f4f70f7f7561a819a1b6da491
Alon Haiut <alon.h@openweb.com>
2022-11-08 14:58:17 +0200

    Almost finish to add OWCustomizationsLayer and related stuff

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWSortOption.swift
@@ -0,0 +1,25 @@
+//
+//  OWSortOption.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 08/11/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+#if NEW_API
+public enum OWSortOption {
+    case `default` // In such case we will use the sorting option from the network configuration
+    case best
+    case newest
+    case oldest
+}
+#else
+enum OWSortOption {
+    case `default` // In such case we will use the sorting option from the network configuration
+    case best
+    case newest
+    case oldest
+}
+#endif
---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/Layers/OWCustomizations.swift
@@ -0,0 +1,23 @@
+//
+//  OWCustomizations.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 08/11/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+#if NEW_API
+public protocol OWCustomizations {
+//    var adsProvider: OWAdsProvider { get set }
+}
+#else
+protocol OWCustomizations {
+    var fontFamily: String? { get set }
+    var spacing: OWSpacing { get set }
+    var sorting: OWSortingCustomizations { get }
+    var themeEnforcement: OWThemeStyleEnforcement { get set }
+    func addElementCallback(_ callback: @escaping OWCustomizableElementCallback)
+}
+#endif
