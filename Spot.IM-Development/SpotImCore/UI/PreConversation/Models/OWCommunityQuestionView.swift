39989dfae6e737b6a63131081b8aa3b585c87d74
Nogah <nogah@applift-consulting.com>
2022-11-08 15:05:44 +0200

    start community question view

--- Spot.IM-Development/SpotImCore/UI/PreConversation/Models/OWCommunityQuestionView.swift
+++ Spot.IM-Development/SpotImCore/UI/PreConversation/Models/OWCommunityQuestionView.swift
@@ -10,13 +10,62 @@ import UIKit
 import RxSwift
 
 // TODO: complete
-internal final class OWCommunityQuestionView: OWBaseView {
+class OWCommunityQuestionView: UIView {
+    fileprivate struct Metrics {
+        static let identifier = "community_question_id"
+        static let fontSize: CGFloat = 20.0
+        static let questionHorizontalOffset: CGFloat = 16.0
+    }
+    
+    fileprivate lazy var questionTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.backgroundColor = .clear
+        textView.font = UIFont.preferred(style: .bold, of: Metrics.fontSize)
+        textView.textColor = OWColorPalette.shared.color(type: .foreground0Color,
+                                                         themeStyle: OWSharedServicesProvider.shared.themeStyleService().currentStyle)
+        return textView
+    }()
     
     fileprivate let viewModel: OWCommunityQuestionViewModeling
+    fileprivate let disposeBag = DisposeBag()
     
     init(with viewModel: OWCommunityQuestionViewModeling) {
         self.viewModel = viewModel
         super.init(frame: .zero)
+        self.accessibilityIdentifier = Metrics.identifier
+        setupViews()
+        setupObservers()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+fileprivate extension OWCommunityQuestionView {
+    func setupViews() {
+        self.backgroundColor = .clear
+        
+        questionTextView.OWSnp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            // avoide device notch in landscape
+            if #available(iOS 11.0, *) {
+                make.leading.equalTo(safeAreaLayoutGuide).offset(Metrics.questionHorizontalOffset)
+                make.trailing.equalTo(safeAreaLayoutGuide).offset(-Metrics.questionHorizontalOffset)
+            } else {
+                make.leading.equalToSuperview().offset(Metrics.questionHorizontalOffset)
+                make.trailing.equalToSuperview().offset(-Metrics.questionHorizontalOffset)
+            }
+        }
+    }
+    
+    func setupObservers() {
+        viewModel.outputs
+            .communityQuestionString
+            .bind(to: questionTextView.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
