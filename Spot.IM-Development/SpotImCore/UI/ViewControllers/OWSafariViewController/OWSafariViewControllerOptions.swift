0a8d9574dd1059290c6601a5e26035e2efc10561
Nogah <nogah@applift-consulting.com>
2022-11-09 11:53:57 +0200

    Open community guidelines link in safari viewcontroller using coordinator

---
+++ Spot.IM-Development/SpotImCore/UI/ViewControllers/OWSafariViewController/OWSafariViewControllerOptions.swift
@@ -0,0 +1,13 @@
+//
+//  OWSafariViewControllerOptions.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 09/11/2022.
+//  Copyright © 2022 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct OWSafariViewControllerOptions {
+    var url: URL
+}
