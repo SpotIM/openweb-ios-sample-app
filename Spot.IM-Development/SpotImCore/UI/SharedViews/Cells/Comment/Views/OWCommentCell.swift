3ea31284740eb19e950bae57cbfcdebd49b6a4bd
Nogah <nogah@applift-consulting.com>
2022-12-21 15:13:30 +0200

    Move cell content to seperate view

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/Comment/Views/OWCommentCell.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/Comment/Views/OWCommentCell.swift
@@ -10,22 +10,9 @@ import Foundation
 import UIKit
 
 class OWCommentCell: UITableViewCell {
-    fileprivate struct Metrics {
-        static let leadingOffset: CGFloat = 16.0
-        static let commentLabelTopPadding: CGFloat = 10.0
-        static let messageContainerTopOffset: CGFloat = 5.0
-    }
-    
-    fileprivate lazy var commentHeaderView: OWCommentHeaderView = {
-        return OWCommentHeaderView()
+    fileprivate lazy var commentView: OWCommentView = {
+       return OWCommentView()
     }()
-    fileprivate lazy var commentLabelView: OWCommentLabelView = {
-        return OWCommentLabelView()
-    }()
-    fileprivate lazy var commentContentView: OWCommentContentView = {
-        return OWCommentContentView()
-    }()
-    
     fileprivate var viewModel: OWCommentCellViewModeling!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -41,9 +28,7 @@ class OWCommentCell: UITableViewCell {
         guard let vm = viewModel as? OWCommentCellViewModeling else { return }
         
         self.viewModel = vm
-        self.commentHeaderView.configure(with: self.viewModel.outputs.commentVM.outputs.commentHeaderVM)
-        self.commentLabelView.configure(viewModel: self.viewModel.outputs.commentVM.outputs.commentLabelVM)
-        self.commentContentView.configure(with: self.viewModel.outputs.commentVM.outputs.contentVM)
+        self.commentView.configure(with: self.viewModel.outputs.commentVM)
         
         setupUI()
         setupObservers()
@@ -53,21 +38,10 @@ class OWCommentCell: UITableViewCell {
 fileprivate extension OWCommentCell {
     func setupUI() {
         self.backgroundColor = .clear
-        self.addSubviews(commentHeaderView, commentLabelView, commentContentView)
+        self.addSubviews(commentView)
         
-        commentHeaderView.OWSnp.makeConstraints { make in
-            make.leading.trailing.top.equalToSuperview()
-            make.height.equalTo(50) // TODO: remove
-        }
-        
-        commentLabelView.OWSnp.makeConstraints { make in
-            make.top.equalTo(commentHeaderView.OWSnp.bottom).offset(Metrics.commentLabelTopPadding)
-            make.leading.equalToSuperview()
-        }
-        
-        commentContentView.OWSnp.makeConstraints { make in
-            make.top.equalTo(commentLabelView.OWSnp.bottom).offset(Metrics.messageContainerTopOffset) // TODO!
-            make.leading.trailing.bottom.equalToSuperview()
+        commentView.OWSnp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
