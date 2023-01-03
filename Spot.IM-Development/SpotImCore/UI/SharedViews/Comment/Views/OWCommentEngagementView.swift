cc7653fc1cc0140077574bd83ec53c7a5dc377f3
Nogah <nogah@applift-consulting.com>
2023-01-03 11:20:44 +0200

    PR fixes

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentEngagementView.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentEngagementView.swift
@@ -19,7 +19,7 @@ class OWCommentEngagementView: UIView {
         static let replyButtonIdentifier = "comment_actions_view_reply_button_id"
     }
     
-    fileprivate var viewModel: OWCommentEngagementViewModeling = OWCommentEngagementViewModel()
+    fileprivate var viewModel: OWCommentEngagementViewModeling!
     fileprivate var disposeBag: DisposeBag = DisposeBag()
     
     fileprivate lazy var replyButton: UIButton = {
