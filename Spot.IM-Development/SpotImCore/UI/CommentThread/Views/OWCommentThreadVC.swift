89de6fe4f5e8e4bedf05f70752401538b5af9073
Alon Shprung <alonshp1@gmail.com>
2023-01-30 17:05:38 +0200

    comment thread view and vc

--- Spot.IM-Development/SpotImCore/UI/CommentThread/Views/OWCommentThreadVC.swift
+++ Spot.IM-Development/SpotImCore/UI/CommentThread/Views/OWCommentThreadVC.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2023 Spot.IM. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class OWCommentThreadVC: UIViewController {
+    fileprivate struct Metrics {
+        
+    }
+    
+    fileprivate let viewModel: OWCommentThreadViewModeling
+    fileprivate let disposeBag = DisposeBag()
+    
+    fileprivate lazy var commentThreadView: OWCommentThreadView = {
+        let commentThreadView = OWCommentThreadView(viewModel: viewModel.outputs.commentThreadViewVM)
+        return commentThreadView
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(viewModel: OWCommentThreadViewModeling) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        setupViews()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.inputs.viewDidLoad.onNext()
+    }
+}
+
+fileprivate extension OWCommentThreadVC {
+    func setupViews() {
+        view.addSubview(commentThreadView)
+        commentThreadView.OWSnp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
