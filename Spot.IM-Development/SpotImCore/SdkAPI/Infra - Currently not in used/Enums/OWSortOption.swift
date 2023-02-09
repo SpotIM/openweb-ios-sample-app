268c5b99f51b4665e2e50dc14b8050d952ad3358
Alon Haiut <alon.h@openweb.com>
2023-02-09 18:03:45 +0200

    Removed duplicate logic file OWCommentSortMode, we have OWSortOption

--- Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWSortOption.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWSortOption.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 #if NEW_API
-public enum OWSortOption {
+public enum OWSortOption: String {
     case `default` // In such case we will use the sorting option from the network configuration
     case best
     case newest
     case oldest
 }
 #else
-enum OWSortOption {
+enum OWSortOption: String {
     case `default` // In such case we will use the sorting option from the network configuration
     case best
     case newest
