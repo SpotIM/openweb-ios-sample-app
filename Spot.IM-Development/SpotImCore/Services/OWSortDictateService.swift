84febb5faef4f3f7e48693e6d79cf9a90b7c94ab
Alon Haiut <alon.h@openweb.com>
2023-02-09 15:32:07 +0200

    Initial commit

---
+++ Spot.IM-Development/SpotImCore/Services/OWSortDictateService.swift
@@ -0,0 +1,17 @@
+//
+//  OWSortDictateService.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 09/02/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+protocol OWSortDictateServicing {
+    
+}
+
+class OWSortDictateService: OWSortDictateServicing {
+    
+}
