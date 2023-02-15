1dc5d1677f9edd77bd61df16828855a5262cbd15
Alon Haiut <alon.h@openweb.com>
2023-02-15 15:01:48 +0200

    Minor improvements after tech meeting

--- Spot.IM-Development/SpotImCore/Models/Sorting/OWSortOption+Extensions.swift
+++ Spot.IM-Development/SpotImCore/Models/Sorting/OWSortOption+Extensions.swift
@@ -20,6 +20,11 @@ extension OWSortOption {
         }
     }
     
+    static var `default`: OWSortOption {
+        // This will be returned as a default sort option
+        return OWSortOption.best
+    }
+    
     // TODO: Remove once we will remove old `SPCommentSortMode` class
     init(fromOldSortType oldSortType: SPCommentSortMode) {
         switch oldSortType {
