d2a6ca82e7da0f4da68464b0bb342bcc29bc3f2a
Nogah <nogah@applift-consulting.com>
2023-02-20 12:04:07 +0200

    PR comments fixes. Fix change locale on spotId change

--- Spot.IM-Development/SpotImCore/Style/Infra - Currently not in used/Colors/OWColorPalette.swift
+++ Spot.IM-Development/SpotImCore/Style/Infra - Currently not in used/Colors/OWColorPalette.swift
@@ -11,7 +11,7 @@ import RxSwift
 
 protocol OWColorPaletteProtocol {
     func color(type: OWColor.OWType, themeStyle: OWThemeStyle) -> UIColor
-    func colorObservable(type: OWColor.OWType) -> Observable<UIColor>
+    var colorDriver: Observable<[OWColor.OWType: OWColor]> { get }
 }
 
 protocol OWColorPaletteConfigurable {
@@ -20,20 +20,24 @@ protocol OWColorPaletteConfigurable {
 
 class OWColorPalette: OWColorPaletteProtocol, OWColorPaletteConfigurable {
     fileprivate var colors = [OWColor.OWType: OWColor]()
-    fileprivate var colorsBehaviorSubject = [OWColor.OWType: BehaviorSubject<OWColor>]()
-    fileprivate let servicesProvider: OWSharedServicesProviding
-
+    fileprivate var colorsMapper = BehaviorSubject<[OWColor.OWType: OWColor]>(value: [:])
+    var colorDriver: Observable<[OWColor.OWType: OWColor]> {
+        return colorsMapper
+            .asObservable()
+            .observe(on: MainScheduler.instance)
+            .share(replay: 1)
+    }
+    
     static let shared: OWColorPaletteProtocol & OWColorPaletteConfigurable = OWColorPalette()
 
-    private init(servicesProvider: OWSharedServicesProviding = OWSharedServicesProvider.shared) {
-        self.servicesProvider = servicesProvider
+    private init() {
         // Initialize default colors
         for type in OWColor.OWType.allCases {
             colors[type] = type.default
-            if type.shouldUpdateRxObservable {
-                colorsBehaviorSubject[type] = BehaviorSubject<OWColor>(value: type.default)
-            }
         }
+        colorsMapper.onNext(colors.filter { color in
+            color.key.shouldUpdateRxObservable
+        })
     }
 
     func color(type: OWColor.OWType, themeStyle: OWThemeStyle) -> UIColor {
@@ -45,27 +49,12 @@ class OWColorPalette: OWColorPaletteProtocol, OWColorPaletteConfigurable {
         return color.color(forThemeStyle: themeStyle)
     }
 
-    func colorObservable(type: OWColor.OWType) -> Observable<UIColor> {
-        guard let color = colorsBehaviorSubject[type] else {
-            return .empty()
-        }
-
-        return Observable
-            .combineLatest(color.asObserver(), servicesProvider.themeStyleService().style) { (color, style) in
-                return color.color(forThemeStyle: style)
-            }
-            .asObservable()
-            .distinctUntilChanged()
-            .observe(on: MainScheduler.instance)
-            .share(replay: 1)
-    }
-
     func setColor(_ color: UIColor, forType type: OWColor.OWType, forThemeStyle themeStyle: OWThemeStyle) {
         guard var encapsulateColor = colors[type] else { return }
         encapsulateColor.setColor(color, forThemeStyle: themeStyle)
         colors[type] = encapsulateColor // We are working with structs here, so we need to re set the encapsulated color for this key
-        if type.shouldUpdateRxObservable {
-            colorsBehaviorSubject[type]?.onNext(encapsulateColor)
-        }
+        colorsMapper.onNext(colors.filter { color in
+            color.key.shouldUpdateRxObservable
+        })
     }
 }
