168259396fda304928bdb5c1102ba5e8d9bc5e6a
Nogah <nogah@applift-consulting.com>
2023-03-01 17:42:10 +0200

    Return to publisher only view without height

--- Spot.IM-Development/SpotImCore/Manager/ManagerLayers/OWUILayer.swift
+++ Spot.IM-Development/SpotImCore/Manager/ManagerLayers/OWUILayer.swift
@@ -32,7 +32,7 @@ class OWUILayer: OWUI, OWUIFlows, OWUIViews {
                          presentationalMode: OWPresentationalMode,
                          additionalSettings: OWPreConversationSettingsProtocol? = nil,
                          callbacks: OWViewActionsCallbacks? = nil,
-                         completion: @escaping OWViewDynamicSizeCompletion) {
+                         completion: @escaping OWViewCompletion) {
         prepareForNewFlow()
 
         setPostId(postId: postId) { result in
@@ -53,7 +53,7 @@ class OWUILayer: OWUI, OWUIFlows, OWUIViews {
                                                 callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
         .subscribe(onNext: { result in
-            completion(.success(result))
+            completion(.success(result as? UIView ?? UIView()))
         }, onError: { err in
             let error: OWError = err as? OWError ?? OWError.conversationFlow
             completion(.failure(error))
