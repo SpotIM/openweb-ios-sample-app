f2559da41e5cd90f29befa25a52c4a8f3fcbd3f9
Alon Shprung <alonshp1@gmail.com>
2023-03-06 17:17:07 +0200

    fetch comment thread

--- Spot.IM-Development/SpotImCore/UI/CommentThread/ViewModels/OWCommentThreadViewVM.swift
+++ Spot.IM-Development/SpotImCore/UI/CommentThread/ViewModels/OWCommentThreadViewVM.swift
@@ -10,7 +10,7 @@ import Foundation
 import RxSwift
 
 protocol OWCommentThreadViewViewModelingInputs {
-
+    var viewInitialized: PublishSubject<Void> { get }
 }
 
 protocol OWCommentThreadViewViewModelingOutputs {
@@ -27,16 +27,43 @@ class OWCommentThreadViewViewModel: OWCommentThreadViewViewModeling, OWCommentTh
     var outputs: OWCommentThreadViewViewModelingOutputs { return self }
 
     fileprivate let servicesProvider: OWSharedServicesProviding
+    fileprivate let _commentThreadData = BehaviorSubject<OWCommentThreadRequiredData?>(value: nil)
+    fileprivate let disposeBag = DisposeBag()
 
-    init (servicesProvider: OWSharedServicesProviding = OWSharedServicesProvider.shared) {
+    var viewInitialized = PublishSubject<Void>()
+
+    init (commentThreadData: OWCommentThreadRequiredData, servicesProvider: OWSharedServicesProviding = OWSharedServicesProvider.shared) {
         self.servicesProvider = servicesProvider
-
+        self._commentThreadData.onNext(commentThreadData)
         setupObservers()
     }
 }
 
 fileprivate extension OWCommentThreadViewViewModel {
     func setupObservers() {
+        // Observable for the conversation network API
+        let conversationThreadReadObservable = _commentThreadData.unwrap().flatMap { [weak self] data -> Observable<SPConversationReadRM> in
+            guard let self = self else { return .empty() }
+            return self.servicesProvider
+            .netwokAPI()
+            .conversation
+            .conversationRead(mode: .newest, page: OWPaginationPage.first, messageId: data.commentId)
+            .response
+        }
 
+        let commentThreadFetchedObservable = viewInitialized
+            .flatMap { _ -> Observable<SPConversationReadRM> in
+                return conversationThreadReadObservable
+                    .take(1)
+            }
+            .share()
+
+        commentThreadFetchedObservable
+            .subscribe(onNext: { [weak self] response in
+                guard let self = self, let responseComments = response.conversation?.comments else { return }
+                // TODO - Build cells from response
+                print(responseComments)
+            })
+            .disposed(by: disposeBag)
     }
 }
