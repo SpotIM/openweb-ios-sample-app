c133496e6ea80df6143d4b18336eb77f96043c76
Alon Shprung <alonshp1@gmail.com>
2023-03-08 12:23:43 +0200

    OWCommentThreadCellOption implementaion

--- Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/MiscellaneousSettings/OWCommentThreadCellOption.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/MiscellaneousSettings/OWCommentThreadCellOption.swift
@@ -7,3 +7,77 @@
 //
 
 import Foundation
+import UIKit
+
+enum OWCommentThreadCellOption: CaseIterable {
+    static var allCases: [OWCommentThreadCellOption] {
+        return [.comment(viewModel: OWCommentCellViewModel.stub()),
+                .commentSkeletonShimmering(viewModel: OWCommentSkeletonShimmeringCellViewModel.stub()),
+                .spacer(viewModel: OWSpacerCellViewModel.stub())]
+    }
+
+    case comment(viewModel: OWCommentCellViewModeling)
+    case commentSkeletonShimmering(viewModel: OWCommentSkeletonShimmeringCellViewModeling)
+    case spacer(viewModel: OWSpacerCellViewModeling)
+}
+
+extension OWCommentThreadCellOption {
+    var viewModel: OWCellViewModel {
+        switch self {
+        case .comment(let viewModel):
+            return viewModel
+        case .commentSkeletonShimmering(let viewModel):
+            return viewModel
+        case .spacer(let viewModel):
+            return viewModel
+        }
+    }
+
+    var cellClass: UITableViewCell.Type {
+        // TODO: Return the actual cell type once developed
+        switch self {
+        case .comment:
+            return OWCommentCell.self
+        case .commentSkeletonShimmering:
+            return OWCommentSkeletonShimmeringCell.self
+        case .spacer:
+            return OWSpacerCell.self
+        }
+    }
+}
+
+extension OWCommentThreadCellOption: Equatable {
+    var identifier: String {
+        // TODO: Once developed, return id of the comment/reply/ad for each.
+        // Spacer will also have a specific id which will be generated with "UUID" as the VM created.
+        // This is necessary so we won't have UI animations/flickering when loading the same data.
+        switch self {
+        case .comment(let viewModel):
+            return viewModel.outputs.id
+        case .commentSkeletonShimmering(let viewModel):
+            return viewModel.outputs.id
+        case .spacer(let viewModel):
+            return viewModel.outputs.id
+        }
+    }
+
+    static func == (lhs: OWCommentThreadCellOption, rhs: OWCommentThreadCellOption) -> Bool {
+        switch (lhs, rhs) {
+        case (.comment(_), .comment(_)):
+            return lhs.identifier == rhs.identifier
+        case (.commentSkeletonShimmering(_), .commentSkeletonShimmering(_)):
+            return lhs.identifier == rhs.identifier
+        case (.spacer(_), .spacer(_)):
+            return lhs.identifier == rhs.identifier
+        default:
+            return false
+        }
+    }
+}
+
+extension OWCommentThreadCellOption: OWIdentifiableType {
+    var identity: String {
+        return self.identifier
+    }
+}
+
