f586fd3fdfb76be70d19fb8f8b9b91b874d09957
Alon Haiut <alon.h@openweb.com>
2023-03-12 14:33:28 +0200

    Initial models for authentication manager

---
+++ Spot.IM-Development/SpotImCore/Services/AuthenticationManager/Models/OWUserActions.swift
@@ -0,0 +1,22 @@
+//
+//  OWUserActions.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 12/03/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum OWUserActions {
+    case mutingUser
+    case commenting
+    case votingComment
+    case reportingComment
+    case sharingComment
+    case editingComment
+    case deletingComment
+    case viewingProfile
+    case viewingSelfProfile
+}
+
