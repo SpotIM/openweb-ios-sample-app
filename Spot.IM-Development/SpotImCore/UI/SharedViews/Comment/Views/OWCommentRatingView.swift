31e7b987a4d8bce741cda5508c4aef6213268524
Nogah <nogah@applift-consulting.com>
2023-03-21 09:13:25 +0200

    cleanup colors

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentRatingView.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentRatingView.swift
@@ -13,9 +13,9 @@ import RxCocoa
 class OWCommentRatingView: UIView {
 
     fileprivate struct Metrics {
-        static let voteButtonSize: CGFloat = 32.0
+        static let voteButtonSize: CGFloat = 24.0
         static let voteButtonInset: CGFloat = 4.0
-        static let fontSize: CGFloat = 16.0
+        static let fontSize: CGFloat = 12.0
         static let stackviewHeight: CGFloat = 34.0
         static let identifier = "comment_voting_view_id"
         static let rankUpButtonIdentifier = "comment_voting_view_rank_up_button_id"
@@ -44,7 +44,7 @@ class OWCommentRatingView: UIView {
         let frame = CGRect(x: 0, y: 0, width: Metrics.voteButtonSize, height: Metrics.voteButtonSize)
         let button = SPAnimatedButton(frame: frame, buttonInset: insets)
             .hugContent(axis: .horizontal)
-        button.imageColorOff = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: .light)
+        button.imageColorOff = OWColorPalette.shared.color(type: .textColor2, themeStyle: .light)
         button.brandColor = OWColorPalette.shared.color(type: .brandColor, themeStyle: .light)
         return button
     }()
@@ -59,7 +59,7 @@ class OWCommentRatingView: UIView {
         let frame = CGRect(x: 0, y: 0, width: Metrics.voteButtonSize, height: Metrics.voteButtonSize)
         let button = SPAnimatedButton(frame: frame, buttonInset: insets)
             .hugContent(axis: .horizontal)
-        button.imageColorOff = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: .light)
+        button.imageColorOff = OWColorPalette.shared.color(type: .textColor2, themeStyle: .light)
         button.brandColor = OWColorPalette.shared.color(type: .brandColor, themeStyle: .light)
         return button
     }()
@@ -69,7 +69,7 @@ class OWCommentRatingView: UIView {
             .textAlignment(.center)
             .font(.preferred(style: .regular, of: Metrics.fontSize))
             .hugContent(axis: .horizontal)
-            .textColor(OWColorPalette.shared.color(type: .foreground3Color, themeStyle: .light))
+            .textColor(OWColorPalette.shared.color(type: .textColor2, themeStyle: .light))
     }()
 
     fileprivate lazy var rankDownLabel: UILabel = {
@@ -77,7 +77,7 @@ class OWCommentRatingView: UIView {
             .textAlignment(.center)
             .font(.preferred(style: .regular, of: Metrics.fontSize))
             .hugContent(axis: .horizontal)
-            .textColor(OWColorPalette.shared.color(type: .foreground3Color, themeStyle: .light))
+            .textColor(OWColorPalette.shared.color(type: .textColor2, themeStyle: .light))
     }()
 
     fileprivate lazy var seperetorView: UIView = {
@@ -217,10 +217,10 @@ fileprivate extension OWCommentRatingView {
             .style
             .subscribe(onNext: { [weak self] currentStyle in
                 guard let self = self else { return }
-                self.rankUpButton.imageColorOff = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: currentStyle)
-                self.rankDownButton.imageColorOff = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: currentStyle)
-                self.rankUpLabel.textColor = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: currentStyle)
-                self.rankDownLabel.textColor = OWColorPalette.shared.color(type: .foreground3Color, themeStyle: currentStyle)
+                self.rankUpButton.imageColorOff = OWColorPalette.shared.color(type: .textColor2, themeStyle: currentStyle)
+                self.rankDownButton.imageColorOff = OWColorPalette.shared.color(type: .textColor2, themeStyle: currentStyle)
+                self.rankUpLabel.textColor = OWColorPalette.shared.color(type: .textColor2, themeStyle: currentStyle)
+                self.rankDownLabel.textColor = OWColorPalette.shared.color(type: .textColor2, themeStyle: currentStyle)
             })
             .disposed(by: disposeBag)
 
