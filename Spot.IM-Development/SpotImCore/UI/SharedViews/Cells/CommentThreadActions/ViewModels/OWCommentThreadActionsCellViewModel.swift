439d4128d7dd33391c2403fea7b295a85490a4c9
Alon Shprung <alonshp1@gmail.com>
2023-03-22 15:00:11 +0200

    comment thread actions cell

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/CommentThreadActions/ViewModels/OWCommentThreadActionsCellViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  OWCommentThreadActionsCellViewModel.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 22/03/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol OWCommentThreadActionsCellViewModelingInputs {
+
+}
+
+protocol OWCommentThreadActionsCellViewModelingOutputs {
+    var id: String { get }
+}
+
+protocol OWCommentThreadActionsCellViewModeling: OWCellViewModel {
+    var inputs: OWCommentThreadActionsCellViewModelingInputs { get }
+    var outputs: OWCommentThreadActionsCellViewModelingOutputs { get }
+}
+
+class OWCommentThreadActionsCellViewModel: OWCommentThreadActionsCellViewModeling, OWCommentThreadActionsCellViewModelingInputs, OWCommentThreadActionsCellViewModelingOutputs {
+    var inputs: OWCommentThreadActionsCellViewModelingInputs { return self }
+    var outputs: OWCommentThreadActionsCellViewModelingOutputs { return self }
+
+    let id: String = UUID().uuidString
+}
+
+extension OWCommentThreadActionsCellViewModel {
+    static func stub() -> OWCommentThreadActionsCellViewModeling {
+        return OWCommentThreadActionsCellViewModel()
+    }
+}
