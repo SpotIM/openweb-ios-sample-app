9f9f70d80d2145a63f7b32f87902dc2b26641991
Alon Haiut <alon.h@openweb.com>
2023-03-28 18:21:10 +0300

    Decided to move `OWPreConversationStyle` Equatable to the SDK. Added OWError types which where missing in the non beta config

--- Spot.IM-Development/Spot-IM.Development/Extension/OpenWebSDK+Extensions/OWPreConversationStyle+Extensions.swift
+++ Spot.IM-Development/Spot-IM.Development/Extension/OpenWebSDK+Extensions/OWPreConversationStyle+Extensions.swift
@@ -52,21 +52,4 @@ extension OWPreConversationStyle {
     }
 }
 
-extension OWPreConversationStyle: Equatable {
-    public static func == (lhs: OWPreConversationStyle, rhs: OWPreConversationStyle) -> Bool {
-        switch (lhs, rhs) {
-        case (.compact, .compact):
-            return true
-        case (.ctaButtonOnly, .ctaButtonOnly):
-            return true
-        case (.ctaWithSummary, .ctaWithSummary):
-                return true
-        case (.regular(let lhsNumOfComments), .regular(let rhsNumOfComments)):
-                return lhsNumOfComments == rhsNumOfComments
-        default:
-            return false
-        }
-    }
-}
-
 #endif
