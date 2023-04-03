fd1621a019f75ef82e415b810ae4b07a3364f33b
Alon Shprung <alonshp1@gmail.com>
2023-04-03 13:19:01 +0300

    add thread cells to comment thread cell option

--- Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/MiscellaneousSettings/OWCommentThreadCellOption.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Protocols/MiscellaneousSettings/OWCommentThreadCellOption.swift
@@ -11,14 +11,20 @@ import UIKit
 
 enum OWCommentThreadCellOption: CaseIterable {
     static var allCases: [OWCommentThreadCellOption] {
-        return [.comment(viewModel: OWCommentCellViewModel.stub()),
-                .commentSkeletonShimmering(viewModel: OWCommentSkeletonShimmeringCellViewModel.stub()),
-                .spacer(viewModel: OWSpacerCellViewModel.stub())]
+        return [
+            .comment(viewModel: OWCommentCellViewModel.stub()),
+            .commentSkeletonShimmering(viewModel: OWCommentSkeletonShimmeringCellViewModel.stub()),
+            .spacer(viewModel: OWSpacerCellViewModel.stub()),
+            .commentThreadCollapse(viewModel: OWCommentThreadCollapseCellViewModel.stub()),
+            .commentThreadExpand(viewModel: OWCommentThreadExpandCellViewModel.stub())
+        ]
     }
 
     case comment(viewModel: OWCommentCellViewModeling)
     case commentSkeletonShimmering(viewModel: OWCommentSkeletonShimmeringCellViewModeling)
     case spacer(viewModel: OWSpacerCellViewModeling)
+    case commentThreadCollapse(viewModel: OWCommentThreadCollapseCellViewModeling)
+    case commentThreadExpand(viewModel: OWCommentThreadExpandCellViewModeling)
 }
 
 extension OWCommentThreadCellOption {
@@ -29,6 +35,10 @@ extension OWCommentThreadCellOption {
         case .commentSkeletonShimmering(let viewModel):
             return viewModel
         case .spacer(let viewModel):
+            return viewModel
+        case .commentThreadCollapse(let viewModel):
+            return viewModel
+        case .commentThreadExpand(let viewModel):
             return viewModel
         }
     }
@@ -41,6 +51,10 @@ extension OWCommentThreadCellOption {
             return OWCommentSkeletonShimmeringCell.self
         case .spacer:
             return OWSpacerCell.self
+        case .commentThreadCollapse:
+            return OWCommentThreadCollapseCell.self
+        case .commentThreadExpand:
+            return OWCommentThreadExpandCell.self
         }
     }
 }
@@ -54,6 +68,10 @@ extension OWCommentThreadCellOption: Equatable {
             return viewModel.outputs.id
         case .spacer(let viewModel):
             return viewModel.outputs.id
+        case .commentThreadCollapse(let viewModel):
+            return viewModel.outputs.id
+        case .commentThreadExpand(let viewModel):
+            return viewModel.outputs.id
         }
     }
 
@@ -64,6 +82,10 @@ extension OWCommentThreadCellOption: Equatable {
         case (.commentSkeletonShimmering(_), .commentSkeletonShimmering(_)):
             return lhs.identifier == rhs.identifier
         case (.spacer(_), .spacer(_)):
+            return lhs.identifier == rhs.identifier
+        case (.commentThreadCollapse(_), .commentThreadCollapse(_)):
+            return lhs.identifier == rhs.identifier
+        case (.commentThreadExpand(_), .commentThreadExpand(_)):
             return lhs.identifier == rhs.identifier
         default:
             return false
