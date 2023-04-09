b03efc98b87f3729cb508a489072f0eeef21a417
Alon Shprung <alonshp1@gmail.com>
2023-04-09 16:12:40 +0300

    tap on comment thread cells

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/CommentThreadActions/ViewModels/OWCommentThreadExpandCellViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Cells/CommentThreadActions/ViewModels/OWCommentThreadExpandCellViewModel.swift
@@ -16,6 +16,7 @@ protocol OWCommentThreadExpandCellViewModelingInputs {
 protocol OWCommentThreadExpandCellViewModelingOutputs {
     var id: String { get }
     var commentPresentationData: OWCommentPresentationData { get }
+    var commentActionsVM: OWCommentThreadActionsViewModel { get }
 }
 
 protocol OWCommentThreadExpandCellViewModeling: OWCellViewModel {
@@ -31,13 +32,20 @@ class OWCommentThreadExpandCellViewModel: OWCommentThreadExpandCellViewModeling,
 
     let commentPresentationData: OWCommentPresentationData
 
+    let commentActionsVM: OWCommentThreadActionsViewModel
+
     init(data: OWCommentPresentationData) {
         self.commentPresentationData = data
+
+        // TODO - Add data
+        commentActionsVM = OWCommentThreadActionsViewModel()
     }
 
     init() {
         // TODO - make OWCommentPresentationData a class
         self.commentPresentationData = OWCommentPresentationData(id: "", shouldShowReplies: false, repliesIds: [], totalRepliesCount: 0, repliesOffset: 0, repliesPresentation: [])
+
+        commentActionsVM = OWCommentThreadActionsViewModel()
     }
 }
 
