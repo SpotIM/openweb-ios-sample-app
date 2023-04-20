406b8aacf8281fecc894b9a2c0e2e436a90fce8a
Alon Haiut <alon.h@openweb.com>
2023-04-20 18:14:08 +0300

    commit in the middle, need to do something else

---
+++ Spot.IM-Development/Spot-IM.Development/UI/Beta/TestingPlayground/ViewModels/TestingPlaygroundIndependentViewViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TestingPlaygroundIndependentViewViewModel.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 20/04/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
