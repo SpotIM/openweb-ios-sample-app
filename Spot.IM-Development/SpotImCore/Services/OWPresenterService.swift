a57cc08ae5e1a2dbdb6a922ede55282d22f2571c
Nogah <nogah@applift-consulting.com>
2023-04-24 16:20:22 +0300

    Add menu to show popover

--- Spot.IM-Development/SpotImCore/Services/OWPresenterService.swift
+++ Spot.IM-Development/SpotImCore/Services/OWPresenterService.swift
@@ -11,8 +11,8 @@ import RxSwift
 
 protocol OWPresenterServicing {
     // TODO: should return some observable for actions
-    func showAlert() -> Observable<UIAlertType>
-    func showPopoverOptions(source: UIView?) -> Observable<UIAlertType>
+    func showAlert(title: String, message: String, actions: [UIRxAlertAction]) -> Observable<UIAlertType>
+    func showMenu(source: UIButton?, actions: [UIRxAlertAction]) -> Observable<UIAlertType>
 }
 
 class OWPresenterService: OWPresenterServicing {
@@ -24,26 +24,42 @@ class OWPresenterService: OWPresenterServicing {
     }
 
     // TODO
-    func showAlert() -> Observable<UIAlertType> {
+    func showAlert(title: String, message: String, actions: [UIRxAlertAction]) -> Observable<UIAlertType> {
         guard let navController = routering?.routering.navigationController
         else { return .empty() }
 
-        // TODO: alert content :)
         return UIAlertController.rx.show(onViewController: navController,
-                                         title: "ALERT",
-                                         message: LocalizationManager.localizedString(key: "No available options"),
-                                         actions: [.init(title: "close", style: .cancel)])
+                                         preferredStyle: .alert,
+                                         title: title,
+                                         message: message,
+                                         actions: actions)
     }
 
-    func showPopoverOptions(source: UIView?) -> Observable<UIAlertType> {
-        guard let navController = routering?.routering.navigationController
-        else { return .empty() }
-
-        // TODO: alert content :)
-        return UIPopoverPresentationController.rx.show(onViewController: navController,
-                                                       sourceView: source,
-                                                       title: "POPOVERR",
-                                                       message: LocalizationManager.localizedString(key: "No available options"),
-                                                       actions: [.init(title: "close", style: .cancel), .init(title: "optionnn", style: .default)])
+    func showMenu(source: UIButton?, actions: [UIRxAlertAction]) -> Observable<UIAlertType> {
+        // Add UIMenu for iOS 14+
+        if #available(iOS 14.0, *) {
+            guard let source = source else { return .empty() }
+            // TODO: items
+            let menuItems: [UIAction] = actions.map {
+                var action = UIAction(title: $0.title, handler: {_ in })
+                action.state = .on
+                return action
+            }
+            var menu: UIMenu {
+                return UIMenu(identifier: nil, options: [], children: menuItems)
+            }
+            source.menu = menu
+            source.showsMenuAsPrimaryAction = true
+            return .empty()
+        } else {
+            // Fallback on earlier versions - show actionSheet
+            guard let navController = routering?.routering.navigationController
+            else { return .empty() }
+            return UIAlertController.rx.show(onViewController: navController,
+                                             preferredStyle: .actionSheet,
+                                             title: nil,
+                                             message: nil,
+                                             actions: actions)
+        }
     }
 }
