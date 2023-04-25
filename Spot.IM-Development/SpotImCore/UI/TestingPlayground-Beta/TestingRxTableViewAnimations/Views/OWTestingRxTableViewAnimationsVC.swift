d813bc19ec9ad00558f9505e15f4f92750ed6ac0
Alon Haiut <alon.h@openweb.com>
2023-04-25 19:18:45 +0300

    Added infra for VC and View for testing table view animations

--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/OWTestingRxTableViewAnimationsVC.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/OWTestingRxTableViewAnimationsVC.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2023 Spot.IM. All rights reserved.
 //
 
-import Foundation
+#if BETA
+
+import UIKit
+
+class OWTestingRxTableViewAnimationsVC: UIViewController {
+    fileprivate struct Metrics { }
+
+    fileprivate let viewModel: OWTestingRxTableViewAnimationsViewModeling
+
+    fileprivate lazy var testingRxTableViewAnimationsView: OWTestingRxTableViewAnimationsView = {
+        return OWTestingRxTableViewAnimationsView(viewModel: viewModel.outputs.viewVM)
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(viewModel: OWTestingRxTableViewAnimationsViewModeling) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func loadView() {
+        super.loadView()
+        setupUI()
+    }
+}
+
+fileprivate extension OWTestingRxTableViewAnimationsVC {
+    func setupUI() {
+        view.addSubview(testingRxTableViewAnimationsView)
+        testingRxTableViewAnimationsView.OWSnp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+#endif
--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/OWTestingRxTableViewAnimationsView.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/OWTestingRxTableViewAnimationsView.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2023 Spot.IM. All rights reserved.
 //
 
-import Foundation
+#if BETA
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class OWTestingRxTableViewAnimationsView: UIView {
+
+    fileprivate struct Metrics {
+        static let cellsGeneratorViewHeight: CGFloat = 60.0
+        static let horizontalMargin: CGFloat = 15.0
+    }
+
+    fileprivate lazy var cellsGeneratorView: UIView = {
+        return UIView()
+            .backgroundColor(.white)
+    }()
+
+    fileprivate var viewModel: OWTestingRxTableViewAnimationsViewViewModeling!
+
+    init(viewModel: OWTestingRxTableViewAnimationsViewViewModeling) {
+        super.init(frame: .zero)
+        self.viewModel = viewModel
+
+        setupUI()
+        setupObservers()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+fileprivate extension OWTestingRxTableViewAnimationsView {
+    func setupUI() {
+        self.backgroundColor = .white
+
+    }
+
+    func setupObservers() {
+
+    }
+}
+
+#endif
