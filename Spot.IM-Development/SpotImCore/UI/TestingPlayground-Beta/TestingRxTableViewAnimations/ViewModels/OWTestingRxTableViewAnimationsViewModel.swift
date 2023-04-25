2323c44193490504b9ea7c3367a59f6b1082f29a
Alon Haiut <alon.h@openweb.com>
2023-04-25 19:18:45 +0300

    Added infra for VC and View for testing table view animations

--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/ViewModels/OWTestingRxTableViewAnimationsViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/ViewModels/OWTestingRxTableViewAnimationsViewModel.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2023 Spot.IM. All rights reserved.
 //
 
+#if BETA
+
 import Foundation
+import RxSwift
+
+protocol OWTestingRxTableViewAnimationsViewModelingInputs { }
+
+protocol OWTestingRxTableViewAnimationsViewModelingOutputs {
+    var viewVM: OWTestingRxTableViewAnimationsViewViewModeling { get }
+}
+
+protocol OWTestingRxTableViewAnimationsViewModeling {
+    var inputs: OWTestingRxTableViewAnimationsViewModelingInputs { get }
+    var outputs: OWTestingRxTableViewAnimationsViewModelingOutputs { get }
+}
+
+class OWTestingRxTableViewAnimationsViewModel: OWTestingRxTableViewAnimationsViewModeling,
+                                OWTestingRxTableViewAnimationsViewModelingInputs,
+                                OWTestingRxTableViewAnimationsViewModelingOutputs {
+    var inputs: OWTestingRxTableViewAnimationsViewModelingInputs { return self }
+    var outputs: OWTestingRxTableViewAnimationsViewModelingOutputs { return self }
+
+    lazy var viewVM: OWTestingRxTableViewAnimationsViewViewModeling = {
+        return OWTestingRxTableViewAnimationsViewViewModel()
+    }()
+}
+
+#endif
