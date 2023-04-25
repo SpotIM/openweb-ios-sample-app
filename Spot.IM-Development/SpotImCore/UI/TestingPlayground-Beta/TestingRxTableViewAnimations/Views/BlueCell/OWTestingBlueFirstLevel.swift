bf400f0e33879220a95811b931e8b6ef02250853
Alon Haiut <alon.h@openweb.com>
2023-04-25 14:36:29 +0300

    Started to implement the testing cells UI+logic

--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/BlueCell/OWTestingBlueFirstLevel.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/BlueCell/OWTestingBlueFirstLevel.swift
@@ -14,7 +14,24 @@ class OWTestingBlueFirstLevel: UIView {
 
     fileprivate struct Metrics {
         static let borderWidth: CGFloat = 2.0
+        static let buttonsMargin: CGFloat = 15.0
     }
+
+    fileprivate lazy var btnRemove: UIButton = {
+        return "Remove"
+            .button
+            .backgroundColor(.lightGray)
+            .textColor(.black)
+            .font(OWFontBook.shared.font(style: .regular, size: 15.0))
+    }()
+
+    fileprivate lazy var btnState: UIButton = {
+        return "Expand"
+            .button
+            .backgroundColor(.lightGray)
+            .textColor(.black)
+            .font(OWFontBook.shared.font(style: .regular, size: 15.0))
+    }()
 
     fileprivate var viewModel: OWTestingBlueFirstLevelViewModeling!
 
@@ -36,6 +53,18 @@ fileprivate extension OWTestingBlueFirstLevel {
     func setupUI() {
         self.backgroundColor = .blue
         self.border(width: Metrics.borderWidth, color: .gray)
+
+        self.addSubview(btnState)
+        btnState.OWSnp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(Metrics.buttonsMargin)
+        }
+
+        self.addSubview(btnRemove)
+        btnRemove.OWSnp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-Metrics.buttonsMargin)
+        }
     }
 }
 
