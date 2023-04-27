12567f2fecdbcf5468a68a77a616e5f369c32afa
Alon Haiut <alon.h@openweb.com>
2023-04-27 19:34:20 +0300

    Added UI infra for adding cells and removing them for the testing purposes

---
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Models/OWTestingCellsGeneratorRequiredData.swift
@@ -0,0 +1,18 @@
+//
+//  OWTestingCellsGeneratorRequiredData.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 27/04/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+#if BETA
+
+import UIKit
+
+struct OWTestingCellsGeneratorRequiredData {
+    let color: UIColor
+    let title: String
+}
+
+#endif
