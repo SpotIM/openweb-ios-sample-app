9e152d3ff11e040ed5b45b6e57ad0636405c9dd1
Alon Haiut <alon.h@openweb.com>
2023-04-30 23:46:26 +0300

    Expand animation on red cells works as well by the text length!

--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/RedCell/OWTestingRedSecondLevel.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/RedCell/OWTestingRedSecondLevel.swift
@@ -21,12 +21,24 @@ class OWTestingRedSecondLevel: UIView {
         static let padding: CGFloat = 8.0
         static let collapsedCellContentHeight: CGFloat = 110.0
         static let expandedCellContentHeight: CGFloat = 170.0
+        static let shortText = "This is a short text"
+        // swiftlint:disable line_length
+        static let longText = "This is a long text just to show how a cell can change it's height via a change in a UILabel length.\nA new line just to make it longer.\nAre you still reading that nonsense?"
+        // swiftlint:enable line_length
     }
 
     fileprivate lazy var lblIdentifier: UILabel = {
         return UILabel()
             .textColor(.black)
             .numberOfLines(1)
+            .font(OWFontBook.shared.font(style: .regular, size: 15.0))
+    }()
+
+    fileprivate lazy var lblLongStuff: UILabel = {
+        return Metrics.shortText
+            .label
+            .textColor(.black)
+            .numberOfLines(0)
             .font(OWFontBook.shared.font(style: .regular, size: 15.0))
     }()
 
@@ -66,6 +78,7 @@ class OWTestingRedSecondLevel: UIView {
         self.viewModel = viewModel
         self.disposeBag = DisposeBag()
         setupObservers()
+        ensureCorrectState()
     }
 }
 
@@ -75,24 +88,28 @@ fileprivate extension OWTestingRedSecondLevel {
             .border(width: Metrics.borderWidth, color: .gray)
             .corner(radius: Metrics.roundCorners)
 
-        self.OWSnp.makeConstraints { make in
-            make.height.equalTo(Metrics.collapsedCellContentHeight)
-        }
-
         self.addSubview(lblIdentifier)
         lblIdentifier.OWSnp.makeConstraints { make in
             make.top.equalToSuperview().offset(Metrics.margin)
             make.leading.trailing.equalToSuperview().inset(Metrics.margin)
         }
 
+        self.addSubview(lblLongStuff)
+        lblLongStuff.OWSnp.makeConstraints { make in
+            make.top.equalTo(lblIdentifier.OWSnp.bottom).offset(Metrics.margin)
+            make.leading.trailing.equalToSuperview().inset(Metrics.margin)
+        }
+
         self.addSubview(btnState)
         btnState.OWSnp.makeConstraints { make in
+            make.top.equalTo(lblLongStuff.OWSnp.bottom).offset(Metrics.margin)
             make.bottom.equalToSuperview().offset(-Metrics.margin)
             make.leading.equalToSuperview().offset(Metrics.margin)
         }
 
         self.addSubview(btnRemove)
         btnRemove.OWSnp.makeConstraints { make in
+            make.top.equalTo(lblLongStuff.OWSnp.bottom).offset(Metrics.margin)
             make.bottom.equalToSuperview().offset(-Metrics.margin)
             make.trailing.equalToSuperview().offset(-Metrics.margin)
         }
@@ -107,6 +124,66 @@ fileprivate extension OWTestingRedSecondLevel {
 
         btnState.rx.tap
             .bind(to: viewModel.inputs.changeCellStateTap)
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.changeCellState
+            .skip(1)
+            .map{ [weak self] state -> String in
+                let text: String
+                switch state {
+                case .collapsed:
+                    text = Metrics.shortText
+                case .expanded:
+                    text = Metrics.longText
+                }
+
+                return text
+            }
+            .bind(to: lblLongStuff.rx.text)
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.changeCellState
+            .map { state -> String in
+                let text: String
+                switch state {
+                case .collapsed:
+                    text = "Expand"
+                case .expanded:
+                    text = "Collapse"
+                }
+
+                return text
+            }
+            .subscribe(onNext: { [weak self] title in
+                self?.btnState.setTitle(title, for: .normal)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    /*
+     This is essential as the cells being reused and sometime and expanded cell might be reused.
+     Here we are ensuring that as soon as the cell configured with a new VM - the correct size updated via the VM output which we use instantly.
+     Note that `changeCellState` have share(reply:1) - means that we getting instantly the last value it had.
+     Setting collapsed height in prepare for reuse won't be enough - as this will cause expanded cells to show as collapsed after scrolling away and back to them.
+     We must ensure correct size as soon as we configure with a new VM.
+     Lastly, since the cell is going to be shown, an animation already happen it the table view - that's why we don't need to update the view side about this height change as the table view already in "animation" mode while the cell created after the cell re-added to the table (from prepareForReuse).
+     */
+    func ensureCorrectState() {
+       viewModel.outputs.changeCellState
+            .take(1)
+            .subscribe(onNext: { [weak self] state in
+                guard let self = self else { return }
+
+                let text: String
+                switch state {
+                case .collapsed:
+                    text = Metrics.shortText
+                case .expanded:
+                    text = Metrics.longText
+                }
+
+                self.lblLongStuff.text = text
+            })
             .disposed(by: disposeBag)
     }
 }
