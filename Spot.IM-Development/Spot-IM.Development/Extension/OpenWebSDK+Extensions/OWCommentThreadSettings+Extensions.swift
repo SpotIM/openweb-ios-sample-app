70c8ec0b8e4ea66cf234270a42441998551d10cd
Alon Shprung <alonshp1@gmail.com>
2023-05-31 17:47:09 +0300

    comment thread default id in settings screen

---
+++ Spot.IM-Development/Spot-IM.Development/Extension/OpenWebSDK+Extensions/OWCommentThreadSettings+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  OWCommentThreadSettings+Extensions.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Shprung on 31/05/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+import SpotImCore
+
+extension OWCommentThreadSettings {
+    static var defaultCommentId = "sp_eCIlROSD_sdk1_c_2LE7TOJIGuzxgkRhyByPUFA73oq_r_2N0Kd6WueGMtK5LYvjTcm4hEMom"
+}
