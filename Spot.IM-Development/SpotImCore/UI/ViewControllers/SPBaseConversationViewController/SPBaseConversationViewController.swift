faf7cbdf8701fbb2d14f53862a6d4cd95a71d97b
Alon Shprung <alonshp1@gmail.com>
2023-06-05 20:53:48 +0300

    prepare show ui activity controller in presentation service

--- Spot.IM-Development/SpotImCore/UI/ViewControllers/SPBaseConversationViewController/SPBaseConversationViewController.swift
+++ Spot.IM-Development/SpotImCore/UI/ViewControllers/SPBaseConversationViewController/SPBaseConversationViewController.swift
@@ -1031,6 +1031,7 @@ extension SPBaseConversationViewController: CommentsActionDelegate {
                     title: LocalizationManager.localizedString(key: "Oops..."),
                     message: error.localizedDescription
                 )
+                // here
             } else if let url = url {
                 let activityViewController = UIActivityViewController(activityItems: [url], applicationActivities: nil)
                 activityViewController.popoverPresentationController?.sourceView = sender
