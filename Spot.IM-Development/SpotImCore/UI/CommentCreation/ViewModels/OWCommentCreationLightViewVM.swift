a5cc5feb95efa66faa9f8718bccf5ada6de7b573
Alon Shprung <alonshp1@gmail.com>
2023-06-07 17:46:24 +0300

    prepare files

---
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/ViewModels/OWCommentCreationLightViewVM.swift
@@ -0,0 +1,9 @@
+//
+//  OWCommentCreationLightViewVM.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 07/06/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
