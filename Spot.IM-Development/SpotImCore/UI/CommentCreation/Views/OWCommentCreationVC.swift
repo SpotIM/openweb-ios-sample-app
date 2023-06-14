ef22b2b313169ca512b888f3ad4f4b74c1cea394
Alon Shprung <alonshp1@gmail.com>
2023-06-14 18:08:28 +0300

    footer animation with keyboard

--- Spot.IM-Development/SpotImCore/UI/CommentCreation/Views/OWCommentCreationVC.swift
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/Views/OWCommentCreationVC.swift
@@ -35,11 +35,17 @@ class OWCommentCreationVC: UIViewController {
     override func loadView() {
         super.loadView()
         setupViews()
+        setupObservers()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.inputs.viewDidLoad.onNext()
+        registerForKeyboardNotifications()
+    }
+
+    deinit {
+        unregisterFromKeyboardNotifications()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -53,9 +59,50 @@ class OWCommentCreationVC: UIViewController {
 
 fileprivate extension OWCommentCreationVC {
     func setupViews() {
+        self.view.backgroundColor = OWColorPalette.shared.color(type: .backgroundColor2, themeStyle: .light)
+
         view.addSubview(commentCreationView)
         commentCreationView.OWSnp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.top.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+
+    func setupObservers() {
+        OWSharedServicesProvider.shared.themeStyleService()
+            .style
+            .subscribe(onNext: { [weak self] currentStyle in
+                guard let self = self else { return }
+                self.view.backgroundColor = OWColorPalette.shared.color(type: .backgroundColor2, themeStyle: currentStyle)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension OWCommentCreationVC: OWKeyboardHandable {
+    func keyboardWillShow(_ notification: Notification) {
+        guard
+            let expandedKeyboardHeight = notification.keyboardSize?.height,
+            let animationDuration = notification.keyboardAnimationDuration
+            else { return }
+        let bottomPadding: CGFloat
+        if #available(iOS 11.0, *) {
+            bottomPadding = tabBarController?.tabBar.frame.height ?? UIApplication.shared.windows[0].safeAreaInsets.bottom
+        } else {
+            bottomPadding = 0
+        }
+        self.commentCreationView.OWSnp.updateConstraints { make in
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-(expandedKeyboardHeight - bottomPadding))
+        }
+        UIView.animate(withDuration: animationDuration) { [weak self] in
+            guard let self = self else { return }
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    func keyboardWillHide(_ notification: Notification) {
+        commentCreationView.OWSnp.updateConstraints { make in
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
     }
 }
