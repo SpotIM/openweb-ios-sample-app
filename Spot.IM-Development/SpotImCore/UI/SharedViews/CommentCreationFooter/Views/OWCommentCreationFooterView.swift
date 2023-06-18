bd33cd1397b413fa28b8a7642e00d98bf5e733c7
Alon Shprung <alonshp1@gmail.com>
2023-06-18 15:27:06 +0300

    prepare footer view

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreationFooter/Views/OWCommentCreationFooterView.swift
@@ -0,0 +1,45 @@
+//
+//  OWCommentCreationFooterView.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 18/06/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class OWCommentCreationFooterView: UIView {
+
+    fileprivate let disposeBag = DisposeBag()
+    fileprivate let viewModel: OWCommentCreationFooterViewModeling
+
+    init(with viewModel: OWCommentCreationFooterViewModeling) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+
+        self.enforceSemanticAttribute()
+
+        setupUI()
+        setupObservers()
+        applyAccessibility()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+fileprivate extension OWCommentCreationFooterView {
+    func setupUI() {
+        backgroundColor = .blue
+    }
+
+    func setupObservers() {
+        
+    }
+
+    func applyAccessibility() {
+
+    }
+}
