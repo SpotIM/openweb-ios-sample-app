8044839ad4cb79e543db6b16a311767a9e45bcdf
Nogah <nogah@applift-consulting.com>
2023-06-19 13:13:53 +0300

    cleanups

--- Spot.IM-Development/SpotImCore/Services/OWPresenterService.swift
+++ Spot.IM-Development/SpotImCore/Services/OWPresenterService.swift
@@ -12,14 +12,7 @@ import UIKit
 
 protocol OWPresenterServicing {
     func showAlert(title: String, message: String, actions: [OWRxPresenterAction], viewableMode: OWViewableMode) -> Observable<OWRxPresenterResponseType>
-    func showMenu(title: String?, actions: [OWRxPresenterAction], viewableMode: OWViewableMode) -> Observable<OWRxPresenterResponseType>
     func showMenu(actions: [OWMenuSelectionItem], sender: UIView, viewableMode: OWViewableMode)
-}
-
-extension OWPresenterServicing {
-    func showMenu(title: String? = nil, actions: [OWRxPresenterAction], viewableMode: OWViewableMode) -> Observable<OWRxPresenterResponseType> {
-        return showMenu(title: title, actions: actions, viewableMode: viewableMode)
-    }
 }
 
 class OWPresenterService: OWPresenterServicing {
@@ -37,24 +30,11 @@ class OWPresenterService: OWPresenterServicing {
                                          actions: actions)
     }
 
-    func showMenu(title: String?, actions: [OWRxPresenterAction], viewableMode: OWViewableMode) -> Observable<OWRxPresenterResponseType> {
-        // TODO: show proper menu instead of actionSheet
-        guard let presenterVC = getPresenterVC(for: viewableMode)
-        else { return .empty() }
-        return .empty()
-//        return UIAlertController.rx.show(onViewController: presenterVC,
-//                                         preferredStyle: .actionSheet,
-//                                         title: title,
-//                                         message: nil,
-//                                         actions: actions)
-    }
-
     func showMenu(actions: [OWMenuSelectionItem], sender: UIView, viewableMode: OWViewableMode) {
         guard let presenterVC = getPresenterVC(for: viewableMode) else { return }
-        print("NOGAH - open menu")
         let menuVM = OWMenuSelectionViewModel(items: actions)
         // OWMenuSelectionWrapperView is addind himself to the presenterVC with propper constraints
-        let wrapperView = OWMenuSelectionWrapperView(menuVM: menuVM, senderView: sender, presenterVC: presenterVC)
+        let _ = OWMenuSelectionWrapperView(menuVM: menuVM, senderView: sender, presenterVC: presenterVC)
     }
 }
 
