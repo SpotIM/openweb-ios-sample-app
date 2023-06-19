703e9bd1760299c1b08bb78723a3762b3e9b6b3e
Nogah <nogah@applift-consulting.com>
2023-06-19 11:37:45 +0300

    sFix

--- Spot.IM-Development/SpotImCore/UI/SharedViews/MenuSelection/Views/OWMenuSelectionCell.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/MenuSelection/Views/OWMenuSelectionCell.swift
@@ -63,8 +63,6 @@ fileprivate extension OWMenuSelectionCell {
             .bind(to: label.rx.text)
             .disposed(by: disposeBag)
 
-        
-
         OWSharedServicesProvider.shared.themeStyleService()
             .style
             .subscribe(onNext: { [weak self] currentStyle in
