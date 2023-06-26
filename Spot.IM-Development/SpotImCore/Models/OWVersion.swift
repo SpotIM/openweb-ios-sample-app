518c150690739c4875743ea5eba5279f59f63f1b
Nogah <nogah@applift-consulting.com>
2023-06-26 16:30:52 +0300

    OWVersion

--- Spot.IM-Development/SpotImCore/Models/OWVersion.swift
+++ Spot.IM-Development/SpotImCore/Models/OWVersion.swift
@@ -9,19 +9,35 @@
 import Foundation
 
 class OWVersion {
-    fileprivate major: Int
-    fileprivate minor: Int
-    fileprivate patch: Int
+    fileprivate let major: Int
+    fileprivate let minor: Int
+    fileprivate let patch: Int
 
     init?(from versionString: String) {
         do {
             let versionDelimiter = "."
             let versions = versionString.components(separatedBy: versionDelimiter)
-            if let a = versions[0] {
-                major = Int(major)
-            }
+            major = Int(versions[0])!
+            minor = versions.count > 1 ? Int(versions[1])! : 0
+            patch = versions.count > 2 ? Int(versions[2])! : 0
         } catch {
             // TODO: Throw error?
         }
+    }
+}
+
+extension OWVersion: Comparable, Equatable {
+    static func < (lhs: OWVersion, rhs: OWVersion) -> Bool {
+        if lhs.major != rhs.major {
+            return lhs.major < rhs.major
+        } else if lhs.minor != rhs.minor {
+            return lhs.minor < rhs.minor
+        } else {
+            return lhs.patch < rhs.patch
+        }
+    }
+
+    static func == (lhs: OWVersion, rhs: OWVersion) -> Bool {
+        return lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rhs.patch
     }
 }
