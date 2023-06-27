6d136e463443339695bbfc8f281479fd7f25eaf7
Alon Haiut <alon.h@openweb.com>
2023-06-27 13:30:40 +0300

    Added ToolbarElementAction and ToolbarElementModel models

---
+++ Spot.IM-Development/Spot-IM.Development/UI/Beta/ToolbarFloatingCommentCreation/Models/ToolbarElementModel.swift
@@ -0,0 +1,14 @@
+//
+//  ToolbarElementModel.swift
+//  Spot-IM.Development
+//
+//  Created by Alon Haiut on 27/06/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct ToolbarElementModel {
+    let emoji: Character
+    let action: ToolbarElementAction
+}
