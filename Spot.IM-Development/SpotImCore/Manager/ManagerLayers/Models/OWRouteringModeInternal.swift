41122c8f6412a34a9e5a481ccdf38459afa598d5
Alon Haiut <alon.h@openweb.com>
2023-06-29 18:52:09 +0300

    Added `activeRouteringMode` concept so it will be easy to understand which is the current router mode (UIFlows vs UIVIews)

---
+++ Spot.IM-Development/SpotImCore/Manager/ManagerLayers/Models/OWRouteringModeInternal.swift
@@ -0,0 +1,14 @@
+//
+//  OWRouteringModeInternal.swift
+//  SpotImCore
+//
+//  Created by Alon Haiut on 29/06/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum OWRouteringModeInternal {
+    case routering(routering: OWRoutering)
+    case compactRoutering(compactRoutering: OWCompactRoutering)
+}
--- Spot.IM-Development/SpotImCore/Manager/ManagerLayers/OWUILayer.swift
+++ Spot.IM-Development/SpotImCore/Manager/ManagerLayers/OWUILayer.swift
@@ -9,11 +9,15 @@
 import Foundation
 import RxSwift
 
-class OWUILayer: OWUI, OWUIFlows, OWUIViews, OWRouteringCompatible, OWCompactRouteringCompatible {
+class OWUILayer: OWUI, OWUIFlows, OWUIViews, OWRouteringModeProtocol, OWCompactRouteringCompatible, OWRouteringCompatible {
     var flows: OWUIFlows { return self }
     var views: OWUIViews { return self }
     var customizations: OWCustomizations { return self._customizations }
     var authenticationUI: OWUIAuthentication { return self._authenticationUI }
+
+    lazy var activeRouteringMode: OWRouteringModeInternal = {
+        return .routering(routering: routering)
+    }()
 
     var routering: OWRoutering {
         return flowsSdkCoordinator.routering
@@ -67,6 +71,9 @@ extension OWUILayer {
                                                 presentationalMode: presentationalMode,
                                                 callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .partOfFlow)
+        })
         .subscribe(onNext: { result in
             completion(.success(result.toShowable()))
         }, onError: { err in
@@ -101,6 +108,9 @@ extension OWUILayer {
                                                  presentationalMode: presentationalMode,
                                                  callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .partOfFlow)
+        })
         .subscribe(onNext: { result in
             switch result {
             case .loadedToScreen:
@@ -145,6 +155,9 @@ extension OWUILayer {
                                                     presentationalMode: presentationalMode,
                                                     callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .partOfFlow)
+        })
         .subscribe(onNext: { result in
             switch result {
             case .loadedToScreen:
@@ -191,6 +204,9 @@ extension OWUILayer {
                                                     presentationalMode: presentationalMode,
                                                     callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .partOfFlow)
+        })
         .subscribe(onNext: { result in
             switch result {
             case .loadedToScreen:
@@ -229,6 +245,9 @@ extension OWUILayer {
                                                            presentationalMode: presentationalMode,
                                                            callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .partOfFlow)
+        })
         .subscribe(onNext: { result in
             switch result {
             case .loadedToScreen:
@@ -271,6 +290,9 @@ extension OWUILayer {
                                                 callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
         .take(1)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .independent)
+        })
         .subscribe(onNext: { result in
             completion(.success(result.toShowable()))
         }, onError: { err in
@@ -303,6 +325,9 @@ extension OWUILayer {
                                                 callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
         .take(1)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .independent)
+        })
         .subscribe(onNext: { result in
             completion(.success(result.toShowable()))
         }, onError: { err in
@@ -333,6 +358,9 @@ extension OWUILayer {
                                                 callbacks: callbacks)
         .observe(on: MainScheduler.asyncInstance)
         .take(1)
+        .do(onNext: { [weak self] _ in
+            self?.setActiveRouter(for: .independent)
+        })
         .subscribe(onNext: { result in
             completion(.success(result.toShowable()))
         }, onError: { err in
@@ -357,5 +385,14 @@ fileprivate extension OWUILayer {
     func prepareForNewFlow() {
         // Discard any previous subscription to other flows
         flowDisposeBag = DisposeBag()
+    }
+
+    func setActiveRouter(for viewableMode: OWViewableMode) {
+        switch viewableMode {
+        case .independent:
+            activeRouteringMode = .compactRoutering(compactRoutering: self.compactRoutering)
+        case .partOfFlow:
+            activeRouteringMode = .routering(routering: self.routering)
+        }
     }
 }
