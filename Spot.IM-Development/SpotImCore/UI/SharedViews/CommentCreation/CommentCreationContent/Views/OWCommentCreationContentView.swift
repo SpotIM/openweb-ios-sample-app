a3062ae442ca8e8fcafcfa56124e03e1ae240e3a
Alon Shprung <alonshp1@gmail.com>
2023-07-03 17:42:05 +0300

    add comment creation content view and vm files

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreation/CommentCreationContent/Views/OWCommentCreationContentView.swift
@@ -0,0 +1,9 @@
+//
+//  OWCommentCreationContentView.swift
+//  SpotImCore
+//
+//  Created by Alon Shprung on 03/07/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
