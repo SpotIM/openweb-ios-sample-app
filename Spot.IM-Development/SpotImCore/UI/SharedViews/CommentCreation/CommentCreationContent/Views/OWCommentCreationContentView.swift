878db82e81610d06b610dd1072ca61b2657c0f59
Alon Shprung <alonshp1@gmail.com>
2023-07-03 17:45:17 +0300

    prepare comment creation content

--- Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreation/CommentCreationContent/Views/OWCommentCreationContentView.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreation/CommentCreationContent/Views/OWCommentCreationContentView.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import RxSwift
+
+class OWCommentCreationContentView: UIView {
+    fileprivate struct Metrics {
+
+    }
+
+    fileprivate let disposeBag = DisposeBag()
+    fileprivate let viewModel: OWCommentCreationContentViewModeling
+
+
+    init(with viewModel: OWCommentCreationContentViewModeling) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+
+        self.enforceSemanticAttribute()
+
+        setupUI()
+        setupObservers()
+        applyAccessibility()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+fileprivate extension OWCommentCreationContentView {
+    func setupUI() {
+
+    }
+
+    func setupObservers() {
+        OWSharedServicesProvider.shared.themeStyleService()
+            .style
+            .subscribe(onNext: { [weak self] currentStyle in
+                guard let self = self else { return }
+            }).disposed(by: disposeBag)
+    }
+
+    func applyAccessibility() {
+
+    }
+}
