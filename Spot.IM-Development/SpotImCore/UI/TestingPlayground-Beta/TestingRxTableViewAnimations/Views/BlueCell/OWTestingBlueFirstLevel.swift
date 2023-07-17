2bf1e1da216d0ee6fc5a418ff48bc08668848e28
Alon Shprung <alonshp1@gmail.com>
2023-07-17 14:49:39 +0300

    undo changes

--- Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/BlueCell/OWTestingBlueFirstLevel.swift
+++ Spot.IM-Development/SpotImCore/UI/TestingPlayground-Beta/TestingRxTableViewAnimations/Views/BlueCell/OWTestingBlueFirstLevel.swift
@@ -52,7 +52,7 @@ class OWTestingBlueFirstLevel: UIView {
 
     fileprivate var viewModel: OWTestingBlueFirstLevelViewModeling!
     fileprivate var disposeBag = DisposeBag()
-    fileprivate var labelsHeightConstraint: OWConstraint?
+    fileprivate var heightConstraint: OWConstraint?
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -78,7 +78,7 @@ fileprivate extension OWTestingBlueFirstLevel {
             .corner(radius: Metrics.roundCorners)
 
         self.OWSnp.makeConstraints { make in
-            labelsHeightConstraint = make.height.equalTo(Metrics.collapsedCellContentHeight).constraint
+            heightConstraint = make.height.equalTo(Metrics.collapsedCellContentHeight).constraint
         }
 
         self.addSubview(lblIdentifier)
@@ -124,7 +124,7 @@ fileprivate extension OWTestingBlueFirstLevel {
                     height = Metrics.expandedCellContentHeight
                 }
 
-                self.labelsHeightConstraint?.update(offset: height)
+                self.heightConstraint?.update(offset: height)
             })
             .disposed(by: disposeBag)
 
@@ -168,7 +168,7 @@ fileprivate extension OWTestingBlueFirstLevel {
                     height = Metrics.expandedCellContentHeight
                 }
 
-                self.labelsHeightConstraint?.update(offset: height)
+                self.heightConstraint?.update(offset: height)
             })
             .disposed(by: disposeBag)
     }
