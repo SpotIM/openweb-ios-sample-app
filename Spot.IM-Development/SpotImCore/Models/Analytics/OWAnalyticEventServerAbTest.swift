df42dcfc43f0cb654ec87d2a9dd8bf07b5728126
Nogah <nogah@applift-consulting.com>
2023-07-17 14:47:38 +0300

    cleanups

---
+++ Spot.IM-Development/SpotImCore/Models/Analytics/OWAnalyticEventServerAbTest.swift
@@ -0,0 +1,14 @@
+//
+//  OWAnalyticEventServerAbTest.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 17/07/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct OWAnalyticEventServerAbTest: Decodable {
+    var selectedTests: [Dictionary<String, String>] = []
+    var affectiveTests: [Dictionary<String, String>] = []
+}
