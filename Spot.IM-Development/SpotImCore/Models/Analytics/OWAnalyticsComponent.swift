9d6d00f36720a95bd7c4f4a77c68754d3b36584d
Nogah <nogah@applift-consulting.com>
2023-07-17 14:47:38 +0300

    cleanups

---
+++ Spot.IM-Development/SpotImCore/Models/Analytics/OWAnalyticsComponent.swift
@@ -0,0 +1,16 @@
+//
+//  OWAnalyticsComponent.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 17/07/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum OWAnalyticsComponent: String {
+    case conversation = "full_conversation"
+    case preConversation = "pre_conversation"
+    case commentThread = "comment_thread"
+    case commentCreation = "comment_creation"
+}
