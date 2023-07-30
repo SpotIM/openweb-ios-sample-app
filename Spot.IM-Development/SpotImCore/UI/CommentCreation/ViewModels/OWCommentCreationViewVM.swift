03fbd4d73a53ece6fb2422259a7b6534839dca27
Alon Shprung <alonshp1@gmail.com>
2023-07-30 11:52:44 +0300

    review fixes

--- Spot.IM-Development/SpotImCore/UI/CommentCreation/ViewModels/OWCommentCreationViewVM.swift
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/ViewModels/OWCommentCreationViewVM.swift
@@ -51,31 +51,28 @@ class OWCommentCreationViewViewModel: OWCommentCreationViewViewModeling, OWComme
             .map { !$0.isEmpty }
             .flatMap { [weak self] hasText -> Observable<Void> in
                 guard let self = self else { return .empty() }
-                if hasText {
-                    let actions = [
-                        OWRxPresenterAction(title: OWLocalizationManager.shared.localizedString(key: "Yes"), type: OWCloseEditorAlert.yes),
-                        OWRxPresenterAction(title: OWLocalizationManager.shared.localizedString(key: "No"), type: OWCloseEditorAlert.no, style: .cancel)
-                    ]
-                    return self.servicesProvider.presenterService()
-                        // TODO - Localization
-                        .showAlert(title: OWLocalizationManager.shared.localizedString(key: "Close editor?"), message: "", actions: actions, viewableMode: viewableMode)
-                        .flatMap { result -> Observable<Void> in
-                            switch result {
-                            case .completion:
+                guard hasText else { return Observable.just(()) }
+                let actions = [
+                    OWRxPresenterAction(title: OWLocalizationManager.shared.localizedString(key: "Yes"), type: OWCloseEditorAlert.yes),
+                    OWRxPresenterAction(title: OWLocalizationManager.shared.localizedString(key: "No"), type: OWCloseEditorAlert.no, style: .cancel)
+                ]
+                return self.servicesProvider.presenterService()
+                    // TODO - Localization
+                    .showAlert(title: OWLocalizationManager.shared.localizedString(key: "Close editor?"), message: "", actions: actions, viewableMode: viewableMode)
+                    .flatMap { result -> Observable<Void> in
+                        switch result {
+                        case .completion:
+                            return Observable.empty()
+                        case .selected(let action):
+                            switch action.type {
+                            case OWCloseEditorAlert.yes:
+                                // TODO - save comment to cache
+                                return Observable.just(())
+                            default:
                                 return Observable.empty()
-                            case .selected(let action):
-                                switch action.type {
-                                case OWCloseEditorAlert.yes:
-                                    // TODO - save comment to cache
-                                    return Observable.just(())
-                                default:
-                                    return Observable.empty()
-                                }
                             }
                         }
-                } else {
-                    return Observable.just(())
-                }
+                    }
             }
     }()
 
