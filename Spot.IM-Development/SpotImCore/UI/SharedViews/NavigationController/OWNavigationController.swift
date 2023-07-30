2cd4bee1a92d08122b3a5196065c2a9e0bf3089d
Alon Haiut <alon.h@openweb.com>
2023-07-30 17:10:14 +0300

    Added style to the navigation bar in which we are using for presented mode

--- Spot.IM-Development/SpotImCore/UI/SharedViews/NavigationController/OWNavigationController.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/NavigationController/OWNavigationController.swift
@@ -17,7 +17,11 @@ protocol OWNavigationControllerProtocol {
 class OWNavigationController: UINavigationController, OWNavigationControllerProtocol {
 
     // We need to create a shared nav controller so it will stay in the memory, Router layer "holds" nav controller in a weak reference
-    static let shared = OWNavigationController()
+    static let shared: OWNavigationController = {
+        let navController = OWNavigationController()
+        navController.setupNavControllerUI()
+        return navController
+    }()
 
     fileprivate let _dismissed = PublishSubject<Void>()
     var dismissed: Observable<Void> {
@@ -33,6 +37,41 @@ class OWNavigationController: UINavigationController, OWNavigationControllerProt
         super.viewDidDisappear(animated)
         if isBeingDismissed {
             _dismissed.onNext()
+        }
+    }
+}
+
+fileprivate extension OWNavigationController {
+    func setupNavControllerUI() {
+        let themeService = OWSharedServicesProvider.shared.themeStyleService()
+        let currentThemeStyle = themeService.currentStyle
+
+        self.navigationBar.isTranslucent = true
+        let navigationBarBackgroundColor = OWColorPalette.shared.color(type: .systemBackground, themeStyle: currentThemeStyle)
+
+        // Setup Title font
+        let navigationTitleTextAttributes = [
+            // Currently this is intentionally to match the SampleApp design. Later on we can have a dedicated design to the nav bar in present mode
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .bold),
+            NSAttributedString.Key.foregroundColor: OWColorPalette.shared.color(type: .systemText, themeStyle: currentThemeStyle)
+        ]
+
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = navigationBarBackgroundColor
+            appearance.titleTextAttributes = navigationTitleTextAttributes
+
+            // Setup Back button
+            let backButtonAppearance = UIBarButtonItemAppearance(style: .plain)
+            backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
+            appearance.backButtonAppearance = backButtonAppearance
+
+            self.navigationBar.standardAppearance = appearance
+            self.navigationBar.scrollEdgeAppearance = self.navigationBar.standardAppearance
+        } else {
+            self.navigationBar.backgroundColor = navigationBarBackgroundColor
+            self.navigationBar.titleTextAttributes = navigationTitleTextAttributes
         }
     }
 }
