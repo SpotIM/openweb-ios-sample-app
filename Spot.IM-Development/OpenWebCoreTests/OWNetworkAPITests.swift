9fa620bec846af941c986e0f745c8e949244c1da
Philip Kluz <philip.kluz@gmail.com>
2023-08-07 12:06:53 +0200

    Squash: Migrating Tests to Quick and Nimble
    Fix: Removing static linkage flag in Podfile
    
    Feat: Adding Quick / Nimble
    
    Feat: Migrating OWThemeServiceTests to Quick/Nimble
    
    Feat: Migrating OWUserDefaultsTests to Quick / Nimble
    
    Feat: Migrating OWUserDefaults to Quick / Nimble and Merging Rx and Non-Rx Tests into Context.
    
    Feat: Migrating OWTimeMeasuringService to Quick / Nimble
    
    Feat: Migrating OWPersistenceRxHelperTests to Quick / Nimble
    
    Feat: Migrating OWRxAppLifeCycleTests to Quick / Nimble
    
    Feat: Migrating OWNetworkAPITests to Quick / Nimble

--- Spot.IM-Development/OpenWebCoreTests/OWNetworkAPITests.swift
+++ Spot.IM-Development/OpenWebCoreTests/OWNetworkAPITests.swift
@@ -10,104 +10,65 @@ import XCTest
 import RxSwift
 import RxTest
 import RxBlocking
+import Quick
+import Nimble
+
 @testable import SpotImCore
 
-class OWNetworkAPITests: XCTestCase {
+final class OWNetworkAPITests: QuickSpec {
     
-    enum MockUserEndpoint: OWEndpoints {
-        case userData
+    override func spec() {
+        var api: OWNetworkAPI!
+        var session: OWSession!
+        var environment: OWEnvironment!
         
-        var method: OWNetworkHTTPMethod {
-            switch self {
-            case .userData:
-                return .get
-            }
-        }
-
-        var path: String {
-            switch self {
-            case .userData:
-                return "/user/data"
-            }
-        }
-
-        var parameters: OWNetworkParameters? {
-            switch self {
-            case .userData:
-                return nil
-            }
-        }
-    }
-    
-    struct MockUser: Codable, Equatable {
-        var name: String
-        var age : Int
-    }
-    
-    var api: OWNetworkAPI!
-    var session: OWSession!
-    var environment: OWEnvironment!
-    var disposeBag: DisposeBag!
-
-    override func setUp() {
-        super.setUp()
-        session = {
-            let mockSessionConfig = URLSessionConfiguration.ephemeral
-            mockSessionConfig.protocolClasses = [MockURLProtocol.self]
-            let mockSession = OWSession(configuration: mockSessionConfig, interceptor: MockNetworkInterceptor())
-            return mockSession
-        }()
-        environment = OWEnvironment(scheme: "http", domain: "localhost")
-        api = OWNetworkAPI(environment: environment, session: session)
-        disposeBag = DisposeBag()
-    }
-
-    override func tearDown() {
-        session.deregisterAllHandlers()
-        session = nil
-        environment = nil
-        api = nil
-        disposeBag = nil
-        super.tearDown()
-    }
-    
-    private func GETRequest(for endpoint: OWEndpoints, with environment: OWEnvironment) -> URLRequest {
-        return try! URLRequest(url: environment.baseURL.appending(path: endpoint.path), method: .get)
-    }
-    
-    func testPerformRequestSuccess() {
-        let handler: MockURLProtocol.RequestHandler = { request in
-            let data = """
-            {
-                "name": "John Doe",
-                "age": 30
-            }
-            """.data(using: .utf8)!
-            
-            let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "2.0", headerFields: nil)!
-            return (response, data)
+        beforeEach {
+            session = {
+                let mockSessionConfig = URLSessionConfiguration.ephemeral
+                mockSessionConfig.protocolClasses = [MockURLProtocol.self]
+                let mockSession = OWSession(configuration: mockSessionConfig, interceptor: MockNetworkInterceptor())
+                return mockSession
+            }()
+            environment = OWEnvironment(scheme: "http", domain: "localhost")
+            api = OWNetworkAPI(environment: environment, session: session)
         }
         
-        session.register(handler: handler, for: GETRequest(for: MockUserEndpoint.userData, with: environment))
+        afterEach {
+            session.deregisterAllHandlers()
+            session = nil
+            environment = nil
+            api = nil
+        }
         
-        let response: OWNetworkResponse<MockUser> = api.performRequest(
-            route: api.request(for: MockUserEndpoint.userData),
-            decoder: JSONDecoder()
-        )
-        
-        let scheduler = TestScheduler(initialClock: 0)
-        let observer = scheduler.createObserver(MockUser.self)
-        
-        response.response
-            .asObservable()
-            .bind(to: observer)
-            .disposed(by: disposeBag)
-        
-        scheduler.start()
-        
-        XCTAssertEqual(observer.events, [
-            .next(0, MockUser(name: "John Doe", age: 30)),
-            .completed(0)
-        ])
+        describe("OWNetworkAPI") {
+            it("should perform a successful request") {
+                let request = try! URLRequest(
+                    url: environment.baseURL.appending(path: MockUserEndpoint.userData.path),
+                    method: .get
+                )
+                
+                let handler: MockURLProtocol.RequestHandler = { request in
+                    let data = """
+                        {
+                            "name": "John Doe",
+                            "age": 30
+                        }
+                        """.data(using: .utf8)!
+                    
+                    let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "2.0", headerFields: nil)!
+                    return (response, data)
+                }
+                
+                session.register(handler: handler, for: request)
+                
+                let response: OWNetworkResponse<MockUser> = api.performRequest(
+                    route: api.request(for: MockUserEndpoint.userData),
+                    decoder: JSONDecoder()
+                )
+                
+                let result = try! response.response.toBlocking().first()
+                expect(result).to(equal(MockUser(name: "John Doe", age: 30)))
+            }
+        }
     }
 }
