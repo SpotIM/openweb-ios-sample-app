a35c168383ccc106a08c2ca24673121ff2db9fde
Alon Shprung <alonshp1@gmail.com>
2023-08-10 17:43:07 +0300

    create OWCommentCreatorNetworkHelper

--- Spot.IM-Development/SpotImCore/UI/CommentCreation/ViewModels/OWCommentCreationViewVM.swift
+++ Spot.IM-Development/SpotImCore/UI/CommentCreation/ViewModels/OWCommentCreationViewVM.swift
@@ -117,8 +117,15 @@ class OWCommentCreationViewViewModel: OWCommentCreationViewViewModeling, OWComme
         let commentCreationNetworkObservable = Observable.merge(commentCreationRegularViewVm.outputs.performCta, commentCreationLightViewVm.outputs.performCta)
             .map { [weak self] commentCreationData -> (OWCommentCreationCtaData, OWNetworkParameters)? in
                 // 1 - get create comment request params
-                guard let self = self else { return nil }
-                return (commentCreationData, self.getParametersForCreateCommentRequest(from: commentCreationData))
+                guard let self = self,
+                      let postId = self.postId
+                else { return nil }
+                return (commentCreationData, OWCommentCreatorNetworkHelper.getParametersForCreateCommentRequest(
+                    from: commentCreationData,
+                    section: self.commentCreationData.article.additionalSettings.section,
+                    commentCreationType: self.commentCreationData.commentCreationType,
+                    postId: postId
+                ))
             }
             .unwrap()
             .flatMapLatest { [weak self] commentCreationData, networkParameters -> Observable<(OWCommentCreationCtaData, Event<OWComment>)> in
@@ -226,62 +233,6 @@ class OWCommentCreationViewViewModel: OWCommentCreationViewViewModeling, OWComme
 fileprivate extension OWCommentCreationViewViewModel {
     func setupObservers() {
 
-    }
-
-    func getParametersForCreateCommentRequest(from commentCreationData: OWCommentCreationCtaData) -> OWNetworkParameters {
-        var metadata: [String: Any] = [:]
-
-        if let bundleId = Bundle.main.bundleIdentifier {
-            metadata["app_bundle_id"] = bundleId
-        }
-
-        var parameters: [String: Any] = [
-            "content": self.getContentRequestParam(from: commentCreationData)
-        ]
-
-        if !commentCreationData.commentLabelIds.isEmpty {
-            parameters["additional_data"] = [
-                "labels": [
-                    "section": self.commentCreationData.article.additionalSettings.section,
-                    "ids": commentCreationData.commentLabelIds
-                ] as [String: Any]
-            ]
-        }
-
-        switch self.commentCreationData.commentCreationType {
-        case .comment:
-            break
-        case .edit(let comment):
-            if let messageId = comment.id {
-                parameters["message_id"] = messageId
-            }
-        case .replyToComment(let originComment):
-            let commentId = originComment.id
-            let rootCommentId = originComment.rootComment
-            let isRootComment = commentId == rootCommentId
-            if !isRootComment {
-                metadata["reply_to"] = ["reply_id": commentId]
-            }
-            parameters["conversation_id"] = postId
-            parameters["parent_id"] = rootCommentId ?? commentId
-        }
-
-        parameters["metadata"] = metadata
-
-        return parameters
-    }
-
-    func getContentRequestParam(from commentCreationData: OWCommentCreationCtaData) -> [[String: Any]] {
-        var content: [[String: Any]] = []
-
-        if !commentCreationData.text.isEmpty {
-            content.append([
-                "type": "text",
-                "text": commentCreationData.text
-            ])
-        }
-
-        return content
     }
 
     func cacheComment(text commentText: String) {
