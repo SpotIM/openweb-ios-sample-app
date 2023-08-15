4cda2183e37b4ad913b9de8675cdfb0e1447ad0d
Nogah <nogah@applift-consulting.com>
2023-08-15 13:25:38 +0300

    Add BIEvent

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Typealiases/OWCustomBIData.swift
@@ -0,0 +1,15 @@
+//
+//  OWCustomBIData.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 15/08/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+#if NEW_API
+public typealias OWCustomBIData = [String: String]
+#else
+typealias OWCustomBIData = [String: String]
+#endif
