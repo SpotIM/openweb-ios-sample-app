a398ee36153e3de108a656d148376522e71ca93a
Nogah <nogah@applift-consulting.com>
2023-08-21 16:35:06 +0300

    Start OWClarityDetailsView

--- Spot.IM-Development/SpotImCore/UI/ClarityDetails/ViewModels/OWClarityDetailsViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/ClarityDetails/ViewModels/OWClarityDetailsViewModel.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import RxSwift
+import UIKit
+
+protocol OWClarityDetailsViewModelingInputs {
+}
+
+protocol OWClarityDetailsViewModelingOutputs {
+    var topParagraphAttributedString: NSAttributedString { get }
+}
+
+protocol OWClarityDetailsViewModeling {
+    var inputs: OWClarityDetailsViewModelingInputs { get }
+    var outputs: OWClarityDetailsViewModelingOutputs { get }
+}
+
+class OWClarityDetailsViewModel: OWClarityDetailsViewModeling,
+                                 OWClarityDetailsViewModelingInputs,
+                                 OWClarityDetailsViewModelingOutputs {
+
+    var inputs: OWClarityDetailsViewModelingInputs { return self }
+    var outputs: OWClarityDetailsViewModelingOutputs { return self }
+
+    fileprivate var type: OWClarityDetailsType = .rejected // TODO: get in init
+
+    lazy var topParagraphAttributedString: NSAttributedString = {
+        switch type {
+        case .rejected:
+            return "Your comment seems to be in breach of our community guidelines and was therefore rejected. It will only be visible to you."
+                .attributedString // TODO: add community guidelines link
+        case .pending:
+            return "Your comment may have been sent for review for one of the following reasons:"
+                .attributedString
+        }
+    }()
+
+}
+
+// TODO: new file
+enum OWClarityDetailsType {
+    case rejected
+    case pending
+}
--- Spot.IM-Development/SpotImCore/UI/ClarityDetails/Views/OWClarityDetailsView.swift
+++ Spot.IM-Development/SpotImCore/UI/ClarityDetails/Views/OWClarityDetailsView.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+
+class OWClarityDetailsView: UIView {
+    fileprivate struct Metrics {
+        static let horizontalPadding: CGFloat = 16
+        static let verticalPadding: CGFloat = 20
+    }
+
+    fileprivate lazy var topParagraphLabel: UILabel = {
+        return UILabel()
+            .textColor(OWColorPalette.shared.color(type: .textColor3, themeStyle: .light))
+            .font(OWFontBook.shared.font(typography: .bodyText))
+            .attributedText(viewModel.outputs.topParagraphAttributedString)
+            .numberOfLines(0)
+    }()
+
+    fileprivate let viewModel: OWClarityDetailsViewModeling
+    fileprivate var disposeBag: DisposeBag
+
+    init(viewModel: OWClarityDetailsViewModeling) {
+        self.viewModel = viewModel
+        self.disposeBag = DisposeBag()
+        super.init(frame: .zero)
+
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+fileprivate extension OWClarityDetailsView {
+    func setupViews() {
+        self.addSubview(topParagraphLabel)
+        topParagraphLabel.OWSnp.makeConstraints { make in
+            make.top.equalToSuperview().inset(Metrics.verticalPadding)
+            make.leading.trailing.equalToSuperview().inset(Metrics.horizontalPadding)
+        }
+    }
+
+    func setupObservers() {
+        OWSharedServicesProvider.shared.themeStyleService()
+            .style
+            .subscribe(onNext: { [weak self] currentStyle in
+                guard let self = self else { return }
+                self.topParagraphLabel.textColor = OWColorPalette.shared.color(type: .textColor3, themeStyle: currentStyle)
+            })
+            .disposed(by: disposeBag)
+
+        OWSharedServicesProvider.shared.appLifeCycle()
+            .didChangeContentSizeCategory
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.topParagraphLabel.font = OWFontBook.shared.font(typography: .bodyText)
+            })
+            .disposed(by: disposeBag)
+    }
+}
