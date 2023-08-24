2b41e9132e85973313fca8cccfe76ca3be11a726
Revital Pisman <revital.pisman@openweb.com>
2023-08-24 19:28:27 +0300

    Fixed stackView animation

--- Spot.IM-Development/SpotImCore/Extensions/Presenter+Rx/UIView+Rx.swift
+++ Spot.IM-Development/SpotImCore/Extensions/Presenter+Rx/UIView+Rx.swift
@@ -14,11 +14,15 @@ extension Reactive where Base: UIView {
 
     var isHiddenAnimated: Binder<Bool> {
         return Binder(self.base) { view, hidden in
-            UIView.animate(withDuration: 0.1, animations: {
-                view.alpha = hidden ? 0 : 1
-            }) { _ in
+            UIView.animate(withDuration: 0.25,
+                           delay: 0,
+                           usingSpringWithDamping: 0.7,
+                           initialSpringVelocity: 0.5,
+                           options: [],
+                           animations: {
+                view.transform = hidden ? CGAffineTransform(scaleX: 0.95, y: 0.95) : .identity
                 view.isHidden = hidden
-            }
+            })
         }
     }
 }
