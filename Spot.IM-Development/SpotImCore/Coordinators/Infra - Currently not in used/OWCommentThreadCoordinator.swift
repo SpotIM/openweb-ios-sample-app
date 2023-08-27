76966fb7ddd3e6ccecb3c1131abd814cccee8f29
Alon Haiut <alon.h@openweb.com>
2023-08-27 16:45:10 +0300

    Infrastructure for report reasons from comment thread is ready

--- Spot.IM-Development/SpotImCore/Coordinators/Infra - Currently not in used/OWCommentThreadCoordinator.swift
+++ Spot.IM-Development/SpotImCore/Coordinators/Infra - Currently not in used/OWCommentThreadCoordinator.swift
@@ -65,6 +65,7 @@ class OWCommentThreadCoordinator: OWBaseCoordinator<OWCommentThreadCoordinatorRe
             .map { OWCommentThreadCoordinatorResult.loadedToScreen }
             .asObservable()
 
+        // Coordinate to comment creation
         let coordinateCommentCreationObservable = commentThreadVM.outputs.commentThreadViewVM.outputs.openCommentCreation
             .flatMap { [weak self] commentCreationType -> Observable<OWCommentCreationCoordinatorResult> in
                 guard let self = self else { return .empty() }
@@ -93,6 +94,49 @@ class OWCommentThreadCoordinator: OWBaseCoordinator<OWCommentThreadCoordinatorRe
                 return Observable.never()
             }
 
+        let reportReasonFromCommentThreadObservable = commentThreadVM.outputs.commentThreadViewVM
+            .outputs.openReportReason
+            .map { commentVM -> OWReportReasonsRequiredData? in
+                guard let commentId = commentVM.outputs.comment.id,
+                    let parentId = commentVM.outputs.comment.parentId else {
+                    return nil
+                }
+
+                return OWReportReasonsRequiredData(commentId: commentId, parentId: parentId)
+            }
+            .unwrap()
+
+        // Coordinate to report reason
+        let coordinateReportReasonObservable = reportReasonFromCommentThreadObservable
+            .asObservable()
+            .filter { [weak self] _ in
+                guard let self = self else { return false }
+                return self.viewableMode == .partOfFlow
+            }
+            .flatMap { [weak self] reportData -> Observable<OWReportReasonCoordinatorResult> in
+                guard let self = self else { return .empty() }
+                let reportReasonCoordinator = OWReportReasonCoordinator(reportData: reportData,
+                                                                        router: self.router,
+                                                                        actionsCallbacks: self.actionsCallbacks,
+                                                                        presentationalMode: self.conversationData.presentationalStyle)
+                return self.coordinate(to: reportReasonCoordinator)
+            }
+            .do(onNext: { coordinatorResult in
+                switch coordinatorResult {
+                case .popped:
+                    // Nothing
+                    break
+                case .submitedReport(_):
+                    // Nothing - already taken care in report VM in which we update the report service
+                    break
+                default:
+                    break
+                }
+            })
+            .flatMap { _ -> Observable<OWConversationCoordinatorResult> in
+                return Observable.never()
+            }
+
         // Coordinate to safari tab
         let coordinateToSafariObservables = Observable.merge(
             commentThreadVM.outputs.commentThreadViewVM.outputs.urlClickedOutput,
@@ -109,7 +153,10 @@ class OWCommentThreadCoordinator: OWBaseCoordinator<OWCommentThreadCoordinatorRe
                 return Observable.never()
             }
 
-        return Observable.merge(commentThreadPoppedObservable, commentThreadLoadedToScreenObservable, coordinateCommentCreationObservable, coordinateToSafariObservables)
+        return Observable.merge(commentThreadPoppedObservable,
+                                commentThreadLoadedToScreenObservable,
+                                coordinateCommentCreationObservable, coordinateToSafariObservables,
+                                coordinateReportReasonObservable)
     }
 
     override func showableComponent() -> Observable<OWShowable> {
