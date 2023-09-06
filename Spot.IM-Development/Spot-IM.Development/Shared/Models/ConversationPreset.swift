c4a83173461615d4e785cd3e51eb5803abb1b613
Nogah <nogah@applift-consulting.com>
2023-09-06 11:23:38 +0300

    default section is nil

--- Spot.IM-Development/Spot-IM.Development/Shared/Models/ConversationPreset.swift
+++ Spot.IM-Development/Spot-IM.Development/Shared/Models/ConversationPreset.swift
@@ -11,7 +11,7 @@ import Foundation
 struct ConversationPreset {
     let displayName: String
     let conversationDataModel: SDKConversationDataModel
-    let section: String?
+    var section: String? = nil
 }
 
 // swiftlint:disable line_length
@@ -21,8 +21,7 @@ extension ConversationPreset {
     static func demoSpot() -> ConversationPreset {
         return ConversationPreset(displayName: "Demo Spot",
                                   conversationDataModel: SDKConversationDataModel(spotId: "sp_eCIlROSD",
-                                                                                  postId: "sdk1"),
-                                  section: nil)
+                                                                                  postId: "sdk1"))
     }
 
     static func createMockModels() -> [ConversationPreset] {
@@ -35,44 +34,36 @@ extension ConversationPreset {
     #else
         return demoConversationPreset + [ConversationPreset(displayName: "FOX News",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_ANQXRpqH",
-                                                                                                            postId: "urn:uri:base64:3cb1232f-b7ea-5546-81a5-395a75a27b1b"),
-                                                            section: nil),
+                                                                                                            postId: "urn:uri:base64:3cb1232f-b7ea-5546-81a5-395a75a27b1b")),
                                          ConversationPreset(displayName: "Yahoo",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_Rba9aFpG",
                                                                                                             postId: "finmb$24937"),
                                                             section: "stock"),
                                          ConversationPreset(displayName: "Ynet",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_AJXaDckj",
-                                                                                                            postId: "S19Z20aTU"),
-                                                            section: nil),
+                                                                                                            postId: "S19Z20aTU")),
                                          ConversationPreset(displayName: "DailyMotion - Staging",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_srsgdH9A",
-                                                                                                            postId: "x8ick9b"),
-                                                            section: nil),
+                                                                                                            postId: "x8ick9b")),
                                          ConversationPreset(displayName: "DailyMotion - beta",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_ToLXXNEQ",
-                                                                                                            postId: "x8cpf62"),
-                                                            section: nil),
+                                                                                                            postId: "x8cpf62")),
                                          ConversationPreset(displayName: "DailyMotion - Prod",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_ybZYQUfH",
-                                                                                                            postId: "x8jj55n"),
-                                                            section: nil),
+                                                                                                            postId: "x8jj55n")),
                                          ConversationPreset(displayName: "mobile SSO",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_mobileSSO",
-                                                                                                            postId: "sdk1"),
-                                                            section: nil),
+                                                                                                            postId: "sdk1")),
                                          ConversationPreset(displayName: "mobile Guest",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_mobileGuest",
                                                                                                             postId: "sdk1"),
                                                             section: "other_section"),
                                          ConversationPreset(displayName: "mobile Social",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_mobileSocial",
-                                                                                                            postId: "sdk1"),
-                                                            section: nil),
+                                                                                                            postId: "sdk1")),
                                          ConversationPreset(displayName: "mobile Social Guest",
                                                             conversationDataModel: SDKConversationDataModel(spotId: "sp_mobileSocialGuest",
-                                                                                                            postId: "sdk1"),
-                                                            section: nil)]
+                                                                                                            postId: "sdk1"))]
 
     #endif
     }
