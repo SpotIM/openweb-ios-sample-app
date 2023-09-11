7195b81fd5ded43b1b3c629c6e906303a4898219
Refael <refaelsommer@gmail.com>
2023-09-11 12:03:51 +0300

    - error states

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/ErrorStates/ViewModels/OWErrorStateViewVM.swift
@@ -0,0 +1,70 @@
+//
+//  OWErrorStateViewVM.swift
+//  SpotImCore
+//
+//  Created by Refael Sommer on 10/09/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import RxSwift
+import Foundation
+
+protocol OWErrorStateViewViewModelingInputs {
+    var tryAgainTapped: PublishSubject<Void> { get }
+}
+
+protocol OWErrorStateViewViewModelingOutputs {
+    var title: String { get }
+    var tryAgainText: NSAttributedString { get }
+    var tryAgainTap: Observable<OWErrorStateTypes> { get }
+}
+
+protocol OWErrorStateViewViewModeling {
+    var inputs: OWErrorStateViewViewModelingInputs { get }
+    var outputs: OWErrorStateViewViewModelingOutputs { get }
+}
+
+class OWErrorStateViewViewModel: OWErrorStateViewViewModeling, OWErrorStateViewViewModelingInputs, OWErrorStateViewViewModelingOutputs {
+    var inputs: OWErrorStateViewViewModelingInputs { return self }
+    var outputs: OWErrorStateViewViewModelingOutputs { return self }
+
+    fileprivate let disposeBag = DisposeBag()
+    fileprivate let errorStateType: OWErrorStateTypes
+
+    init(errorStateType: OWErrorStateTypes) {
+        self.errorStateType = errorStateType
+    }
+
+    var tryAgainTapped = PublishSubject<Void>()
+    lazy var tryAgainTap: Observable<OWErrorStateTypes> = {
+        return tryAgainTapped
+            .map { [weak self] _ -> OWErrorStateTypes? in
+                guard let self = self else { return nil }
+                return self.errorStateType
+            }
+            .unwrap()
+            .asObservable()
+    }()
+
+    lazy var title: String = {
+        let key = {
+            switch errorStateType {
+                case .loadConversationReplies:
+                    return "ErrorStateLoadConversationReplies"
+                }
+            }()
+        return OWLocalizationManager.shared.localizedString(key: key)
+    }()
+
+    lazy var tryAgainText: NSAttributedString = {
+        let tryAgainText = OWLocalizationManager.shared.localizedString(key: "TryAgain")
+        var attributedString = NSMutableAttributedString(string: tryAgainText)
+        attributedString.addAttribute(.foregroundColor,
+                                      value: UIColor.brandColor,
+                                         range: NSRange(location: 0, length: attributedString.length))
+        attributedString.addAttribute(.font,
+                                      value: OWFontBook.shared.font(typography: .bodyInteraction, forceOpenWebFont: false),
+                                         range: NSRange(location: 0, length: attributedString.length))
+        return attributedString
+    }()
+}
