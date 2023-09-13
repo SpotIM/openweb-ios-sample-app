471d74789ffd2e9f4a928239e7fc9031a92f9b1d
Refael <refaelsommer@gmail.com>
2023-09-13 17:19:46 +0300

    - Removed Nogah's fix

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/ViewModels/OWCommentViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/ViewModels/OWCommentViewModel.swift
@@ -89,7 +89,6 @@ class OWCommentViewModel: OWCommentViewModeling,
 
             return status != .none
         }
-        .startWith(false)
     }
     var showBlockingLayoutView: Observable<Bool> {
         // Using Observable.merge because in the future we might have more cases where we show disable layout
