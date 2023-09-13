7ed909fdef4d04bc94897e077bbde22917ca2c78
Nogah <nogah@applift-consulting.com>
2023-09-13 09:43:14 +0300

    Show one toast properly

--- Spot.IM-Development/SpotImCore/Services/ToastNotificationService/OWToastNotificationService.swift
+++ Spot.IM-Development/SpotImCore/Services/ToastNotificationService/OWToastNotificationService.swift
@@ -18,6 +18,8 @@ protocol OWToastNotificationServicing {
 
 class OWToastNotificationService: OWToastNotificationServicing {
     fileprivate let queue = OWQueue<OWToastNotificationPresentData>()
+    fileprivate var blockerService: OWBlockerServicing = OWSharedServicesProvider.shared.blockerServicing() // TODO: di
+    fileprivate var disposeBag: DisposeBag = DisposeBag()
 
     fileprivate var _toastToShow = BehaviorSubject<OWToastNotificationPresentData?>(value: nil)
     var toastToShow: Observable<OWToastNotificationPresentData?> {
@@ -25,14 +27,57 @@ class OWToastNotificationService: OWToastNotificationServicing {
             .asObservable()
     }
 
+    fileprivate var newToast = PublishSubject<Void>()
+    fileprivate var newToastObservable: Observable<Void> {
+        newToast
+            .asObservable()
+            .share()
+    }
+
+    init() {
+        setupObservers()
+    }
+
     func showToast(presentData: OWToastNotificationPresentData) {
         queue.insert(presentData)
-        // Blocking ?
+        newToast.onNext()
     }
 
     func clearNotifications() {
         // TODO: should add to OWQueue?
         // Should be called when post changes
+    }
+}
+
+fileprivate extension OWToastNotificationService {
+    func sendToastToShow() { // TODO: rename
+        guard !queue.isEmpty() else { return }
+        print("NOGAH: sendToastToShow")
+        let action = OWDefaultBlockerAction(blockerType: .toastNotification)
+        blockerService.add(blocker: action)
+        let toast = queue.popFirst()
+        _toastToShow.onNext(toast)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) { [weak self] in // TODO: handle properly
+            self?._toastToShow.onNext(nil)
+            print("NOGAH: toast finish")
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) { [weak self] in // TODO: handle properly
+                action.finish()
+                print("NOGAH: action.finish()")
+            }
+        }
+    }
+
+    func setupObservers() {
+        newToastObservable
+            .flatMap { [weak self] _ -> Observable<Void> in
+                guard let self = self else { return .empty() }
+                return self.blockerService.waitForNonBlocker(for: [.toastNotification])
+            }
+            .debug("NOGAH: newToastObservable")
+            .subscribe(onNext: { [weak self] in
+                self?.sendToastToShow()
+            })
+            .disposed(by: disposeBag)
     }
 }
 
