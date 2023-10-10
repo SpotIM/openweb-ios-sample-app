e1cbdbf232d8a2e67a5401f231814e4f40942da9
Nogah <nogah@applift-consulting.com>
2023-10-10 13:03:38 +0300

    PR fixes

---
+++ Spot.IM-Development/SpotImCore/Services/UserBadgeService/models/OWBadgesKeys.swift
@@ -0,0 +1,16 @@
+//
+//  OWBadgesKeys.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 10/10/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+enum OWBadgesKeys: String {
+    case admin = "user.badges.admin"
+    case journalist = "user.badges.journalist"
+    case moderator = "user.badges.moderator"
+    case communityModerator = "user.badges.community-moderator"
+}
