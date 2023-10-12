c00e518842b6e1fe26fc31e52f813cfa74f7bf87
Alon Shprung <alonshp1@gmail.com>
2023-10-12 12:45:16 +0300

    fix updating ui from background thread errors

--- Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentContentView.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/Comment/Views/OWCommentContentView.swift
@@ -108,6 +108,7 @@ fileprivate extension OWCommentContentView {
             .disposed(by: disposeBag)
 
         viewModel.outputs.mediaSize
+            .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] size in
                 guard let self = self else { return }
                 self.mediaView.OWSnp.updateConstraints { make in
@@ -145,6 +146,7 @@ fileprivate extension OWCommentContentView {
         }
 
         viewModel.outputs.isEdited
+            .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] isEdited in
                 guard let self = self else { return }
                 self.editedLabel.OWSnp.updateConstraints { make in
