00f8c8df6439ffb7c42168ed47603209ff47c69a
Alon Haiut <alon.h@openweb.com>
2023-10-13 12:23:24 +0300

    Using lock inside OWCommentsService to prevent "data race" Intentionally leaving debugs in this commit so it will be easy to return. Will remove in the next commit

--- Spot.IM-Development/SpotImCore/Services/OWCommentsService.swift
+++ Spot.IM-Development/SpotImCore/Services/OWCommentsService.swift
@@ -12,16 +12,22 @@ typealias OWCommentsMapper = [String: OWComment]
 
 protocol OWCommentsServicing {
     func get(commentId id: String, postId: String) -> OWComment?
-    func set(comments: [OWComment], postId: String)
+    func set(comments: [OWComment], postId: OWPostId)
 
     func cleanCache()
 }
 
 class OWCommentsService: OWCommentsServicing {
 
+    // Multiple threads / queues access to this class
+    // Avoiding "data race" by using a lock
+    fileprivate let lock: OWLock = OWUnfairLock()
+
     fileprivate var _mapPostIdToComments = [OWPostId: OWCommentsMapper]()
 
     func get(commentId id: String, postId: String) -> OWComment? {
+        self.lock.lock(); defer { self.lock.unlock() }
+
         guard let comments = _mapPostIdToComments[postId],
               let comment = comments[id]
         else { return nil }
@@ -29,11 +35,17 @@ class OWCommentsService: OWCommentsServicing {
     }
 
     func set(comments: [OWComment], postId: OWPostId) {
+        self.lock.lock(); defer { self.lock.unlock() }
+
         let commentIdToCommentTupples: [(String, OWComment)] = comments.map {
             guard let id = $0.id else { return nil }
             return (id, $0)
         }.unwrap()
         let commentIdsToComment: OWCommentsMapper = Dictionary(uniqueKeysWithValues: commentIdToCommentTupples)
+
+        let queueName = self.queueName()
+        let log = "Alon - Inside `set(comments`, \(queueName)"
+        print(log)
 
         if let existingCommentsForPostId = _mapPostIdToComments[postId] {
             // merge and replacing current comments
@@ -53,5 +65,31 @@ class OWCommentsService: OWCommentsServicing {
 
     func cleanCache() {
         _mapPostIdToComments.removeAll()
+    }
+}
+
+fileprivate extension OWCommentsService {
+    func queueName() -> String {
+        if let currentOperationQueue = OperationQueue.current {
+            if let currentDispatchQueue = currentOperationQueue.underlyingQueue {
+                return "dispatch queue: \(currentDispatchQueue.label.nonEmpty ?? currentDispatchQueue.description)"
+            } else {
+                return "operation queue: \(currentOperationQueue.name?.nonEmpty ?? currentOperationQueue.description)"
+            }
+        } else {
+            let currentThread = Thread.current
+            return "UNKNOWN QUEUE on thread: \(currentThread.name?.nonEmpty ?? currentThread.description)"
+        }
+    }
+}
+
+fileprivate extension String {
+    /// Returns this string if it is not empty, else `nil`.
+    var nonEmpty: String? {
+        if self.isEmpty {
+            return nil
+        } else {
+            return self
+        }
     }
 }
