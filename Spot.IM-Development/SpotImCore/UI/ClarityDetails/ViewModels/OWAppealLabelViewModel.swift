6ae0c95258c1c3d6ec595aed7f41843ed5f55ada
Nogah <nogah@applift-consulting.com>
2023-10-31 15:33:38 +0200

    Default view

--- Spot.IM-Development/SpotImCore/UI/ClarityDetails/ViewModels/OWAppealLabelViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/ClarityDetails/ViewModels/OWAppealLabelViewModel.swift
@@ -17,6 +17,8 @@ protocol OWAppealLabelViewModelingOutputs {
     var viewType: Observable<OWAppealLabelViewType> { get }
     var backgroundColor: Observable<UIColor> { get }
     var borderColor: Observable<UIColor> { get }
+    var defaultAttributedText: Observable<NSAttributedString> { get }
+    var appealClickableText: String { get }
 }
 
 protocol OWAppealLabelViewModeling {
@@ -57,7 +59,7 @@ class OWAppealLabelViewModel: OWAppealLabelViewModeling,
         Observable.combineLatest(
             viewType,
             servicesProvider.themeStyleService().style
-        ) { type, theme in
+        ) { type, _ in
             switch type {
             case .skeleton, .appealRejected, .default, .unavailable:
                 return OWDesignColors.D1
@@ -66,6 +68,37 @@ class OWAppealLabelViewModel: OWAppealLabelViewModeling,
             }
         }
     }
+
+    lazy private var accessibilityChange: Observable<Bool> = {
+        servicesProvider.appLifeCycle()
+            .didChangeContentSizeCategory
+            .map { true }
+            .startWith(false)
+    }()
+
+    let appealClickableText: String = "appeal" // TODO: translation
+    lazy var defaultAttributedText: Observable<NSAttributedString> = {
+        Observable.combineLatest(
+            servicesProvider.themeStyleService().style,
+            accessibilityChange
+        ) { [weak self] style, _ in
+            guard let self = self else { return nil }
+            let string = "Feel free to rewrite and repost your comment, or, if you wish, you can appeal, and we will re-examine our decision." // TODO: translations
+            let attributes: [NSAttributedString.Key: Any] = [
+                .foregroundColor: OWColorPalette.shared.color(type: .textColor3, themeStyle: style),
+                .font: OWFontBook.shared.font(typography: .bodyText)
+            ]
+            let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
+            if let range = string.range(of: self.appealClickableText) {
+                let nsRange = NSRange(range, in: string)
+                attributedString.addAttribute(.underlineStyle, value: 1, range: nsRange)
+                attributedString.addAttribute(.foregroundColor, value: OWColorPalette.shared.color(type: .brandColor, themeStyle: style), range: nsRange)
+                attributedString.addAttribute(.font, value: OWFontBook.shared.font(typography: .bodyInteraction), range: nsRange)
+            }
+            return attributedString
+        }
+        .unwrap()
+    }()
 
 
     init(servicesProvider: OWSharedServicesProviding = OWSharedServicesProvider.shared) {
