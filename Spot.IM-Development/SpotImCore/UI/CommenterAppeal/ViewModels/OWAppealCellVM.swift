323ce1605531ff696f7fd4291aaba927efc5ccf0
Nogah <nogah@applift-consulting.com>
2023-11-06 13:28:30 +0200

    Create cell & vm for appeal reason

---
+++ Spot.IM-Development/SpotImCore/UI/CommenterAppeal/ViewModels/OWAppealCellVM.swift
@@ -0,0 +1,43 @@
+//
+//  OWAppealCellViewModel.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 06/11/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import RxSwift
+import Foundation
+
+protocol OWAppealCellViewModelingInputs {
+    var setSelected: BehaviorSubject<Bool> { get }
+}
+
+protocol OWAppealCellViewModelingOutputs {
+    var title: String { get }
+    var isSelected: Observable<Bool> { get }
+}
+
+protocol OWAppealCellViewModeling {
+    var inputs: OWAppealCellViewModelingInputs { get }
+    var outputs: OWAppealCellViewModelingOutputs { get }
+}
+
+class OWAppealCellViewModel: OWAppealCellViewModelingInputs, OWAppealCellViewModelingOutputs, OWAppealCellViewModeling {
+
+    let title: String
+
+    var setSelected = BehaviorSubject(value: false)
+    var isSelected: Observable<Bool> {
+        self.setSelected
+            .asObservable()
+    }
+
+    var inputs: OWAppealCellViewModelingInputs { return self }
+    var outputs: OWAppealCellViewModelingOutputs { return self }
+
+    // TODO: OWReportReason will be replaced with proppen structs
+    init(reason: OWReportReason) {
+        self.title = reason.type.localizedTitle
+    }
+}
