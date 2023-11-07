9b7372f3dd02d770b9ac2fcddb966fc9ab253b5e
Nogah <nogah@applift-consulting.com>
2023-11-07 08:39:03 +0200

    sFix

--- Spot.IM-Development/SpotImCore/Coordinators/Infra - Currently not in used/Infra/SDKCoordinators/OWClarityDetailsCoordinator.swift
+++ Spot.IM-Development/SpotImCore/Coordinators/Infra - Currently not in used/Infra/SDKCoordinators/OWClarityDetailsCoordinator.swift
@@ -153,7 +153,6 @@ class OWClarityDetailsCoordinator: OWBaseCoordinator<OWClarityDetailsCoordinator
                 }
             }
 
-
         return Observable.merge(resultsWithPopAnimation, loadedToScreenObservable, coordinateToSafariObservable, resultWithoutPopAnimation, coordinateToAppealObservable)
     }
 
