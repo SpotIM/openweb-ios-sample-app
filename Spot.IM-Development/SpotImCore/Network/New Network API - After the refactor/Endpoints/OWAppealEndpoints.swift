72daa5b42346614f0d3de5e8d15cf7a37633c378
Nogah <nogah@applift-consulting.com>
2023-11-21 08:44:09 +0200

    Add getAppealOptions endpoint

--- Spot.IM-Development/SpotImCore/Network/New Network API - After the refactor/Endpoints/OWAppealEndpoints.swift
+++ Spot.IM-Development/SpotImCore/Network/New Network API - After the refactor/Endpoints/OWAppealEndpoints.swift
@@ -9,11 +9,13 @@
 import Foundation
 
 enum OWAppealEndpoints: OWEndpoints {
-    case  isEligibleToAppeal(commentId: String)
+    case appealOptions
+    case isEligibleToAppeal(commentId: String)
 
     // MARK: - HTTPMethod
     var method: OWNetworkHTTPMethod {
         switch self {
+        case .appealOptions: return .get
         case .isEligibleToAppeal: return .get
         }
     }
@@ -21,6 +23,7 @@ enum OWAppealEndpoints: OWEndpoints {
     // MARK: - Path
     var path: String {
         switch self {
+        case .appealOptions: return "conversation/v2/appeal/options"
         case .isEligibleToAppeal: return "conversation/v2/appeal"
         }
     }
@@ -28,6 +31,8 @@ enum OWAppealEndpoints: OWEndpoints {
     // MARK: - Parameters
     var parameters: OWNetworkParameters? {
         switch self {
+        case .appealOptions:
+            return nil
         case .isEligibleToAppeal:
             return nil
         }
@@ -35,6 +40,8 @@ enum OWAppealEndpoints: OWEndpoints {
 
     var queryParams: [Foundation.URLQueryItem]? {
         switch self {
+        case .appealOptions:
+            return nil
         case .isEligibleToAppeal(let commentId):
             return [Foundation.URLQueryItem(name: "messageId", value: commentId)]
         }
@@ -42,6 +49,7 @@ enum OWAppealEndpoints: OWEndpoints {
 }
 
 protocol OWAppealAPI {
+    func getAppealOptions() -> OWNetworkResponse<Array<OWAppealReason>>
     func isEligibleToAppeal(commentId: String) -> OWNetworkResponse<IsEligibleToAppealResponse>
 }
 
@@ -49,6 +57,12 @@ extension OWNetworkAPI: OWAppealAPI {
     // Access by .appeal for readability
     var appeal: OWAppealAPI { return self }
 
+    func getAppealOptions() -> OWNetworkResponse<Array<OWAppealReason>> {
+        let endpoint = OWAppealEndpoints.appealOptions
+        let requestConfigure = request(for: endpoint)
+        return performRequest(route: requestConfigure)
+    }
+    
     func isEligibleToAppeal(commentId: String) -> OWNetworkResponse<IsEligibleToAppealResponse> {
         let endpoint = OWAppealEndpoints.isEligibleToAppeal(commentId: commentId)
         let requestConfigure = request(for: endpoint)
