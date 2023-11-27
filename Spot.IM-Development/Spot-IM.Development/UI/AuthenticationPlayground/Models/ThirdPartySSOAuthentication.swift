1a2cbc09b084e2939661ca3c8348f56c8049fca0
Nogah <nogah@applift-consulting.com>
2023-11-27 08:19:37 +0200

    start adding new providers

--- Spot.IM-Development/Spot-IM.Development/UI/AuthenticationPlayground/Models/ThirdPartySSOAuthentication.swift
+++ Spot.IM-Development/Spot-IM.Development/UI/AuthenticationPlayground/Models/ThirdPartySSOAuthentication.swift
@@ -26,10 +26,10 @@ extension ThirdPartySSOAuthentication {
     #else
         return [
             // swiftlint:disable line_length
-            ThirdPartySSOAuthentication(displayName: "Fox - piano provider",
+            ThirdPartySSOAuthentication(displayName: "Fox",
                                      spotId: "sp_ANQXRpqH",
                                      token: "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5REEwNkVEMjAxOCIsInR5cCI6IkpXVCJ9.eyJwaWQiOiJ1cy1lYXN0LTE6NDAxYjMyMjgtZWE2NC00YTU2LThmYzEtZTgyYzkxY2FhZDJmIiwidWlkIjoiTkRBeFlqTXlNamd0WldFMk5DMDBZVFUyTFRobVl6RXRaVGd5WXpreFkyRmhaREptIiwic2lkIjoiYjU2ZWFhNjUtZWQzZC00NGYxLThmMTItZDU1ZjljOGE1ODIxIiwic2RjIjoidXMtZWFzdC0xIiwiYXR5cGUiOiJpZGVudGl0eSIsImR0eXBlIjoid2ViIiwidXR5cGUiOiJlbWFpbCIsImRpZCI6IiIsIm12cGRpZCI6IiIsInZlciI6MiwiZW50Ijp7fSwiZXhwIjoxNzAwNTI5NzE0LCJqdGkiOiI4YTAwNTQ0Ni1iODllLTQ0ZjUtYmIzOC1jOGJhZDhiNzZmYmYiLCJpYXQiOjE2NTMyMjU3MTR9.YSsurteEDw6ZaPvO5rcC_ld2aW6l486gRajXsjTQjLsDqc2wREqi-AfnjTWI6qMc7Jkp08x-gHL3HxmgnD6xisHsskJLLZC_Sa7O5Aky1PydbN6ixmu1tVe-ZwA1TxfTh7B4B2MyjaTs9az56KZ1Uw5Q4Xr0Wl57eyZCZVdOotw7F1cTlOAPb7fYuApN0lvRioURDlxjeKStHL4BLfpypydW8YjhMtlbYJ_3QQy7bXY5uWPfBV5Cv7CKaE-4b0m923Q5KFV9_czBm8QjtLiNEjZCfcDVfnn4lpFBQCjXg0piNq7Xl0bOCODAJepLQGfyY63t2xbILiwy8EQh_qLLOw",
-                                        provider: .piano)
+                                        provider: .foxid)
             ]
             // swiftlint:enable line_length
     #endif
