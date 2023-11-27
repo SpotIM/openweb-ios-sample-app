dbac7cdb6c5041074b33ad7f54e91198fb72cff7
Nogah <nogah@applift-consulting.com>
2023-11-27 08:19:37 +0200

    start adding new providers

--- Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWSSOProvider.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Infra - Currently not in used/Enums/OWSSOProvider.swift
@@ -10,27 +10,39 @@ import Foundation
 
 #if NEW_API
 public enum OWSSOProvider {
+    case janrain
     case gigya
     case piano
     case auth0
+    case foxid
+    case hearst
 }
 #else
 enum OWSSOProvider {
+    case janrain
     case gigya
     case piano
     case auth0
+    case foxid
+    case hearst
 }
 #endif
 
 internal extension OWSSOProvider {
     func parameters(token: String) -> OWNetworkParameters {
         switch self {
+        case .janrain:
+            return ["provider": "janrain"] // TODO: token
         case .gigya:
             return ["provider": "gigya", "uid": token]
         case .piano:
             return ["provider": "piano", "jwt_token": token]
         case .auth0:
             return ["provider": "auth0", "access_token": token]
+        case .foxid:
+            return ["provider": "foxid"] // TODO: token
+        case .hearst:
+            return ["provider": "hearst"] // TODO: token
         }
     }
 }
