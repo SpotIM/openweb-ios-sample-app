190f2eba707ee9ff85baf8b2a86daa9622982737
Nogah <nogah@applift-consulting.com>
2023-12-05 10:54:17 +0200

    Enable customizing colors in app

---
+++ Spot.IM-Development/SpotImCore/SdkAPI/Models/OWTheme.swift
@@ -0,0 +1,17 @@
+//
+//  OWTheme.swift
+//  SpotImCore
+//
+//  Created by  Nogah Melamed on 05/12/2023.
+//  Copyright © 2023 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+public struct OWTheme {
+    public let backgroundColor1: OWColor?
+
+    public init(backgroundColor1: OWColor? = nil) {
+        self.backgroundColor1 = backgroundColor1
+    }
+}
