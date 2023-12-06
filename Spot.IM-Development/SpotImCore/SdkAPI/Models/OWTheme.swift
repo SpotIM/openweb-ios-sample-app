2aa9abbdf45646babdb8a9503c79b568918d9023
Nogah <nogah@applift-consulting.com>
2023-12-06 16:57:10 +0200

    Add all customize color options

--- Spot.IM-Development/SpotImCore/SdkAPI/Models/OWTheme.swift
+++ Spot.IM-Development/SpotImCore/SdkAPI/Models/OWTheme.swift
@@ -9,13 +9,52 @@
 import Foundation
 
 public struct OWTheme {
-    public let backgroundColor1: OWColor? // TODO: remove!!
     public let skeletonColor: OWColor?
+    public let skeletonShimmeringColor: OWColor?
+    public let primarySeparatorColor: OWColor?
+    public let secondarySeparatorColor: OWColor?
+    public let tertiarySeparatorColor: OWColor?
+    public let primaryTextColor: OWColor?
+    public let secondaryTextColor: OWColor?
+    public let tertiaryTextColor: OWColor?
+    public let primaryBackgroundColor: OWColor?
+    public let secondaryBackgroundColor: OWColor?
+    public let tertiaryBackgroundColor: OWColor?
+    public let primaryBorderColor: OWColor?
+    public let secondaryBorderColor: OWColor?
+    public let loaderColor: OWColor?
+    public let brandColor: OWColor?
 
-    public init(backgroundColor1: OWColor? = nil,
-                skeletonColor: OWColor? = nil
+    public init(skeletonColor: OWColor? = nil,
+                skeletonShimmeringColor: OWColor? = nil,
+                primarySeparatorColor: OWColor? = nil,
+                secondarySeparatorColor: OWColor? = nil,
+                tertiarySeparatorColor: OWColor? = nil,
+                primaryTextColor: OWColor? = nil,
+                secondaryTextColor: OWColor? = nil,
+                tertiaryTextColor: OWColor? = nil,
+                primaryBackgroundColor: OWColor? = nil,
+                secondaryBackgroundColor: OWColor? = nil,
+                tertiaryBackgroundColor: OWColor? = nil,
+                primaryBorderColor: OWColor? = nil,
+                secondaryBorderColor: OWColor? = nil,
+                loaderColor: OWColor? = nil,
+                brandColor: OWColor? = nil
     ) {
-        self.backgroundColor1 = backgroundColor1
         self.skeletonColor = skeletonColor
+        self.skeletonShimmeringColor = skeletonShimmeringColor
+        self.primarySeparatorColor = primarySeparatorColor
+        self.secondarySeparatorColor = secondarySeparatorColor
+        self.tertiarySeparatorColor = tertiarySeparatorColor
+        self.primaryTextColor = primaryTextColor
+        self.secondaryTextColor = secondaryTextColor
+        self.tertiaryTextColor = tertiaryTextColor
+        self.primaryBackgroundColor = primaryBackgroundColor
+        self.secondaryBackgroundColor = secondaryBackgroundColor
+        self.tertiaryBackgroundColor = tertiaryBackgroundColor
+        self.primaryBorderColor = primaryBorderColor
+        self.secondaryBorderColor = secondaryBorderColor
+        self.loaderColor = loaderColor
+        self.brandColor = brandColor
     }
 }
