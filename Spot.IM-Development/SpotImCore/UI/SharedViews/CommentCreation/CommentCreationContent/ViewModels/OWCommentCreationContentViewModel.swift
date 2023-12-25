dbfd230c08d3351737a520546d44077ea4e54ad7
Nogah <nogah@applift-consulting.com>
2023-12-25 18:20:11 +0200

    Unwrap to make sure rx chain not disposed when completed event from config arrive

--- Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreation/CommentCreationContent/ViewModels/OWCommentCreationContentViewModel.swift
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/CommentCreation/CommentCreationContent/ViewModels/OWCommentCreationContentViewModel.swift
@@ -83,6 +83,7 @@ class OWCommentCreationContentViewModel: OWCommentCreationContentViewModeling,
                     return nil
                 }
             }
+            .unwrap()
             .startWith(nil)
     }()
 
