b78658a6ea763ad54d97573dd5b01f82fdfcc5ec
Refael <refaelsommer@gmail.com>
2024-03-10 09:36:13 +0200

    - Added replace text to OWTextView and implement to OWUserMentionViewVM - Fixed Range from NSRange to work with emojis

---
+++ Spot.IM-Development/SpotImCore/UI/SharedViews/OWTextView/OWTextViewReplaceData.swift
@@ -0,0 +1,14 @@
+//
+//  OWTextViewReplaceData.swift
+//  SpotImCore
+//
+//  Created by Refael Sommer on 07/03/2024.
+//  Copyright © 2024 Spot.IM. All rights reserved.
+//
+
+import Foundation
+
+struct OWTextViewReplaceData {
+    let text: String
+    let range: NSRange
+}
