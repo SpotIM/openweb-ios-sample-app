d481c217870350903ba02c300835ac8671e1e2ea
Alon Haiut <alon.h@openweb.com>
2024-03-14 17:29:30 +0200

    Changed path

--- OpenWeb-Development/scripts/building_ipa_and_app_files_amazon_s3.sh
+++ OpenWeb-Development/scripts/building_ipa_and_app_files_amazon_s3.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 date=$(date '+%d-%m-%Y')
-path="/upload_s3/beta_app/nightly/develop/$date"
+path="upload_s3/beta_app/nightly/develop/$date"
 echo "path: $path"
 
 git status
 # git checkout apple_api_key.json
-git add .
-git status
 git config credential.helper 'cache --timeout=120'
 git config --global user.email "ios-dev@openweb.com"
 git config --global user.name "OpenWeb Mobile bot via CircleCI"
