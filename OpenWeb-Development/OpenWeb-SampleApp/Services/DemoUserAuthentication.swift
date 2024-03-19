e727d5b7710e553f307217f85f241dc14d7880a6
Nogah <nogah@applift-consulting.com>
2024-03-19 15:15:53 +0200

    merge fix

--- OpenWeb-Development/OpenWeb-SampleApp/Services/DemoUserAuthentication.swift
+++ OpenWeb-Development/OpenWeb-SampleApp/Services/DemoUserAuthentication.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Alamofire
-import SpotImCore
+import OpenWebSDK
 
 internal class DemoUserAuthentication {
 
--- OpenWeb-Development/OpenWeb-SampleApp/UI/Settings/ViewModels/NetworkSettingsVM.swift
+++ OpenWeb-Development/OpenWeb-SampleApp/UI/Settings/ViewModels/NetworkSettingsVM.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import RxSwift
-import SpotImCore
+import OpenWebSDK
 
 protocol NetworkSettingsViewModelingInputs {
     var networkEnvironmentSelectedIndex: BehaviorSubject<Int> { get }
