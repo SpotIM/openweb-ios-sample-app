d92060805f4afdc74ae3764cfa94ded21d219e36
Nogah <nogah@applift-consulting.com>
2024-03-27 10:28:19 +0200

    Rename header to OpenWebSDK

--- OpenWeb-Development/OpenWeb-SDK/Resources/OpenWebSDK.h
+++ OpenWeb-Development/OpenWeb-SDK/Resources/OpenWebSDK.h
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for SpotImCore.
-FOUNDATION_EXPORT double SpotImCoreVersionNumber;
+FOUNDATION_EXPORT double OpenWebSDKVersionNumber;
 
 //! Project version string for SpotImCore.
-FOUNDATION_EXPORT const unsigned char SpotImCoreVersionString[];
+FOUNDATION_EXPORT const unsigned char OpenWebSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SpotImCore/PublicHeader.h>
