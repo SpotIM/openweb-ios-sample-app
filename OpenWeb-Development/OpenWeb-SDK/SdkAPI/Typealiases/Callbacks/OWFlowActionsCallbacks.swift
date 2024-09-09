18ef23b83fb36356a7ae575227e23e48bee1a467
Refael <refaelsommer@gmail.com>
2024-09-09 11:37:22 +0300

    - Added OWFlowActionsCallbacks.swift and OWFlowActionCallbackType.swift

---
+++ OpenWeb-Development/OpenWeb-SDK/SdkAPI/Typealiases/Callbacks/OWFlowActionsCallbacks.swift
@@ -0,0 +1,11 @@
+//
+//  OWFlowActionsCallbacks.swift
+//  OpenWebSDK
+//
+//  Created by Refael Sommer on 09/09/2024.
+//  Copyright © 2024 OpenWeb. All rights reserved.
+//
+
+import Foundation
+
+public typealias OWFlowActionsCallbacks = (OWFlowActionCallbackType, OWViewSourceType, String) -> Void
