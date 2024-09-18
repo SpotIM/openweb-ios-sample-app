16ea5d0a54c3206eaaf8e235565bf63dd657cc5f
Refael <refaelsommer@gmail.com>
2024-09-18 17:19:32 +0300

    - Removed unneeded callbacks in flows.userStatus

--- OpenWeb-SampleApp/UI/Automation/ViewModels/AutomationViewModel.swift
+++ OpenWeb-SampleApp/UI/Automation/ViewModels/AutomationViewModel.swift
@@ -102,7 +102,6 @@ fileprivate extension AutomationViewModel {
 
                 flows.userStatus(presentationalMode: OWPresentationalMode.push(navigationController: navigationController),
                                         additionalSettings: OWAutomationSettings(),
-                                        callbacks: nil,
                                         completion: { [weak self] result in
                     guard let self = self else { return }
                     switch result {
