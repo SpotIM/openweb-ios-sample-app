5f3077a390fcfcf5a4262c08e56a4a5fe8be586e
Yonat Sharon <yonat.sharon@openweb.com>
2024-09-29 09:29:12 +0300

    Revert "OW-29530 cleanup some unused extensions: String+Sizes, UIDevice.ScreenType"
    This reverts commit 7250ed5a3788b3c13300111eea824218a4d051d1.

--- OpenWeb-Development/OpenWeb-SDK/Extensions/UIDevice+Extensions.swift
+++ OpenWeb-Development/OpenWeb-SDK/Extensions/UIDevice+Extensions.swift
@@ -42,6 +42,62 @@ internal extension UIDevice {
             return "unsuppoted new value " + String(self.userInterfaceIdiom.rawValue)
         }
     }
+
+    // MARK: Screen sizes
+    var iPhoneX: Bool { UIScreen.main.nativeBounds.height == 2436 }
+    var iPhone: Bool { UIDevice.current.userInterfaceIdiom == .phone }
+    var iPad: Bool { UIDevice().userInterfaceIdiom == .pad } // swiftlint:disable:this discouraged_direct_init
+    enum ScreenType: String {
+        case iPhones_4_4S = "iPhone 4 or iPhone 4S"
+        case iPhones_5_5s_5c_SE = "iPhone 5, iPhone 5s, iPhone 5c or iPhone SE"
+        case iPhones_6_6s_7_8 = "iPhone 6, iPhone 6S, iPhone 7 or iPhone 8"
+        case iPhones_6Plus_6sPlus_7Plus_8Plus = "iPhone 6 Plus, iPhone 6S Plus, iPhone 7 Plus or iPhone 8 Plus"
+        case iPhones_X_XS = "iPhone X or iPhone XS"
+        case iPhone_XR_11 = "iPhone XR or iPhone 11"
+        case iPhone_XSMax_ProMax = "iPhone XS Max or iPhone Pro Max"
+        case iPhone_11Pro = "iPhone 11 Pro"
+        case unknown
+    }
+    var screenType: ScreenType {
+        switch UIScreen.main.nativeBounds.height {
+        case 1136:
+            return .iPhones_5_5s_5c_SE
+        case 1334:
+            return .iPhones_6_6s_7_8
+        case 1792:
+            return .iPhone_XR_11
+        case 1920, 2208:
+            return .iPhones_6Plus_6sPlus_7Plus_8Plus
+        case 2426:
+            return .iPhone_11Pro
+        case 2436:
+            return .iPhones_X_XS
+        case 2688:
+            return .iPhone_XSMax_ProMax
+        default:
+            return .unknown
+        }
+    }
+
+     var hasNotch: Bool {
+           if #available(iOS 11.0, *) {
+               // Case 1: Portrait && top safe area inset >= 44
+               let case1 = !UIDevice.current.orientation.isLandscape && (UIApplication.shared.keyWindow?.safeAreaInsets.top ?? 0) >= 44
+               // Case 2: Lanscape && left/right safe area inset > 0
+               let case2 = UIDevice.current.orientation.isLandscape &&
+               ((UIApplication.shared.keyWindow?.safeAreaInsets.left ?? 0) > 0 ||
+                (UIApplication.shared.keyWindow?.safeAreaInsets.right ?? 0) > 0)
+
+               return case1 || case2
+           } else {
+               // Fallback on earlier versions
+               return false
+           }
+       }
+
+    func isPortrait() -> Bool {
+        return UIScreen.main.bounds.width < UIScreen.main.bounds.height
+    }
 }
 
 extension UIDevice {
