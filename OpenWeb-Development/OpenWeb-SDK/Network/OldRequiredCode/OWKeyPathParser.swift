0fbe57f4f2be6adb0da76b43e2dde074f82adb57
Yonat Sharon <yonat.sharon@openweb.com>
2024-09-29 09:29:59 +0300

    Revert "OW-29530 cleanup some unused classes: OWHostApplicationHelper, OWNetworkAuthenticationInterceptor, OWAuthenticationRenewerService, OWKeyPathParser"
    This reverts commit d12fe34e5cfdca0b447258dce9b3b7efc0bb68e8.

---
+++ OpenWeb-Development/OpenWeb-SDK/Network/OldRequiredCode/OWKeyPathParser.swift
@@ -0,0 +1,36 @@
+//
+//  KeyPathParser.swift
+//  APIClient
+//
+
+import Foundation
+
+public typealias JSON = [String: Any]
+
+public enum OWParserError: Error {
+
+    case keyNotFound
+    case generalParseError
+
+}
+
+internal class OWKeyPathParser {
+
+    private let keyPath: String?
+
+    public init(keyPath: String? = nil) {
+        self.keyPath = keyPath
+    }
+
+    internal func valueForKeyPath(in object: Any) throws -> Any {
+        if let keyPath = keyPath, let dictionary = object as? JSON {
+            if let value = dictionary[keyPath] {
+                return value
+            }
+            throw OWParserError.keyNotFound
+        } else {
+            return object
+        }
+    }
+
+}
