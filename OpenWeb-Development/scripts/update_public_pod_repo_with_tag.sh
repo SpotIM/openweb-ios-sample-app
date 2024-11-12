5effd34c03e74f91cbc5a4e0efcbd444fec837a3
Alon Shprung <alonshp1@gmail.com>
2024-11-12 18:26:07 +0200

    update release ci scripts to support adapter target

--- OpenWeb-Development/scripts/update_public_pod_repo_with_tag.sh
+++ OpenWeb-Development/scripts/update_public_pod_repo_with_tag.sh
@@ -12,11 +12,27 @@ rm -fr OpenWebSDK.xcframework
 ls -l
 cp -r ../Release/OpenWebSDK.xcframework .
 ls -l
+
 PREVIOUS_SDK_VERSION=`cat OpenWebSDK.podspec | grep -m 1 s.version |  cut -d "=" -f2 | cut -d \" -f2 | cut -d \' -f2`
 echo "OpenWebSDK.podspec - replacing previous version ($PREVIOUS_SDK_VERSION) with current version ($RELEASE_VERSION)"
-sed -i '' -e "s/${PREVIOUS_SDK_VERSION}/${RELEASE_VERSION}/g" OpenWebSDK.podspec
+sed -i '' -e "/s.version *= /s/'${PREVIOUS_SDK_VERSION}'/'${RELEASE_VERSION}'/" OpenWebSDK.podspec
 echo "OpenWebSDK.podspec - update source tag to $TAG"
 sed -i '' -e "s/tag => s.version.to_s/tag => '${TAG}'/g" OpenWebSDK.podspec
+
+echo "OpenWebSDK.podspec - update OpenWebSDKAdapter dependency to use tag - $TAG"
+sed -i '' -e "/s.dependency 'OpenWebSDKAdapter'/s|,.*|, :git => 'https://github.com/SpotIM/openweb-ios-sdk-pod.git', :tag => '${TAG}'|" OpenWebSDK.podspec
+
+echo "OpenWebSDKAdapter.podspec - replacing previous version ($PREVIOUS_SDK_VERSION) with current version ($RELEASE_VERSION)"
+sed -i '' -e "/s.version *= /s/'${PREVIOUS_SDK_VERSION}'/'${RELEASE_VERSION}'/" OpenWebSDKAdapter.podspec
+echo "OpenWebSDKAdapter.podspec - update source tag to $TAG"
+sed -i '' -e "s/tag => s.version.to_s/tag => '${TAG}'/g" OpenWebSDKAdapter.podspec
+
+echo "Update OpenWebSDKAdapter files"
+if [ -d "OpenWebSDKAdapter" ]; then
+rm -rf "OpenWebSDKAdapter"
+fi
+cp -r ../OpenWeb-Development/OpenWeb-SDKAdapter OpenWebSDKAdapter
+
 git status
 git add .
 git status
