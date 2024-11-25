6518cee759eee95dd0d9304e89aa399aa5c9f15a
Alon Shprung <alonshp1@gmail.com>
2024-11-25 15:49:34 +0200

    prepare temp iau sdk

---
+++ OpenWeb-Development/IAUTempSDK/OpenWebIAUSDK.xcframework/ios-arm64/OpenWebIAUSDK.framework/Headers/SpotImStandaloneAds.h
@@ -0,0 +1,13 @@
+//
+//  SpotImStandaloneAds.h
+//  OpenWebIAUSDK
+//  Created by Pavel Yevtukhov on 27.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFra.
+FOUNDATION_EXPORT double SpotImStandaloneAdsVersionNumber;
+
+//! Project version string for SampleFra.
+FOUNDATION_EXPORT const unsigned char SpotImStandaloneAdsVersionString[];
