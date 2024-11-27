28bf94e05e93f17f1d573de1c45151c2ec1d31f2
AnaelPeg <anael@applift-consulting.com>
2024-11-27 11:13:59 +0200

    Add logger to independent monetization example

--- OpenWeb-SampleApp/UI/Monetization/ViewModels/IndependentMonetizationExampleViewModel.swift
+++ OpenWeb-SampleApp/UI/Monetization/ViewModels/IndependentMonetizationExampleViewModel.swift
@@ -14,6 +14,7 @@ protocol IndependentMonetizationExampleViewModelingInputs {}
 
 protocol IndependentMonetizationExampleViewModelingOutputs {
     var title: String { get }
+    var loggerViewModel: UILoggerViewModeling { get }
     var showAdView: Observable<UIView> { get }
 }
 
@@ -26,6 +27,7 @@ class IndependentMonetizationExampleViewModel: IndependentMonetizationExampleVie
     var inputs: IndependentMonetizationExampleViewModelingInputs { return self }
     var outputs: IndependentMonetizationExampleViewModelingOutputs { return self }
     
+    private let postId: OWPostId
     private var manager = OpenWebIAU.manager
     private let _showAdView = BehaviorSubject<UIView?>(value: nil)
     var showAdView: Observable<UIView> {
@@ -33,27 +35,32 @@ class IndependentMonetizationExampleViewModel: IndependentMonetizationExampleVie
             .unwrap()
             .asObservable()
     }
-   
-    lazy var title: String = {
-        return NSLocalizedString("Independent Example", comment: "")
-    }()
-    
-    private let postId: OWPostId
-
+        
     init(postId: OWPostId) {
         self.postId = postId
         setupObservers()
     }
+    
+    lazy var title: String = {
+        return NSLocalizedString("Independent Example", comment: "")
+    }()
+    
+    lazy var loggerViewModel: UILoggerViewModeling = {
+        return UILoggerViewModel(title: "Independent example logger")
+    }()
 }
 
 private extension IndependentMonetizationExampleViewModel {
     func setupObservers() {
         let adConfiguration = OWIAUAdConfiguration.server(remote: .tmsServer(index: 0))
         let adSettings: OWIAUAdSettingsProtocol = OWIAUAdSettings(configuration: adConfiguration)
-    
+        let viewEventCallbacks: OWIAUAdViewEventsCallbacks = { [weak self] eventType, _, _ in
+            self?.loggerViewModel.inputs.log(text: eventType.description)
+        }
+        
         manager.ui.ad(postId: postId,
                       settings: adSettings,
-                      viewEventCallbacks: nil,
+                      viewEventCallbacks: viewEventCallbacks,
                       actionsCallbacks: nil,
                       completion: { [weak self] result in
             guard let self else { return }
@@ -61,7 +68,7 @@ private extension IndependentMonetizationExampleViewModel {
             case .success(let adView):
                 _showAdView.onNext(adView)
             case .failure(let error):
-                print(error.localizedDescription)
+                DLog("Independent monetization example failed: \(error.localizedDescription)")
             }
         })
     }
--- OpenWeb-SampleApp/UI/Monetization/Views/IndependentMonetizationExampleVC.swift
+++ OpenWeb-SampleApp/UI/Monetization/Views/IndependentMonetizationExampleVC.swift
@@ -12,11 +12,19 @@ import RxSwift
 class IndependentMonetizationExampleVC: UIViewController {
     private struct Metrics {
         static let identifier = "uiviews_monetization_independent_example_id"
+        static let adViewIdentifier = "independent_monetization_ad_view_id"
+        static let loggerHeight: CGFloat = 0.3 * (UIApplication.shared.delegate?.window??.screen.bounds.height ?? 800)
+        static let adViewTopMargin: CGFloat = 16
+        static let adViewHorizontalMargin: CGFloat = -16
     }
 
     private let viewModel: IndependentMonetizationExampleViewModeling
     private let disposeBag = DisposeBag()
 
+    private lazy var loggerView: UILoggerView = {
+        return UILoggerView(viewModel: viewModel.outputs.loggerViewModel)
+    }()
+   
     init(viewModel: IndependentMonetizationExampleViewModeling) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -46,6 +54,13 @@ private extension IndependentMonetizationExampleVC {
     func setupViews() {
         view.backgroundColor = ColorPalette.shared.color(type: .background)
         self.navigationItem.largeTitleDisplayMode = .never
+        
+        self.view.addSubview(self.loggerView)
+        self.loggerView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.layoutMarginsGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(Metrics.loggerHeight)
+        }
     }
     
     func setupObservers() {
@@ -55,13 +70,14 @@ private extension IndependentMonetizationExampleVC {
             .observe(on: MainScheduler.instance)
             .subscribe { [weak self] adView in
                 guard let self else { return }
-                view.addSubview(adView)
-                adView.backgroundColor = .red
+               
+                self.view.addSubview(adView)
                 adView.snp.makeConstraints { make in
-                    make.top.equalTo(self.view.layoutMarginsGuide.snp.top)
+                    make.top.equalTo(self.loggerView.snp.bottom).offset(Metrics.adViewTopMargin)
                     make.centerX.equalToSuperview()
-                    make.width.equalToSuperview().offset(-16)
+                    make.width.equalToSuperview().offset(Metrics.adViewHorizontalMargin)
                 }
+                adView.accessibilityIdentifier = Metrics.adViewIdentifier
             }
             .disposed(by: disposeBag)
     }
