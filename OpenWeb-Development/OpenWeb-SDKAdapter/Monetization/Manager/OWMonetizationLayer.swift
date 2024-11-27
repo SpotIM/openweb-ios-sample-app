9397eb0a3a43d772e7f3cb6c5ca9cce0e116855e
AnaelPeg <anael@applift-consulting.com>
2024-11-27 18:05:26 +0200

    Expose IAU sdk in monetization layer

--- OpenWeb-Development/OpenWeb-SDKAdapter/Monetization/Manager/OWMonetizationLayer.swift
+++ OpenWeb-Development/OpenWeb-SDKAdapter/Monetization/Manager/OWMonetizationLayer.swift
@@ -13,8 +13,20 @@ import Foundation
 @_exported import OpenWebIAUSDK
 
 public class OWMonetizationLayer: OWMonetization {
-    // TODO - lmplementation
-    public init() {}
+    public var ui: OpenWebIAUSDK.OWIAUUI
+    public var analytics: OpenWebIAUSDK.OWIAUAnalytics
+    public var settings: OpenWebIAUSDK.OWIAUSettingsProtocol
+    public var helpers: OpenWebIAUSDK.OWIAUHelpers
+    
+    public init(ui: OpenWebIAUSDK.OWIAUUI = OpenWebIAU.manager.ui,
+                analytics: OpenWebIAUSDK.OWIAUAnalytics = OpenWebIAU.manager.analytics,
+                settings: OpenWebIAUSDK.OWIAUSettingsProtocol = OpenWebIAU.manager.settings,
+                helpers: OpenWebIAUSDK.OWIAUHelpers = OpenWebIAU.manager.helpers) {
+        self.ui = ui
+        self.analytics = analytics
+        self.settings = settings
+        self.helpers = helpers
+    }
 }
 
 #else
