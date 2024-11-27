7c78255d497a2acaa3c8122e8978b2122f3b28c1
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2024-11-27 14:13:17 +0200

    Merge pull request #1141 from SpotIM/task/OW-26123/error-toast-for-delete-comment
    OW-26123 - Added toast with retry for error in delete comment

--- OpenWeb-Development/OpenWeb-SDK/UI/PreConversation/ViewModels/OWPreConversationViewVM.swift
+++ OpenWeb-Development/OpenWeb-SDK/UI/PreConversation/ViewModels/OWPreConversationViewVM.swift
@@ -362,6 +362,7 @@ class OWPreConversationViewViewModel: OWPreConversationViewViewModeling,
     private var deleteComment = PublishSubject<OWCommentViewModeling>()
     private var muteCommentUser = PublishSubject<OWCommentViewModeling>()
     private var retryMute = PublishSubject<Void>()
+    private var retryDelete = PublishSubject<Void>()
 
     var viewInitialized = PublishSubject<Void>()
     private lazy var viewInitializedObservable: Observable<OWLoadingTriggeredReason> = {
@@ -1289,13 +1290,17 @@ private extension OWPreConversationViewViewModel {
                     .response
                     .materialize()
             }
-            .map { event -> OWCommentDelete? in
+            .map { [weak self] event -> OWCommentDelete? in
+                guard let self else { return nil }
                 switch event {
                 case .next(let commentDelete):
                     // TODO: Clear any RX variables which affect error state in the View layer (like _shouldShowError).
                     return commentDelete
                 case .error:
-                    // TODO: handle error - update something like _shouldShowError RX variable which affect the UI state for showing error in the View layer
+                    let data = OWToastRequiredData(type: .warning, action: .tryAgain, title: OWLocalizationManager.shared.localizedString(key: "SomethingWentWrong"))
+                    self.servicesProvider.toastNotificationService()
+                        .showToast(data: OWToastNotificationCombinedData(presentData: OWToastNotificationPresentData(data: data),
+                                                                         actionCompletion: self.retryDelete))
                     return nil
                 default:
                     return nil
@@ -1398,7 +1403,6 @@ private extension OWPreConversationViewViewModel {
                                                                          actionCompletion: nil))
                     return true
                 case .error:
-                    // TODO: handle error - update something like _shouldShowError RX variable which affect the UI state for showing error in the View layer
                     let data = OWToastRequiredData(type: .warning, action: .tryAgain, title: OWLocalizationManager.shared.localizedString(key: "SomethingWentWrong"))
                     self.servicesProvider.toastNotificationService()
                         .showToast(data: OWToastNotificationCombinedData(presentData: OWToastNotificationPresentData(data: data),
@@ -1414,14 +1418,16 @@ private extension OWPreConversationViewViewModel {
             })
             .disposed(by: disposeBag)
 
-        // Retry when triggerd
+        // Retry mute when triggerd
         retryMute
-            .withLatestFrom(muteCommentUser) { _, comment -> OWCommentViewModeling in
-                return comment
-            }
-            .subscribe(onNext: { [weak self] comment in
-                self?.muteCommentUser.onNext(comment)
-            })
+            .withLatestFrom(muteCommentUser)
+            .bind(to: muteCommentUser)
+            .disposed(by: disposeBag)
+
+        // Retry delete when triggerd
+        retryDelete
+            .withLatestFrom(deleteComment)
+            .bind(to: deleteComment)
             .disposed(by: disposeBag)
 
         // Handling muting comments "locally" of a muted user
