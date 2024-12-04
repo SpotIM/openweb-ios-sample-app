13103cd131ea9adf762c1b099c4cf08f78614276
AnaelPeg <anael@applift-consulting.com>
2024-12-04 15:23:02 +0200

    Create OWMonetizationBridge for using IAU in social sdk

---
+++ OpenWeb-Development/OpenWeb-SDKAdapter/Monetization/Bridge/OWMonetizationBridge.swift
@@ -0,0 +1,54 @@
+//
+//  OWMonetizationBridge.swift
+//  OpenWeb-Development
+//
+//  Created by Anael on 04/12/2024.
+//  Copyright © 2024 OpenWeb. All rights reserved.
+//
+
+import UIKit
+
+#if canImport(OpenWebIAUSDK)
+@_exported import OpenWebIAUSDK
+#endif
+
+public protocol OWMonetizationBridgeProtocol {
+    func getAd(postId: String,
+               tmsServerIndex: Int,
+               completion: @escaping (Result<UIView, Error>) -> Void)
+}
+
+public class OWMonetizationBridge: OWMonetizationBridgeProtocol {
+    public func getAd(postId: String,
+                      tmsServerIndex: Int,
+                      completion: @escaping (Result<UIView, any Error>) -> Void) {
+        guard isIAUAvailble() else { return }
+        
+        let adConfiguration = OWIAUAdConfiguration.server(remote: .tmsServer(index: tmsServerIndex))
+        let adSettings: OWIAUAdSettingsProtocol = OWIAUAdSettings(configuration: adConfiguration)
+        
+        OpenWebIAU.manager.ui.ad(postId: postId,
+                                 settings: adSettings,
+                                 viewEventCallbacks: nil,
+                                 actionsCallbacks: nil,
+                                 completion: { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let adView):
+                completion(.success(adView))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
+
+private extension OWMonetizationBridge {
+    func isIAUAvailble() -> Bool {
+        #if canImport(OpenWebIAUSDK)
+            return true
+        #else
+            return false
+        #endif
+    }
+}
