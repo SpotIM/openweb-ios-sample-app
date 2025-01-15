43443cb1ece348aca6fda0f5093f9cf99161d8f4
AnaelPeg <anael@applift-consulting.com>
2025-01-15 17:18:43 +0200

    OW-30449 Get preconversation in table view vm and not in cell vm

--- OpenWeb-SampleApp/UI/Monetization/ViewModels/PreconversationWithAdExample/PreconversationWithAdViewModel.swift
+++ OpenWeb-SampleApp/UI/Monetization/ViewModels/PreconversationWithAdExample/PreconversationWithAdViewModel.swift
@@ -8,7 +8,10 @@
 import RxSwift
 import OpenWebSDK
 
-protocol PreconversationWithAdViewModelingInputs {}
+protocol PreconversationWithAdViewModelingInputs {
+    func setNavigationController(_ navController: UINavigationController?)
+    func setPresentationalVC(_ viewController: UIViewController)
+}
 
 protocol PreconversationWithAdViewModelingOutputs {
     var title: String { get }
@@ -35,8 +38,10 @@ class PreconversationWithAdViewModel: PreconversationWithAdViewModeling, Preconv
     private let imageProviderAPI: ImageProviding
     private let postId: OWPostId
     private let userDefaultsProvider: UserDefaultsProviderProtocol
-    private let actionSettings: SDKUIFlowActionSettings
     private let commonCreatorService: CommonCreatorServicing
+    private weak var navController: UINavigationController?
+    private weak var presentationalVC: UIViewController?
+
     private let _articleImageURL = BehaviorSubject<URL?>(value: nil)
     var cells: Observable<[PreconversationWithAdCellOption]> = Observable.just(PreconversationWithAdCellOption.allCases)
     var articleImageURL: Observable<URL> {
@@ -45,11 +50,18 @@ class PreconversationWithAdViewModel: PreconversationWithAdViewModeling, Preconv
             .asObservable()
     }
 
+    private let _actionSettings = BehaviorSubject<SDKUIFlowActionSettings?>(value: nil)
+    private var actionSettings: Observable<SDKUIFlowActionSettings> {
+        return _actionSettings
+            .unwrap()
+            .asObservable()
+    }
+
     lazy var preconversationCellViewModel: PreconversationCellViewModeling = {
         PreconversationCellViewModel(
             userDefaultsProvider: userDefaultsProvider,
-            actionSettings: actionSettings,
-            commonCreatorService: commonCreatorService
+            showPreConversation: showPreConversation,
+            adSizeChanged: adSizeChanged
         )
     }()
 
@@ -61,8 +73,20 @@ class PreconversationWithAdViewModel: PreconversationWithAdViewModeling, Preconv
         return NSLocalizedString("MockArticle", comment: "")
     }()
 
-    var loggerEnabled: Observable<Bool> {
-        return preconversationCellViewModel.outputs.loggerEnabled
+    private let _showPreConversation = BehaviorSubject<UIView?>(value: nil)
+    var showPreConversation: Observable<UIView?> {
+        return _showPreConversation
+            .asObservable()
+    }
+
+    lazy var loggerEnabled: Observable<Bool> = {
+        return userDefaultsProvider.values(key: .flowsLoggerEnabled, defaultValue: false)
+    }()
+
+    private let _adSizeChanged = PublishSubject<Void>()
+    var adSizeChanged: Observable<Void> {
+        return _adSizeChanged
+            .asObservable()
     }
 
     lazy var loggerViewModel: UILoggerViewModeling = {
@@ -83,10 +107,19 @@ class PreconversationWithAdViewModel: PreconversationWithAdViewModeling, Preconv
         self.userDefaultsProvider = userDefaultsProvider
         self.commonCreatorService = commonCreatorService
         self.imageProviderAPI = imageProviderAPI
-        self.actionSettings = actionSettings
+        _actionSettings.onNext(actionSettings)
         self.postId = postId
 
         setupObservers()
+        setupBICallaback()
+    }
+
+    func setNavigationController(_ navController: UINavigationController?) {
+        self.navController = navController
+    }
+
+    func setPresentationalVC(_ viewController: UIViewController) {
+        presentationalVC = viewController
     }
 }
 
@@ -95,12 +128,119 @@ private extension PreconversationWithAdViewModel {
         let articleURL = imageProviderAPI.randomImageUrl()
         _articleImageURL.onNext(articleURL)
 
-        Observable.merge(preconversationCellViewModel.outputs.loggerEvents,
-                         independentAdCellViewModel.outputs.loggerEvents)
+        independentAdCellViewModel.outputs.loggerEvents
             .subscribe(onNext: { [weak self] logEvent in
                 self?.loggerViewModel.inputs.log(text: logEvent)
             })
             .disposed(by: disposeBag)
 
+        actionSettings
+            .map { settings -> (PresentationalModeCompact, String)? in
+                if case .preConversation(let mode) = settings.actionType {
+                    return (mode, settings.postId)
+                } else {
+                    return nil
+                }
+            }
+            .unwrap()
+        // Small delay so the navigation controller will be set from the view controller
+            .delay(.milliseconds(50), scheduler: ConcurrentDispatchQueueScheduler(qos: .userInteractive))
+            .withLatestFrom(loggerEnabled) { result, loggerEnabled -> (PresentationalModeCompact, String, Bool) in
+                return (result.0, result.1, loggerEnabled)
+            }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                guard let self else { return }
+                let mode = result.0
+                let postId = result.1
+                let loggerEnabled = result.2
+
+                let manager = OpenWeb.manager
+                let flows = manager.ui.flows
+
+                let additionalSettings = self.commonCreatorService.additionalSettings()
+                let article = self.commonCreatorService.mockArticle(for: manager.spotId)
+
+                guard let presentationalMode = self.presentationalMode(fromCompactMode: mode) else { return }
+
+                if shouldUseAsyncAwaitCallingMethod() {
+                    Task { @MainActor [weak self] in
+                        guard let self else { return }
+                        do {
+                            let preConversationView = try await flows.preConversation(
+                                postId: postId,
+                                article: article,
+                                presentationalMode: presentationalMode,
+                                additionalSettings: additionalSettings,
+                                callbacks: actionCallbacks(loggerEnabled: loggerEnabled)
+                            )
+                            _showPreConversation.onNext(preConversationView)
+                        } catch {
+                            DLog("Calling flows.preConversation error: \(error)")
+                        }
+                    }
+                } else {
+                    flows.preConversation(postId: postId,
+                                          article: article,
+                                          presentationalMode: presentationalMode,
+                                          additionalSettings: additionalSettings,
+                                          callbacks: actionCallbacks(loggerEnabled: loggerEnabled),
+                                          completion: { [weak self] result in
+                        guard let self else { return }
+                        switch result {
+                        case .success(let preConversationView):
+                            self._showPreConversation.onNext(preConversationView)
+                        case .failure(let error):
+                            DLog("Calling flows.preConversation error: \(error)")
+                        }
+                    })
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+
+    func setupBICallaback() {
+        let analytics: OWAnalytics = OpenWeb.manager.analytics
+
+        let BIClosure: OWBIAnalyticEventCallback = { [weak self] event, additionalInfo, postId in
+            let log = "Received BI Event: \(event), additional info: \(additionalInfo), postId: \(postId)\n"
+            self?.loggerViewModel.inputs.log(text: log)
+        }
+
+        analytics.addBICallback(BIClosure)
+    }
+
+    func actionCallbacks(loggerEnabled: Bool) -> OWFlowActionsCallbacks? {
+        return { [weak self] callbackType, sourceType, postId in
+            guard let self else { return }
+
+            switch callbackType {
+            case .adSizeChanged:
+                _adSizeChanged.onNext()
+            case .adEvent(event: let event):
+                let log = "preconversationAd: \(event.description)\n"
+                self.loggerViewModel.inputs.log(text: log)
+            default:
+                guard loggerEnabled else { return }
+                let log = "Received OWFlowActionsCallback type: \(callbackType), from source: \(sourceType), postId: \(postId)\n"
+                self.loggerViewModel.inputs.log(text: log)
+            }
+        }
+    }
+
+    func shouldUseAsyncAwaitCallingMethod() -> Bool {
+        return SampleAppCallingMethod.asyncAwait == userDefaultsProvider.get(key: .callingMethodOption, defaultValue: .default)
+    }
+
+    func presentationalMode(fromCompactMode mode: PresentationalModeCompact) -> OWPresentationalMode? {
+        guard let navController = self.navController,
+              let presentationalVC = self.presentationalVC else { return nil }
+
+        switch mode {
+        case .present(let style):
+            return OWPresentationalMode.present(viewController: presentationalVC, style: style)
+        case .push:
+            return OWPresentationalMode.push(navigationController: navController)
+        }
     }
 }
