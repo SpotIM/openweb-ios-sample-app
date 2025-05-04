499238b53b11242cae99ec6a7c0e2c162c426368
Yonat Sharon <yoant.sharon@openweb.com>
2025-05-04 15:05:49 +0300

    OW-33050 Fix report reason header color

---
+++ OpenWeb-SampleApp/XcodePreviews/OWReportReasonView-preview.swift
@@ -0,0 +1,71 @@
+//
+//  OWReportReasonView-preview.swift
+//  OpenWeb-SampleApp
+//
+//  Created by Yonat Sharon on 04/05/2025.
+//
+
+#if DEBUG
+@testable import OpenWebSDK
+import RxSwift
+import UIKit
+
+class MockSpotConfigurationService: OWSpotConfigurationServicing {
+    func spotChanged(spotId: OpenWebCommon.OWSpotId) {}
+
+    func config(spotId: String) -> Observable<SPSpotConfiguration> {
+        let jsonString = """
+        {
+            "shared": {
+                "votesType": "updown",
+                "reportReasonsOptions": {
+                    "reasons": [
+                        {"reportType": "spam", "requiredAdditionalInfo": false},
+                        {"reportType": "identity_attack", "requiredAdditionalInfo": false},
+                        {"reportType": "hate_speech", "requiredAdditionalInfo": false},
+                        {"reportType": "false_information", "requiredAdditionalInfo": true}
+                    ]
+                },
+                "communityGuidelines": "Community guidelines text. Click here to learn more."
+            },
+            "mobile-sdk": {
+                "reportReasonsCounterMaxLength": 280,
+                "openwebWebsiteUrl": "https://www.openweb.com",
+                "openwebPrivacyUrl": "https://www.openweb.com/privacy-policy",
+                "openwebTermsUrl": "https://www.openweb.com/terms-of-use",
+                "imageUploadBaseUrl": "https://images.openweb.com",
+                "fetchImageBaseUrl": "https://images.openweb.com",
+                "enableCommentEditing": true
+            }
+        }
+        """
+
+        do {
+            let config = try JSONDecoder().decode(SPSpotConfiguration.self, from: jsonString.data(using: .utf8)!)
+            return .just(config)
+        } catch {
+            print(error)
+            return .empty()
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    let reportReasonView = OWReportReasonView(viewModel: OWReportReasonViewViewModel(
+        reportData: OWReportReasonsRequiredData(commentId: "comment123", parentId: "parent123", postId: "post123"),
+        viewableMode: .partOfFlow,
+        presentationalMode: .none,
+        servicesProvider: MockServicesProvider(spotConfigurationService: MockSpotConfigurationService())
+    ))
+
+    let themeInjectorView = ThemeInjectorView()
+    themeInjectorView.addSubview(reportReasonView)
+    reportReasonView.OWSnp.makeConstraints { make in
+        make.edges.equalToSuperview()
+    }
+
+    return themeInjectorView
+}
+
+#endif
--- OpenWeb-SampleApp/XcodePreviews/OWRoundCheckBox-preview.swift
+++ OpenWeb-SampleApp/XcodePreviews/OWRoundCheckBox-preview.swift
@@ -10,18 +10,6 @@
 import SnapKit
 import UIKit
 
-class ThemeInjectorView: UIView, OWThemeStyleInjectorProtocol {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        useAsThemeStyleInjector()
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 @available(iOS 17.0, *)
 #Preview {
     let checkBox = OWRoundCheckBox()
