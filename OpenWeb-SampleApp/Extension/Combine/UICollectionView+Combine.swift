c5ffbbfca9d5810d4a115533c0a37082ab1d0e10
Yonat Sharon <yonat.sharon@openweb.com>
2025-05-29 14:10:59 +0300

    OW-32108 Fix doc comments (error caught by CoPilot)

--- OpenWeb-SampleApp/Extension/Combine/UICollectionView+Combine.swift
+++ OpenWeb-SampleApp/Extension/Combine/UICollectionView+Combine.swift
@@ -9,11 +9,11 @@ import Combine
 import CombineDataSources
 
 extension UICollectionView {
-    /// A table view specific `Subscriber` that receives `[Element]` input and updates a single section table view,
+    /// A collection view specific `Subscriber` that receives `[Element]` input and updates a single section collection view,
     /// with `reuseIdentifier: cellType.identifierName`.
-    /// - Parameter cellType: The required cell type for table rows.
+    /// - Parameter cellType: The required cell type for collection rows.
     /// - Parameter cellConfig: A closure that receives an initialized cell and a collection element
-    ///     and configures the cell for displaying in its containing table view.
+    ///     and configures the cell for displaying in its containing collection view.
     public func itemsSubscriber<CellType, Items>(cellType: CellType.Type, cellConfig: @escaping CollectionViewItemsController<[Items]>.CellConfig<Items.Element, CellType>)
         -> AnySubscriber<Items, Never> where CellType: UICollectionViewCell,
         Items: RandomAccessCollection,
