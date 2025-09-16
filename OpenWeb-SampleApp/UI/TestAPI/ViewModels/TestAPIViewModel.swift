ee7e0f8e3b97dbca264fe621c20930ec312da84f
Refael <refaelsommer@gmail.com>
2025-09-16 18:18:03 +0300

    OW-34674 Added joinedString for collections Added some configuration events

--- OpenWeb-SampleApp/UI/TestAPI/ViewModels/TestAPIViewModel.swift
+++ OpenWeb-SampleApp/UI/TestAPI/ViewModels/TestAPIViewModel.swift
@@ -397,6 +397,8 @@ private extension TestAPIViewModel {
         helpers.languageStrategy = UserDefaultsProvider.shared.get(key: .languageStrategy, defaultValue: OWLanguageStrategy.default)
         helpers.localeStrategy = UserDefaultsProvider.shared.get(key: .localeStrategy, defaultValue: OWLocaleStrategy.default)
         helpers.orientationEnforcement = UserDefaultsProvider.shared.get(key: .orientationEnforcement, defaultValue: OWOrientationEnforcement.default)
+        helpers.loggerConfiguration.level = .medium
+        helpers.loggerConfiguration.methods = [.nsLog]
         let authentication = OpenWeb.manager.authentication
         authentication.shouldDisplayLoginPrompt = UserDefaultsProvider.shared.get(key: .showLoginPrompt, defaultValue: false)
         customizations.commentActions.color = UserDefaultsProvider.shared.get(key: .commentActionsColor, defaultValue: OWCommentActionsColor.default)
