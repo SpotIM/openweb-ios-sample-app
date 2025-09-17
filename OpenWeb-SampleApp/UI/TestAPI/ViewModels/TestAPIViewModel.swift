d47b4c8df6bd99e4404d17e58ba2ea9830bd3aa6
Refael <refaelsommer@gmail.com>
2025-09-17 18:25:12 +0300

    OW-34674 Added theme colors events

--- OpenWeb-SampleApp/UI/TestAPI/ViewModels/TestAPIViewModel.swift
+++ OpenWeb-SampleApp/UI/TestAPI/ViewModels/TestAPIViewModel.swift
@@ -415,7 +415,6 @@ private extension TestAPIViewModel {
         let BIClosure: OWBIAnalyticEventCallback = { event, additionalInfo, postId in
             DLog("Received BI Event: \(event), additional info: \(additionalInfo), postId: \(postId)")
         }
-
         analytics.addBICallback(BIClosure)
     }
 
