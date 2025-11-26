e2f05217964d5922523de4f558b5e4780d368e06
AnaelPeg <anael@applift-consulting.com>
2025-11-26 14:44:10 +0200

    OW-34506 Create previews

--- OpenWeb-SampleApp/XcodePreviews/OWCommentCreationView-preview.swift
+++ OpenWeb-SampleApp/XcodePreviews/OWCommentCreationView-preview.swift
@@ -82,4 +82,66 @@ extension OWCommentCreationRequiredData {
     viewController.view.bringSubviewToFront(mockKeyboardView)
     return viewController
 }
+
+
+@available(iOS 17.0, *)
+#Preview("regular with nudge") {
+    let viewModel = OWCommentCreationViewViewModel(
+        commentCreationData: .mock(commentCreationStyle: .regular),
+        viewableMode: .independent
+    )
+
+    let nudgeData = OWModerationNudgeData(state: .pending, tag: .toxicity)
+    viewModel.outputs.commentCreationRegularViewVm.inputs.showModerationNudge.send(nudgeData)
+
+    return OWCommentCreationView(viewModel: viewModel)
+}
+
+@available(iOS 17.0, *)
+#Preview("light with nudge") {
+    let viewModel = OWCommentCreationViewViewModel(
+        commentCreationData: .mock(commentCreationStyle: .light),
+        viewableMode: .independent
+    )
+
+    let nudgeData = OWModerationNudgeData(state: .pending, tag: .toxicity)
+    viewModel.outputs.commentCreationLightViewVm.inputs.showModerationNudge.send(nudgeData)
+
+    return OWCommentCreationView(viewModel: viewModel)
+}
+
+@available(iOS 17.0, *)
+#Preview("floating with nudge") {
+    let viewController = UIViewController() // ensure full screen
+    viewController.view.backgroundColor = .secondarySystemBackground
+
+    let mockKeyboardView = UIImageView(image: UIImage(systemName: "keyboard"))
+    mockKeyboardView.contentMode = .scaleAspectFit
+    mockKeyboardView.tintColor = .systemBackground
+    mockKeyboardView.backgroundColor = .placeholderText
+    viewController.view.addSubview(mockKeyboardView)
+    mockKeyboardView.snp.makeConstraints { make in
+        make.leading.trailing.bottom.equalToSuperview()
+        make.height.equalTo(300)
+    }
+
+    let viewModel = OWCommentCreationViewViewModel(
+        commentCreationData: .mock(commentCreationStyle: .floatingKeyboard),
+        viewableMode: .independent
+    )
+
+    let nudgeData = OWModerationNudgeData(state: .pending, tag: .toxicity)
+    viewModel.outputs.commentCreationFloatingKeyboardViewVm.inputs.showModerationNudge.send(nudgeData)
+
+    let commentCreationView = OWCommentCreationView(viewModel: viewModel)
+
+    viewController.view.addSubview(commentCreationView)
+    commentCreationView.snp.makeConstraints { make in
+        make.leading.trailing.equalToSuperview()
+        make.bottom.equalTo(mockKeyboardView.snp.top)
+    }
+
+    viewController.view.bringSubviewToFront(mockKeyboardView)
+    return viewController
+}
 #endif
