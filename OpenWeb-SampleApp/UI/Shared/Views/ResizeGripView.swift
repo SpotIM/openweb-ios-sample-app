fb3631085432c043458c008e43ee681fced75db3
Yonat Sharon <yonat.sharon@openweb.com>
2025-12-12 10:02:06 +0200

    OW-36762 Sample app: Allow 0 height view below partial screen conversation

--- OpenWeb-SampleApp/UI/Shared/Views/ResizeGripView.swift
+++ OpenWeb-SampleApp/UI/Shared/Views/ResizeGripView.swift
@@ -8,30 +8,29 @@ import SnapKit
 
 class ResizeGripView: UIView {
 
-    struct Metrics {
-        var handleWidth: CGFloat = 40
-        var handleHeight: CGFloat = 4
-        var topPadding: CGFloat = 8
+    private struct Metrics {
+        static let handleWidth: CGFloat = 40
+        static let handleHeight: CGFloat = 4
+        static let handlePadding: CGFloat = 8
         // swiftlint:disable:next no_magic_numbers
-        var handleColor: UIColor = .white.withAlphaComponent(0.6)
-        var minHeight: CGFloat = 50
-        var maxHeight: CGFloat = 400
+        static let handleColor: UIColor = .white.withAlphaComponent(0.6)
+        static let defaultHeight: CGFloat = 50
     }
 
-    private let metrics: Metrics
+    private let maxHeight: CGFloat
     private weak var targetView: UIView?
     private var heightConstraint: Constraint?
     private var currentHeight: CGFloat = 0
 
     private lazy var handleView: UIView = {
         let view = UIView()
-        view.backgroundColor = metrics.handleColor
-        view.layer.cornerRadius = metrics.handleHeight / 2
+        view.backgroundColor = Metrics.handleColor
+        view.layer.cornerRadius = Metrics.handleHeight / 2
         return view
     }()
 
-    init(metrics: Metrics = Metrics()) {
-        self.metrics = metrics
+    init(maxHeight: CGFloat = 400) {
+        self.maxHeight = maxHeight
         super.init(frame: .zero)
         setupViews()
     }
@@ -43,7 +42,7 @@ class ResizeGripView: UIView {
     func attach(to targetView: UIView, heightConstraint: Constraint) {
         self.targetView = targetView
         self.heightConstraint = heightConstraint
-        self.currentHeight = targetView.frame.height > 0 ? targetView.frame.height : (heightConstraint.layoutConstraints.first?.constant ?? metrics.minHeight)
+        self.currentHeight = targetView.frame.height > 0 ? targetView.frame.height : (heightConstraint.layoutConstraints.first?.constant ?? Metrics.defaultHeight)
         targetView.addSubview(self)
         snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
@@ -55,11 +54,11 @@ private extension ResizeGripView {
     func setupViews() {
         addSubview(handleView)
         handleView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(metrics.topPadding)
+            make.top.equalToSuperview().offset(Metrics.handlePadding)
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview()
-            make.width.equalTo(metrics.handleWidth)
-            make.height.equalTo(metrics.handleHeight)
+            make.width.equalTo(Metrics.handleWidth)
+            make.height.equalTo(Metrics.handleHeight)
         }
 
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
@@ -71,7 +70,7 @@ private extension ResizeGripView {
         case .changed:
             let translation = gesture.translation(in: superview)
             let newHeight = currentHeight - translation.y
-            let clampedHeight = min(max(newHeight, metrics.minHeight), metrics.maxHeight)
+            let clampedHeight = min(max(newHeight, 0), maxHeight)
             heightConstraint?.update(offset: clampedHeight)
         case .ended, .cancelled:
             currentHeight = targetView?.frame.height ?? currentHeight
