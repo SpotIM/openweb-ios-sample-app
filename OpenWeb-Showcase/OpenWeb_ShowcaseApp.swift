e739ced3f2b5fcbafb903f310265affab3a89cf1
Nogah <nogah@applift-consulting.com>
2026-03-02 13:42:26 +0200

    Create new app - OpenWeb-Showcase

---
+++ OpenWeb-Showcase/OpenWeb_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeb_ShowcaseApp.swift
+//  OpenWeb-Showcase
+//
+//  Created by  Nogah Melamed on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeb_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
