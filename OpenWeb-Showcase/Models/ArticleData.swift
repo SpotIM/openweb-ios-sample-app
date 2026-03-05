a0e9de6a5fa2c8d99a0adcbdde53c8b94e122dd8
Nogah <nogah@applift-consulting.com>
2026-03-05 12:44:48 +0200

    OW-38241 Add mock data for articles

---
+++ OpenWeb-Showcase/Models/ArticleData.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleData.swift
+//  OpenWeb-Showcase
+//
+//  Created by  Nogah Melamed on 02/03/2026.
+//  Copyright © 2026 OpenWeb. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleData {
+    let conversationIds: ConversationIdentifiers
+    let title: String
+    let paragraphs: [String]
+}
---
+++ OpenWeb-Showcase/Models/ConversationIdentifiers.swift
@@ -0,0 +1,14 @@
+//
+//  ConversationIdentifiers.swift
+//  OpenWeb-Showcase
+//
+//  Created by  Nogah Melamed on 02/03/2026.
+//  Copyright © 2026 OpenWeb. All rights reserved.
+//
+
+import Foundation
+
+struct ConversationIdentifiers {
+    let spotId: String
+    let postId: String
+}
