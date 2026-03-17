a45fd8527c2a361e846d94f8d22f4ff72f63c6b4
Nogah <nogah@applift-consulting.com>
2026-03-17 13:19:33 +0200

    OW-38250 SettingsSDKApplicable

---
+++ OpenWeb-Showcase/UI/SettingsScreen/SettingsSDKApplicable.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsSDKApplicable.swift
+//  OpenWeb-Showcase
+//
+//  Created by  Nogah Melamed on 17/03/2026.
+//  Copyright © 2026 OpenWeb. All rights reserved.
+//
+
+import OpenWebSDK
+
+// MARK: - SDKApplicable
+
+extension CustomizationsViewModel.SortOptionSetting: SDKApplicable {
+    func applyToSDK() {
+        let strategy: OWInitialSortStrategy = switch self {
+        case .server: .useServerConfig
+        case .best: .use(sortOption: .best)
+        case .newest: .use(sortOption: .newest)
+        case .oldest: .use(sortOption: .oldest)
+        }
+
+        // MARK: OpenWeb SDK
+        OpenWeb.manager.ui.customizations.sorting.initialOption = strategy
+    }
+}
